324c27967228bed95a2334905a1b83ee60e3dda3
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-07-05 14:30:58 -0300

    V0.22.07.05

--- app/Views/Benancib/Headers/navbar.php
+++ app/Views/Benancib/Headers/navbar.php
@@ -1,6 +1,21 @@
+<?php
+$Socials = new \App\Models\Socials();
+if ((isset($_SESSION['id'])) and ($_SESSION['id'] != ''))
+  {
+    $acesso = $Socials->nav_user();
+  } else {    
+    $acesso = '<li class="nav-item" style="list-style-type: none;">';
+    $acesso .= '<button class="btn btn-outline-danger" ';
+    $acesso .= 'onclick="location.href=/social" ';
+    $acesso .= 'style="margin-left: 7px;" type="submit">';
+    $acesso .= 'ACESSO';
+    $acesso .= '</button>';
+    $acesso .= '</li>';
+  }
+?>
 <nav class="navbar navbar-expand-lg bg-light fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="/favicon.png" style="height: 32px;"></a>
+    <a class="navbar-brand" href="<?=URL.COLLECTION;?>"><img src="/favicon.png" style="height: 32px;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-battery-charging" viewBox="0 0 16 16">
@@ -14,7 +29,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="<?=URL.COLLECTION;?>">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
@@ -38,6 +53,7 @@
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <?php echo $acesso;?>
     </div>
   </div>
 </nav>
