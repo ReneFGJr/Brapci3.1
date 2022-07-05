3cde46c6397055767eb8a720065dfdf04dca5521
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-07-05 14:32:53 -0300

    Update navbar.php

--- app/Views/Benancib/Headers/navbar.php
+++ app/Views/Benancib/Headers/navbar.php
@@ -15,7 +15,7 @@ if ((isset($_SESSION['id'])) and ($_SESSION['id'] != ''))
 ?>
 <nav class="navbar navbar-expand-lg bg-light fixed-top">
   <div class="container-fluid">
-    <a class="navbar-brand" href="<?=URL.COLLECTION;?>"><img src="/favicon.png" style="height: 32px;"></a>
+    <a class="navbar-brand" href="<?=URL.'/'.COLLECTION;?>"><img src="/favicon.png" style="height: 32px;"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-battery-charging" viewBox="0 0 16 16">
@@ -29,7 +29,7 @@ if ((isset($_SESSION['id'])) and ($_SESSION['id'] != ''))
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="<?=URL.COLLECTION;?>">Home</a>
+          <a class="nav-link active" aria-current="page" href="<?=URL.'/'.COLLECTION;?>">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Link</a>
