3bafbd86649900f29abe4beabbd08af2285bd845
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-07-11 11:40:27 -0300

    Update Pq.php

--- app/Controllers/Pq.php
+++ app/Controllers/Pq.php
@@ -7,28 +7,29 @@ use App\Controllers\BaseController;
 $this->session = \Config\Services::session();
 $language = \Config\Services::language();
 
-helper(['boostrap', 'url', 'sisdoc_forms', 'form', 'nbr','sessions','cookie']);
+helper(['boostrap', 'url', 'sisdoc_forms', 'form', 'nbr', 'sessions', 'cookie']);
 $session = \Config\Services::session();
 
-define("URL",getenv("app.baseURL"));
-define("PATH",getenv("app.baseURL").'/');
-define("MODULE",'');
-define("PREFIX",'');
+define("URL", getenv("app.baseURL"));
+define("PATH", getenv("app.baseURL") . '/');
+define("MODULE", '');
+define("PREFIX", '');
+define("COLLECTION ", 'pq/');
 
 class Pq extends BaseController
 {
-    public function index($d1='',$d2='',$d3='',$d4='')
+    public function index($d1 = '', $d2 = '', $d3 = '', $d4 = '')
     {
         $PQ = new \App\Models\PQ\Index();
         $data['page_title'] = 'Brapci - Pesquisadores Produtividade PQ CNPq';
         $data['bg'] = 'bg-pq';
         $sx = '';
-        $sx .= view('Brapci/Headers/header',$data);
-        $sx .= view('Brapci/Headers/navbar',$data);
-        $sa = '<a href="'.PATH.'/pq">'.view('Pq/logo',$data).'</a>';
-        $sb = $PQ->index($d1,$d2,$d3,$d4);
-        $sx .= bs(bsc($sa,2).bsc($sb,10));
-        $sx .= view('Brapci/Headers/footer',$data);
+        $sx .= view('Brapci/Headers/header', $data);
+        $sx .= view('Brapci/Headers/navbar', $data);
+        $sa = '<a href="' . PATH . '/pq">' . view('Pq/logo', $data) . '</a>';
+        $sb = $PQ->index($d1, $d2, $d3, $d4);
+        $sx .= bs(bsc($sa, 2) . bsc($sb, 10));
+        $sx .= view('Brapci/Headers/footer', $data);
         $sx .= '<div style="height: 80px; wdith: 100%;">X</div>';
         return $sx;
     }
