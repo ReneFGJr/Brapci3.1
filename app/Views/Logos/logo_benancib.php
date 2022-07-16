6ac183b6ecc72e364908617bd80e95356ba64419
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-07-16 12:20:01 -0300

    Update logo_benancib.php

--- app/Views/Logos/logo_benancib.php
+++ app/Views/Logos/logo_benancib.php
@@ -1,4 +1,2 @@
 <?php
-#echo '<img src="<?= URL;
-?>/img/logo/logo_benancib.png">';
-echo '<img src="<?= URL; ?>/img/logo/logo_benancib.gif" height="250px">';
+echo '<img src="' . URL . '/img/logo/logo_benancib.gif" height="250px">';
