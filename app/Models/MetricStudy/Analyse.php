a4568bc4cfc0af223470dca981a01249dfdc1ec6
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-08-07 13:11:31 -0300

    Update Analyse.php

--- app/Models/MetricStudy/Analyse.php
+++ app/Models/MetricStudy/Analyse.php
@@ -92,7 +92,7 @@ class Analyse extends Model
         $dd['types_total'] = pie($source, 'pie', lang('brapci.DocumentType'), 'DocumentType', 10, 1);
 
         arsort($auth);
-        $dd['authors_total'] = pie($auth, 'bar', lang('brapci.auth'), 'authTotal',10,1);
+        $dd['authors_total'] = graph($auth, 'bar', lang('brapci.auth'), 'authTotal',10,1);
 
         $sx = view('MetricStudy/DrashBoard', $dd);
 
