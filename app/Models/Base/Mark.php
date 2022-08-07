c5f00bae848aa86a4773419bd4067bbaa87d2343
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-08-07 11:09:12 -0300

    Update Mark.php

--- app/Models/Base/Mark.php
+++ app/Models/Base/Mark.php
@@ -44,34 +44,36 @@ class Mark extends Model
     protected $afterDelete    = [];
 
     function saveMark()
-        {
-            $Socials = new \App\Models\Socials();
-            $sx = '';
+    {
+        $Socials = new \App\Models\Socials();
+        $sx = '';
 
-            $user = $_SESSION['id'];
-            if (isset($_SESSION['markName']))
-                {
+        $user = $_SESSION['id'];
+        if (isset($_SESSION['markName'])) {
+            $idm = round($_SESSION['markName']);
+            $dt['mk_selected'] = $_SESSION['sel'];
+            $dt['mk_update_at'] = date("Y-m-dTH:i:s");
+            $this->set($dt)->where('id_mk', $idm)->update();
+            $sx .= 'Update ID:' . $idm;
+        } else {
+            $name = get("nameMark");
+            $sx .= form_open();
+            $sx .= form_input(array('name' => 'nameMark', 'class' => 'form-control', 'value' => $name));
+            $sx .= form_submit(array('name' => 'action', 'value' => lang('brapci.save')));
+            $sx .= form_close();
 
-                } else {
-                    $name = get("nameMark");
-                    $sx .= form_open();
-                    $sx .= form_input(array('name' =>'nameMark','class' =>'form-control','value' => $name));
-                    $sx .= form_submit(array('name' => 'action', 'value' =>lang('brapci.save')));
-                    $sx .= form_close();
-                }
+            if ((get("nameMark") != '') and (get("action") != '')) {
+                $dt['mk_user'] = $_SESSION['id'];
+                $dt['mk_name'] = get("nameMark");
+                $dt['mk_selected'] = $_SESSION['sel'];
+                $dt['mk_update_at'] = date("Y-m-dTH:i:s");
+                $idm = $this->set($dt)->insert();
+                $_SESSION['markName'] = $idm;
 
-            if ((get("nameMark") != '') and (get("action") != ''))
-                {
-                    $dt['mk_user'] = $_SESSION['id'];
-                    $dt['mk_name'] = get("nameMark");
-                    $dt['mk_selected'] = $_SESSION['sel'];
-                    $dt['mk_update_at'] = date("Y-m-dTH:i:s");
-                    $this->set($dt)->insert();
-                } else {
-
-                }
-            $sx = bs(bsc($sx,12));
-            return $sx;
-            pre($_SESSION);
+                $sx .= 'Saved "'.$dt['mk_name'].'"';
+            }
         }
+        $sx = bs(bsc($sx, 12));
+        return $sx;
+    }
 }
