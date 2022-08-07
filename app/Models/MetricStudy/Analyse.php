a5fe27569e0ed992a672f5beb23aae7d4227d538
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-08-07 11:23:41 -0300

    v0.22.08.07

--- app/Models/MetricStudy/Analyse.php
+++ app/Models/MetricStudy/Analyse.php
@@ -42,47 +42,59 @@ class Analyse extends Model
 
     function index()
     {
-        $Work = new \App\Models\Base\Work();
-        $sel = $Work->getWorkMark();
-        $dd['works'] = count($sel);
-
-        /********************************************************** TYPE */
-        /******************************************************* AUTHORS */
-        /******************************************************* SOURCES */
-        /******************************************************* SUBJECT */
-
-        $sx = view('MetricStudy/DrashBoard', $dd);
-        return $sx;
-    }
-
-    function a()
-        {
         $Keywords = new \App\Models\Base\Keywords();
         $Authors = new \App\Models\Base\Authors();
         $Sections = new \App\Models\Base\Sections();
         $Indexshow = new \App\Models\Base\IndexShow();
+        $Work = new \App\Models\Base\Work();
 
-        $dt = $this->issueWorks($id_rdf);
-        $sx = '';
+        $sel = $Work->getWorkMark();
+        $dd['works'] = count($sel);
 
-        /******************************* */
         /* Index */
         $auth = array();
         $keys = array();
         $sect = array();
 
+
+        foreach ($sel as $id => $selected) {
+            if ($selected == 1) {
+                if (substr($id, 0, 1) == 'w') {
+                    $id = round(substr($id, 1, 20));
+                    $keys = $Keywords->index_keys($keys, $id);
+                    $auth = $Authors->index_auths($auth, $id);
+                    $sect = $Sections->index_sections($sect, $id);
+                }
+            }
+        }
+
+        /********************************************************** TYPE */
+        /******************************************************* AUTHORS */
+        /******************************************************* SOURCES */
+        /******************************************************* SUBJECT */
+        $dd['authors_total'] = count($auth);
+        $dd['keys_total'] = count($keys);
+        $dd['sections_total'] = count($sect);
+        $sx = view('MetricStudy/DrashBoard', $dd);
+
+        return $sx;
+    }
+
+    function a()
+    {
+
         for ($r = 0; $r < count($dt); $r++) {
             $line = $dt[$r];
-            $sx .= '<p>' . $Work->show_reference($line['siw_work_rdf']) . '</p>';
+            $sx .= '<p>' . $Work->show_reference($id) . '</p>';
 
-            $keys = $Keywords->index_keys($keys, $line['siw_work_rdf']);
-            $auth = $Authors->index_auths($auth, $line['siw_work_rdf']);
-            $sect = $Sections->index_sections($sect, $line['siw_work_rdf']);
+            $keys = $Keywords->index_keys($keys, $id);
+            $auth = $Authors->index_auths($auth, $id);
+            $sect = $Sections->index_sections($sect, $id);
         }
         $key_index = $Indexshow->show_index($auth, 'authors');
         $key_index .= '<br>';
-        $key_index .= $Indexshow->show_index($sect,'sections');
+        $key_index .= $Indexshow->show_index($sect, 'sections');
         $key_index .= '<br>';
-        $key_index .= $Indexshow->show_index($keys,'keyword');
-        }
+        $key_index .= $Indexshow->show_index($keys, 'keyword');
+    }
 }
--- app/Views/MetricStudy/DrashBoard.php
+++ app/Views/MetricStudy/DrashBoard.php
@@ -1,7 +1,19 @@
 <div class="container">
     <div class="row">
         <div class="col-6">
-            <h1><?=$works;?></h1>
+            <h1><?= $works; ?> Works</h1>
         </div>
+    </div>
+
+    <div>
+        <?= $authors_total; ?> Authors
+    </div>
+
+    <div>
+        <?= $keys_total; ?> Keywords
+    </div>
+
+    <div>
+        <?= $sections_total; ?> Sections
     </div>
 </div>
