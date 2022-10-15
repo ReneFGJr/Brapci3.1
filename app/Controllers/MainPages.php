78f54cdc258b6de9a3a8255e80aed11fbd535af1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-10-15 04:33:34 -0300

    Update MainPages.php

--- app/Controllers/MainPages.php
+++ app/Controllers/MainPages.php
@@ -26,7 +26,7 @@ class MainPages extends BaseController
         /**** PAGES */
         if ($pag == '') {
             $pag = 'search';
-            $pag = 'under_construction';
+            //$pag = 'under_construction';
         }
         $data['page_title'] = 'Brapci - ' . ucfirst($pag);
         $sx .= view('Brapci/Headers/header', $data);
