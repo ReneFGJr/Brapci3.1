a73cb74fb5390e7878c8748d30a4b3d178eea299
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-10-22 05:28:41 -0300

    Update PeerReview.php

--- app/Models/ScientificCommunication/PeerReview.php
+++ app/Models/ScientificCommunication/PeerReview.php
@@ -93,10 +93,13 @@ class PeerReview extends Model
                 $membros = $dt['op_membros'];
                 $type = $dt['op_type'];
                 $membros = $dt['op_membros'];
+
+                $data = stodbr($dt['op_date']);
+
                 $sx .= bsc(h($dt['op_title'],3),10);
                 $sx .= bsc(h($type, 5), 2, 'text-end');
                 $sx .= bsc(h('<i>'.$dt['op_name']. '</i>',5),6);
-                $sx .= bsc(h($inst, 5),6);
+                $sx .= bsc(h($inst.' '.$data, 5),6,'text-end');
                 $sx .= h('<i>'.$membros. '</i>', 5);
                 return $sx;
             }
