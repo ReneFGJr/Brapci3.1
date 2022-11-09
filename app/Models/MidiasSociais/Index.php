95ecf963b171f18db54d128fc360f09deefa1c40
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-11-09 08:31:00 -0300

    v0.22.11.09

--- app/Models/MidiasSociais/Index.php
+++ app/Models/MidiasSociais/Index.php
@@ -6,86 +6,81 @@ use CodeIgniter\Model;
 
 class Index extends Model
 {
-	protected $DBGroup              = 'default';
-	protected $table                = 'source_listrecords';
-	protected $primaryKey           = 'id_lr';
-	protected $useAutoIncrement     = true;
-	protected $insertID             = 0;
-	protected $returnType           = 'array';
-	protected $useSoftDeletes       = false;
-	protected $protectFields        = true;
-	protected $allowedFields        = [
-		'id_lr', '	lr_identifier', 'lr_datestamp',
-		'lr_setSpec', 'lr_status', 'lr_jnl',
-		'lr_procees', 'lr_issue', 'lr_local_file',
-		'lr_work'
-	];
+    protected $DBGroup          = 'default';
+    protected $table            = 'indices';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
 
-	// Dates
-	protected $useTimestamps        = false;
-	protected $dateFormat           = 'datetime';
-	protected $createdField         = 'created_at';
-	protected $updatedField         = 'updated_at';
-	protected $deletedField         = 'deleted_at';
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
 
-	// Validation
-	protected $validationRules      = [];
-	protected $validationMessages   = [];
-	protected $skipValidation       = false;
-	protected $cleanValidationRules = true;
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
 
-	// Callbacks
-	protected $allowCallbacks       = true;
-	protected $beforeInsert         = [];
-	protected $afterInsert          = [];
-	protected $beforeUpdate         = [];
-	protected $afterUpdate          = [];
-	protected $beforeFind           = [];
-	protected $afterFind            = [];
-	protected $beforeDelete         = [];
-	protected $afterDelete          = [];
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
 
     function sharing($dt)
-        {
-            pre($dt);
-            $sx = '<i>Compartilhe</i><br>';
-            $sx .= $this->twitter($dt);
-            $sx .= $this->facebook($dt);
-            return $sx;
-        }
-        
-        function facebook($data) {
-            $d = $this -> dados($data);
-            $url = $d['doi'];
-            if (strlen($url) == 0) {
-                $url = $d['http'];
-            }
-            $nm = $d['title'] . '. ' . $d['autor_resumido'] . ' ' . $url;
-            $nm = urlencode($nm);
-            //https://www.facebook.com/dialog/share?app_id=140586622674265&display=popup&href=http%3A%2F%2Fseer.ufrgs.br%2Findex.php%2FEmQuestao%2Farticle%2Fview%2F56837%23.W3NwkU7trRU.facebook&picture=&title=Empoderamento%20das%20mulheres%20quilombolas%3A%20contribui%C3%A7%C3%B5es%20das%20pr%C3%A1ticas%20mediacionais%20desenvolvidas%20na%20Ci%C3%AAncia%20da%20Informa%C3%A7%C3%A3o&description=&redirect_uri=http%3A%2F%2Fs7.addthis.com%2Fstatic%2Fthankyou.html
-            $url = 'https://www.facebook.com/dialog/share?app_id=140586622674265&display=popup&href=' . $url . '&picture=&title=Divulgação Científica: '.$nm;
-            $link = '<span onclick="newwin2(\'' . $url . '\',800,400);" id="tw' . date("Ymdhis") . '" style="cursor: pointer;">';
-            $link .= '<img src="' . base_url('img/nets/icone_facebook.png') . '" class="icone_nets">';
-            $link .= '</span>' . cr();
-            return ($link);
-        }
+    {
+        pre($dt);
+        $sx = '<i>Compartilhe</i><br>';
+        $sx .= $this->twitter($dt);
+        $sx .= $this->facebook($dt);
+        return $sx;
+    }
     
-        function twitter($data) {
-            $d = $this -> dados($data);
-            //echo '<pre>';
-            //print_r($d);
-            //echo '</pre>';
-            $url = $d['doi'];
-            if (strlen($url) == 0) {
-                $url = $d['http'];
-            }
-    
-            $nm = $d['title'] . '. ' . $d['autor_resumido'] . '. ' . $url . ' #BRAPCI';
-            $nm = urlencode($nm);
-            $url = 'https://twitter.com/intent/tweet?text=' . $nm . '&related=';
-            $link = '<span onclick="newwin2(\'' . $url . '\',800,400);" id="tw' . date("Ymdhis") . '" style="cursor: pointer;">';
-            $link .= '<img src="' . base_url('img/nets/icone_twitter.png') . '" class="icone_nets">';
-            $link .= '</span>' . cr();
-            return ($link);
-        } 
+    function facebook($data) {
+        $d = $this -> dados($data);
+        $url = $d['doi'];
+        if (strlen($url) == 0) {
+            $url = $d['http'];
+        }
+        $nm = $d['title'] . '. ' . $d['autor_resumido'] . ' ' . $url;
+        $nm = urlencode($nm);
+        //https://www.facebook.com/dialog/share?app_id=140586622674265&display=popup&href=http%3A%2F%2Fseer.ufrgs.br%2Findex.php%2FEmQuestao%2Farticle%2Fview%2F56837%23.W3NwkU7trRU.facebook&picture=&title=Empoderamento%20das%20mulheres%20quilombolas%3A%20contribui%C3%A7%C3%B5es%20das%20pr%C3%A1ticas%20mediacionais%20desenvolvidas%20na%20Ci%C3%AAncia%20da%20Informa%C3%A7%C3%A3o&description=&redirect_uri=http%3A%2F%2Fs7.addthis.com%2Fstatic%2Fthankyou.html
+        $url = 'https://www.facebook.com/dialog/share?app_id=140586622674265&display=popup&href=' . $url . '&picture=&title=Divulgação Científica: '.$nm;
+        $link = '<span onclick="newwin2(\'' . $url . '\',800,400);" id="tw' . date("Ymdhis") . '" style="cursor: pointer;">';
+        $link .= '<img src="' . base_url('img/nets/icone_facebook.png') . '" class="icone_nets">';
+        $link .= '</span>' . cr();
+        return ($link);
+    }
+
+    function twitter($data) {
+        $d = $this -> dados($data);
+        //echo '<pre>';
+        //print_r($d);
+        //echo '</pre>';
+        $url = $d['doi'];
+        if (strlen($url) == 0) {
+            $url = $d['http'];
+        }
+
+        $nm = $d['title'] . '. ' . $d['autor_resumido'] . '. ' . $url . ' #BRAPCI';
+        $nm = urlencode($nm);
+        $url = 'https://twitter.com/intent/tweet?text=' . $nm . '&related=';
+        $link = '<span onclick="newwin2(\'' . $url . '\',800,400);" id="tw' . date("Ymdhis") . '" style="cursor: pointer;">';
+        $link .= '<img src="' . base_url('img/nets/icone_twitter.png') . '" class="icone_nets">';
+        $link .= '</span>' . cr();
+        return ($link);
+    }     
 }
