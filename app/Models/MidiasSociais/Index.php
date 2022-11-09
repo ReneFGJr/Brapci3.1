52729747ec6a53273f0d83673284f8f310c5e9db
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-11-09 08:47:56 -0300

    Update Index.php

--- app/Models/MidiasSociais/Index.php
+++ app/Models/MidiasSociais/Index.php
@@ -42,7 +42,7 @@ class Index extends Model
 
     function sharing($dt)
     {
-        pre($dt,false);
+        //pre($dt,false);
         $dt['http'] = 'https://cip.brapci.inf.br/benancib/v/'.$dt['id_cc'];
         $dt['doi'] = '';
         $dt['title'] = $dt['Title']['pt-BR'];
