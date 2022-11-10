a3dbb8870985e193e436e794905902ffa197102b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-11-10 08:21:22 -0300

    Update Header.php

--- app/Views/Benancib/Base/Header.php
+++ app/Views/Benancib/Base/Header.php
@@ -1,2 +1,3 @@
 <picture><img src="<?= URL; ?>/img/logo/logo_benancib.gif"></picture>
+<img width="200" height="200" src="<?= URL; ?>/img/logo/logo_benancib.gif" class="custom-logo" alt="Brapci" srcset="https://brapci.inf.br/img/brapci_200x200.png 150w" sizes="(max-width: 200px) 100vw, 200px" style="display: none;">
 <link rel="shortcut icon" href="<?= URL; ?>/img/logo/logo_benancib.gif">" type="image/x-icon" /> 
