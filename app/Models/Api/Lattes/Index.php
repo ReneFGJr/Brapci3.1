679782633833ca7442912355e37f501a6ffb547c
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2023-01-02 10:57:30 -0300

    Update Index.php

--- app/Models/Api/Lattes/Index.php
+++ app/Models/Api/Lattes/Index.php
@@ -44,23 +44,22 @@ class Index extends Model
     protected $beforeDelete   = [];
     protected $afterDelete    = [];
 
-    function index($d1='',$d2='',$d3='')
-        {
-            switch($d1)
-                {
-                    case 'extract':
-                        echo $this->extract_from_markers();
-                        exit;
-                        break;
-                    default:
-                        break;
-                }
+    function index($d1 = '', $d2 = '', $d3 = '')
+    {
+        switch ($d1) {
+            case 'extract':
+                echo $this->extract_from_markers();
+                exit;
+                break;
+            default:
+                break;
         }
+    }
 
     function checkID($code)
     {
-        $dig = substr($code,15,1);
-        $code = substr($code,0,15);
+        $dig = substr($code, 15, 1);
+        $code = substr($code, 0, 15);
 
         $weightflag = true;
         $sum = 0;
@@ -69,136 +68,125 @@ class Index extends Model
             $weightflag = !$weightflag;
         }
         $ver = (10 - ($sum % 10)) % 10;
-        if ($ver == $dig)
-            {
-                return 1;
-            } else {
-                return 0;
-            }
+        if ($ver == $dig) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
-    function registre_id_lattes($id,$data,$force=true)
-        {
-            if ($force == true)
-                {
-                    $dt = array();
-                } else {
-                    $dt = $this->where('cv_NRO_ID_CNPQ', $id)->findAll();
-                }
-
-            if (count($dt) == 0)
-                {
-                    $data['cv_NRO_ID_CNPQ'] = $id;
-                    $data['cv_SGL_PAIS'] = $data[1];
-                    $data['COD_AREA'] = $data[3];
-                    $data['cv_COD_NIVEL'] = $data[4];
-                    $data['DT_ATUALIZA'] = stodus(brtos($data[2]));
-                    $data['DTA_CARGA'] = date("Y-m-d");
-                    $this->insert($data);
-                    return true;
-                }
-            return false;
+    function registre_id_lattes($id, $data, $force = true)
+    {
+        if ($force == true) {
+            $dt = array();
+        } else {
+            $dt = $this->where('cv_NRO_ID_CNPQ', $id)->findAll();
         }
+
+        if (count($dt) == 0) {
+            $data['cv_NRO_ID_CNPQ'] = $id;
+            $data['cv_SGL_PAIS'] = $data[1];
+            $data['COD_AREA'] = $data[3];
+            $data['cv_COD_NIVEL'] = $data[4];
+            $data['DT_ATUALIZA'] = stodus(brtos($data[2]));
+            $data['DTA_CARGA'] = date("Y-m-d");
+            $this->insert($data);
+            return true;
+        }
+        return false;
+    }
 
     function harvesting_next($prj)
-        {
-            $LattesExtrator = new \App\Models\LattesExtrator\Index();
-            $ProjectsHarvestingXml = new \App\Models\Tools\ProjectsHarvestingXml();
-            $LattesDados = new \App\Models\LattesExtrator\LattesDados();
+    {
+        $LattesExtrator = new \App\Models\LattesExtrator\Index();
+        $ProjectsHarvestingXml = new \App\Models\Tools\ProjectsHarvestingXml();
+        $LattesDados = new \App\Models\LattesExtrator\LattesDados();
 
-            /* PHASE I ****************************** Localiza Próxima Coleta */
-            $dt = $ProjectsHarvestingXml
-                -> where('hx_project',$prj)
-                -> where('hx_status',0)
-                -> limit(1)
-                -> orderby("updated_at")
-                ->first();
+        /* PHASE I ****************************** Localiza Próxima Coleta */
+        $dt = $ProjectsHarvestingXml
+            ->where('hx_project', $prj)
+            ->where('hx_status', 0)
+            ->limit(1)
+            ->orderby("updated_at")
+            ->first();
 
-            /* PHASE II - MARCA REGISTRO DE ATUALIZACAO */
-            $dta['updated_at'] = date("Y-m-d H:i:s");
-            $ProjectsHarvestingXml->set($dta)->where('hx_project', $prj)->update();
+        /* PHASE II - MARCA REGISTRO DE ATUALIZACAO */
+        $dta['updated_at'] = date("Y-m-d H:i:s");
+        $ProjectsHarvestingXml->set($dta)->where('hx_project', $prj)->update();
 
-            /* SE EXISTE DADOS PARA COLETAR vai para proxima faase ****************/
-            if ($dt != '')
-                {
-                    /* PHASE III ******************************* Coleta dadados */
-                    $id_lattes = $dt['hx_id_lattes'];
+        /* SE EXISTE DADOS PARA COLETAR vai para proxima faase ****************/
+        if ($dt != '') {
+            /* PHASE III ******************************* Coleta dadados */
+            $id_lattes = $dt['hx_id_lattes'];
 
-                    $LT = new \App\Models\LattesExtrator\Index();
-                    $dt = $LT->harvesting($id);
+            $LT = new \App\Models\LattesExtrator\Index();
+            $dt = $LT->harvesting($id);
 
-                    /* PHASE IV ******************* Checa se existe arquivo *****/
+            /* PHASE IV ******************* Checa se existe arquivo *****/
 
-                    $sx .= '<br>Harvesting ' . $id;
-                    $file_xml = '../.tmp/lattes/' . $id . '.xml';
-                    if (file_exists($file_xml)) {
-                        $sx .= '<br>File XML OK';
-                    } else {
-                        $sx .= '<br>File ERRO OK';
-                    }
-
-                /* PHASE V ****************** Recupera dados do Pesquisador *****/
-                $dta = $LattesDados->le_id($id);
-                if (count($dta) == 0) {
-                    echo "ERRO: registro vazio";
-                    exit;
-                }
-                $dt = array();
-                $dt['hx_name'] = $dta['lt_name'];
-                $dt['hx_updated'] = $dta['lt_atualizacao'];
-                $dt['updated_at'] = date("Y-m-d H:i:s");
-                $dt['hx_status'] = 1;
-                $this->set($dt)->where('hx_id_lattes', $id)->update();
-                $sx .= cr().'Updated';
-
-                echo $sx;
-                exit;
+            $sx .= '<br>Harvesting ' . $id;
+            $file_xml = '../.tmp/lattes/' . $id . '.xml';
+            if (file_exists($file_xml)) {
+                $sx .= '<br>File XML OK';
+            } else {
+                $sx .= '<br>File ERRO OK';
             }
 
-                }
+            /* PHASE V ****************** Recupera dados do Pesquisador *****/
+            $dta = $LattesDados->le_id($id);
+            if (count($dta) == 0) {
+                echo "ERRO: registro vazio";
+                exit;
+            }
+            $dt = array();
+            $dt['hx_name'] = $dta['lt_name'];
+            $dt['hx_updated'] = $dta['lt_atualizacao'];
+            $dt['updated_at'] = date("Y-m-d H:i:s");
+            $dt['hx_status'] = 1;
+            $this->set($dt)->where('hx_id_lattes', $id)->update();
+            $sx .= cr() . 'Updated';
+
+            echo $sx;
+            exit;
         }
+    }
 
     function extract_from_markers()
-        {
-            $sx = '';
-            $url = 'http://memoria.cnpq.br/web/portal-lattes/extracoes-de-dados';
-            $url = 'http://memoria.cnpq.br/documents/313759/83395da6-f582-46bc-a308-060a6ec1ceaa';
+    {
+        $sx = '';
+        $url = 'http://memoria.cnpq.br/web/portal-lattes/extracoes-de-dados';
+        $url = 'http://memoria.cnpq.br/documents/313759/83395da6-f582-46bc-a308-060a6ec1ceaa';
 
-            $file = '../_Documments/Lattes/R358737.csv';
-            if (file_exists($file)) {
-                    /********************* Read Line by Line */
-                        $offset = round(get("offset"));
-                        $nr = 0;
-                        $limit = 500;
-                        $handle = fopen($file, "r");
-                        if ($handle) {
-                            while (($line = fgets($handle)) !== false) {
-                                /*************** In Range Scopo */
-                                if (($nr > 0) and ($nr >= $offset) and ($nr <= ($offset+$limit)))
-                                    {
-                                        $line = troca($line, '"', '');
-                                        $fld = explode(",", $line);
+        $file = '../_Documments/Lattes/R358737.csv';
+        if (file_exists($file)) {
+            /********************* Read Line by Line */
+            $offset = round(get("offset"));
+            $nr = 0;
+            $limit = 500;
+            $handle = fopen($file, "r");
+            if ($handle) {
+                while (($line = fgets($handle)) !== false) {
+                    /*************** In Range Scopo */
+                    if (($nr > 0) and ($nr >= $offset) and ($nr <= ($offset + $limit))) {
+                        $line = troca($line, '"', '');
+                        $fld = explode(",", $line);
 
-                                        if ($this->registre_id_lattes($fld[0],$fld))
-                                            {
-                                                $sx .= ($fld[0].' - '.$fld[1]).'<br>';
-                                            } else {
-                                                $sx .= ($fld[0].' - '.$fld[1].' - Already') . '<br>';
-                                            }
-                                    }
-                                /************** Stop ************/
-                                if ($nr >= ($offset + $limit + 1))
-                                    {
-                                        $sx .= metarefresh(PATH.COLLECTION.'/lattes/extract/?offset='.$nr,3);
-                                        break;
-                                    }
-                                $nr++;
-                            }
-                            fclose($handle);
+                        if ($this->registre_id_lattes($fld[0], $fld)) {
+                            $sx .= ($fld[0] . ' - ' . $fld[1]) . '<br>';
+                        } else {
+                            $sx .= ($fld[0] . ' - ' . $fld[1] . ' - Already') . '<br>';
                         }
-                    return $sx;
+                    }
+                    /************** Stop ************/
+                    if ($nr >= ($offset + $limit + 1)) {
+                        $sx .= metarefresh(PATH . COLLECTION . '/lattes/extract/?offset=' . $nr, 3);
+                        break;
+                    }
+                    $nr++;
                 }
-
-
+                fclose($handle);
+            }
+            return $sx;
         }
+    }
 }
