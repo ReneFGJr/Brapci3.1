17f0ea439cd6ede59270a598d394ff5b58a774b3
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2023-01-12 17:43:41 -0300

    Update Export.php

--- app/Models/Base/Export.php
+++ app/Models/Base/Export.php
@@ -44,68 +44,62 @@ class Export extends Model
     protected $afterDelete    = [];
     var $eof = 0;
 
-    function index($d1,$d2,$d3)
-        {
-            $sx = 'EXPORT '.$d1;
-            switch($d1)
-                {
-                    default:
-                        $sx = bsc($this->menu(),12);
-                        break;
-                }
-            $sx = bs($sx);
-            return $sx;
+    function index($d1, $d2, $d3)
+    {
+        $sx = 'EXPORT ' . $d1;
+        switch ($d1) {
+            default:
+                $sx = bsc($this->menu(), 12);
+                break;
         }
+        $sx = bs($sx);
+        return $sx;
+    }
 
-    function next($type='')
-        {
-            $dt = $this->where('task_status',1)->orderBy('task_propriry')->findAll();
-            if (count($dt) > 0)
-                {
-                    $dt = $dt[0];
-                }
-            return $dt;
+    function next($type = '')
+    {
+        $dt = $this->where('task_status', 1)->orderBy('task_propriry')->findAll();
+        if (count($dt) > 0) {
+            $dt = $dt[0];
         }
+        return $dt;
+    }
 
-    function register($task_id,$priority,$offset,$status)
-        {
-            $dta['task_id'] = $task_id;
-            $dta['task_status'] = $status;
-            $dta['task_propriry'] = $priority;
-            $dta['task_offset'] = $offset;
-            $dta['updated_at'] = date("Y-m-d H:i:s");
-            $dt = $this->where('task_id',$task_id)->findAll();
-            if (count($dt) == 0)
-                {
-                    $this->set($dta)->insert();
+    function register($task_id, $priority, $offset, $status)
+    {
+        $dta['task_id'] = $task_id;
+        $dta['task_status'] = $status;
+        $dta['task_propriry'] = $priority;
+        $dta['task_offset'] = $offset;
+        $dta['updated_at'] = date("Y-m-d H:i:s");
+        $dt = $this->where('task_id', $task_id)->findAll();
+        if (count($dt) == 0) {
+            $this->set($dta)->insert();
+        } else {
+            $this->set($dta)->where('task_id', $task_id)->update();
+        }
+        return true;
+    }
+
+    function cron($d1, $d2, $d3 = '')
+    {
+        $sx = '';
+        switch ($d1) {
+                /************************************ DEFAULT */
+            default:
+                if ($d1 == '') {
+                    $sx .= '=====EXPORT============' . cr();
+                    $dtd = $this->next();
+
+                    if (count($dtd) > 0) {
+                        $sx .= $this->export_works($dtd);
+                    }
                 } else {
-                    $this->set($dta)->where('task_id',$task_id)->update();
+                    echo "OPS EXPORT NOT FOUND [$d1]";
                 }
-            return true;
         }
-
-    function cron($d1,$d2,$d3='')
-        {
-            $sx = '';
-            switch($d1)
-                {
-                    /************************************ DEFAULT */
-                    default:
-                    if ($d1 == '')
-                        {
-                            $sx .= '=====EXPORT============'.cr();
-                            $dtd = $this->next();
-
-                            if (count($dtd) > 0)
-                                {
-                                    $sx .= $this->export_works($dtd);
-                                }
-                        } else {
-                            echo "OPS EXPORT NOT FOUND [$d1]";
-                        }
-                }
-            return $sx;
-        }
+        return $sx;
+    }
 
 
     function menu()
@@ -114,7 +108,7 @@ class Export extends Model
         $menu = array();
         $mod = 'export';
         $menu['#brapci.EXPORT_ELASTIC'] = '#';
-        $menu[PATH.'admin/' . $mod . '/articles'] = lang('brapci.export').' '.lang('brapci.articles');
+        $menu[PATH . 'admin/' . $mod . '/articles'] = lang('brapci.export') . ' ' . lang('brapci.articles');
         $menu[PATH . 'admin/' . $mod . '/proceeding'] = lang('brapci.export') . ' ' . lang('brapci.proceeding');
         $menu[PATH . 'admin/' . $mod . '/books'] = lang('brapci.export') . ' ' . lang('brapci.books');
         $sx = menu($menu);
@@ -122,70 +116,73 @@ class Export extends Model
         return $sx;
     }
 
-    function export_works($dta,$id=0)
-        {
-            $sx = '';
-            $offset = round(0);
-            $limit = 100;
+    function export_works($dta, $id = 0)
+    {
+        $sx = '';
+        $offset = round(0);
+        $limit = 100;
 
-            $TYPE = $dta['task_id'];
+        $TYPE = $dta['task_id'];
 
-            if ($dta['task_id'] == 'EXPORT_ARTICLE')
-                {
-                    $class = 'Article';
-                    $type = 'JA';
-                    $offset = $dta['task_offset'];
-                    $sx .= "<br>OFFSET: $offset";
-                    $sx .= $this->export_data($class, $type, $offset, $limit);
-                    if ($this->eof)
-                        {
-                            $this->register($TYPE, 0, 0, 0);
-                        } else {
-                            $this->register($TYPE, 1, $offset + $limit, 1);
-                        }
-
-                    $sx .= '<CONTINUE>';
-                }
-            return $sx;
+        if ($dta['task_id'] == 'EXPORT_ARTICLE') {
+            $class = 'Article';
+            $type = 'JA';
+        }
+        if ($dta['task_id'] == 'EXPORT_BOOK') {
+            $class = 'Book';
+            $type = 'BO';
+        }
+        if ($dta['task_id'] == 'EXPORT_PROCEEDING') {
+            $class = 'Proceeding';
+            $type = 'EV';
         }
 
-    function export_data($class,$type,$offset,$limit)
-        {
-            $RDF = new \App\Models\Rdf\RDF();
-            $RDFClass = new \App\Models\Rdf\RDFClass();
-            $RDFConcept = new \App\Models\Rdf\RDFConcept();
-            $Metadata = new \App\Models\Base\Metadata();
-            $ElasticRegister = new \App\Models\ElasticSearch\Register();
+        $offset = $dta['task_offset'];
+        $sx .= "<br>OFFSET: $offset";
+        $sx .= $this->export_data($class, $type, $offset, $limit);
+        if ($this->eof) {
+            $this->register($TYPE, 0, 0, 0);
+        } else {
+            $this->register($TYPE, 1, $offset + $limit, 1);
+            $sx .= '<CONTINUE>';
+        }
+        return $sx;
+    }
 
-            $id = $RDFClass->Class($class,false);
+    function export_data($class, $type, $offset, $limit)
+    {
+        $RDF = new \App\Models\Rdf\RDF();
+        $RDFClass = new \App\Models\Rdf\RDFClass();
+        $RDFConcept = new \App\Models\Rdf\RDFConcept();
+        $Metadata = new \App\Models\Base\Metadata();
+        $ElasticRegister = new \App\Models\ElasticSearch\Register();
 
-            $total = $RDFConcept->select('count(*) as total')
-                        ->where('cc_class', $id)
-                        ->findAll();
-            $ids = $RDFConcept->where('cc_class',$id)->findAll($limit,$offset);
-            if (count($ids) == 0)
-                {
-                    $this->eof = 1;
-                    return "FIM";
-                }
-            $sx = '';
-            $total = $total[0]['total'];
-            $sx .= '<br>Processado: '.(number_format($offset/$total*100,1,',','.')).'%';
-            $sx .= '<ul>';
-            for ($r=0;$r < count($ids);$r++)
-                {
-                    $line = $ids[$r];
-                    $idr = $line['id_cc'];
+        $id = $RDFClass->Class($class, false);
 
-                    $dir = $RDF->directory($idr);
-                    $file = $dir . 'article.json';
+        $total = $RDFConcept->select('count(*) as total')
+            ->where('cc_class', $id)
+            ->findAll();
+        $ids = $RDFConcept->where('cc_class', $id)->findAll($limit, $offset);
+        if (count($ids) == 0) {
+            $this->eof = 1;
+            return "FIM";
+        }
+        $sx = '';
+        $total = $total[0]['total'];
+        $sx .= '<br>Processado: ' . (number_format($offset / $total * 100, 1, ',', '.')) . '%';
+        $sx .= '<ul>';
+        for ($r = 0; $r < count($ids); $r++) {
+            $line = $ids[$r];
+            $idr = $line['id_cc'];
 
-                    if (!file_exists($file))
-                        {
-                            $RDF->c($idr);
-                        }
+            $dir = $RDF->directory($idr);
+            $file = $dir . 'article.json';
 
-                    /*
+            if (!file_exists($file)) {
+                $RDF->c($idr);
+            }
+
+            /*
                     $json = file_get_contents($file);
                     $json = (array)json_decode($json);
                     $json['type'] = $type;
@@ -194,17 +191,17 @@ class Export extends Model
                     $sx .= '<li>' . strzero($idr, 8) . ' ' . $ElasticRegister->data($idr, $json) . ' (' . $dir . ')</li>';
                     */
 
-                    $line = $RDF->le($idr);
-                    $Metadata->metadata = array();
-                    $Metadata->metadata($line);
-                    $meta = $Metadata->metadata;
-                    $meta['collection'] = substr($class,0,1);
-                    $meta['fulltext'] = '';
-                    $meta['year'] = '';
-                    $meta['pdf'] = 0;
-                    $sx .= '<li>'.strzero($meta['article_id'],8).' '.$ElasticRegister->data($idr,$meta).' ('.$dir.')</li>';
-                }
-            $sx .= '</ul>';
-            return $sx;
+            $line = $RDF->le($idr);
+            $Metadata->metadata = array();
+            $Metadata->metadata($line);
+            $meta = $Metadata->metadata;
+            $meta['collection'] = substr($class, 0, 1);
+            $meta['fulltext'] = '';
+            $meta['year'] = '';
+            $meta['pdf'] = 0;
+            $sx .= '<li>' . strzero($meta['article_id'], 8) . ' ' . $ElasticRegister->data($idr, $meta) . ' (' . $dir . ')</li>';
         }
+        $sx .= '</ul>';
+        return $sx;
+    }
 }
