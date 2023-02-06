91e7c6abfbbb5d25f9c58b8c722d75da33aa8a6c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-02-06 20:26:58 -0300

    Update Socials.php

--- app/Models/Socials.php
+++ app/Models/Socials.php
@@ -1145,9 +1145,7 @@ class Socials extends Model
 		$txt = troca($txt, '$link', $link_html);
 		$txt .= '</center>';
 		$subject = '[' . getenv('app.project_name') . '] ' . lang('social.forgout_email_title');
-
 		$Email->sendmail($email, $subject, $txt);
-		$Email->sendmail('renefgj@gmail.com', 'cc:'.$subject, 'Send to:'.$email.'<hr>'.$txt);
 
 		return $sx;
 	}
