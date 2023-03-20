62d872c5ae3b15fa9aa546cca1cf8f98b0f9f9b0
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-03-19 22:30:58 -0300

    Update Sumary.php

--- app/Models/AI/NLP/Book/Sumary.php
+++ app/Models/AI/NLP/Book/Sumary.php
@@ -44,11 +44,18 @@ class Sumary extends Model
 
     function show_form()
     {
+        $options = [];
+        $options[1]  = 'Método 1 - Títulos em Caixa Alta';
+        $options[2] = 'method 2 - Manual Inserção';
+
         $txt = get('text');
         $txt = $this->process($txt);
         $sx = '';
         $sx .= form_open();
         $sx .= form_textarea(array('name' => 'text', 'value' => $txt, 'class' => 'form-control-lm', 'style' => 'width: 100%;'));
+        $sx .= form_label('Method') . ': ';
+        $sx .= form_dropdown('method', $options);
+        $sx .= '<br>';
         $sx .= form_submit(array('name' => 'actiom', 'value' => lang('brapci.save')));
         $sx .= form_close();
 
@@ -64,7 +71,7 @@ class Sumary extends Model
         $js = $j;
         $j = (array)json_decode($j);
 
-        $sx .= '<textarea class="form-control" style="width: 100%;">'.$js.'</textarea>';
+        $sx .= '<textarea class="form-control" style="width: 100%; height: 200px;">' . $js . '</textarea>';
 
         $sx .= '<ul>';
         foreach ($j as $line) {
@@ -102,6 +109,7 @@ class Sumary extends Model
         return $sx;
     }
 
+    /**************************************** Processamento */
     function process($txt)
     {
         $txt = troca($txt, chr(10), chr(13));
@@ -121,14 +129,30 @@ class Sumary extends Model
 
         $lr = array();
 
-        foreach ($ln as $line) {
-            if (strtolower(ascii(trim($line))) == 'sumario') {
-                $line = '';
+        $continue = false;
+        $line = '';
+
+        foreach ($ln as $lline) {
+            if (strtolower(ascii(trim($lline))) == 'sumario') {
+                $lline = '';
             }
+
+            if (!$continue) {
+                $line = $lline;
+            } else {
+                $line .= $lline;
+            }
+            $continue = false;
+
+            /* Remove Capítulo */
+            $line = $this->remove_captitulo($line);
 
             if (strlen(trim($line)) > 0) {
                 $first = trim(substr($line, 0, 10));
                 $last = trim(substr($line, strlen($line) - 5, 5));
+                $clast = trim(substr($line, strlen($line) - 1, 1));
+                $cfirst = substr($line,0,1);
+                $cfirst_low = substr(mb_strtolower($line), 0, 1);
 
                 /*************************************** Elimina Números */
                 if ($first == sonumero($first)) {
@@ -139,25 +163,143 @@ class Sumary extends Model
                 $sonum = sonumero($first);
                 if (round($sonum) > 0) {
                     $first2 = $first;
-                    $c = array('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
+                    $c = array('.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.');
                     $first2 = trim(str_replace($c, array(''), $first));
                     $line = troca($line, $first, $first2);
                 }
 
                 /*********************** Insere o número da pagina inicial */
-                $sonum = sonumero(substr($line,10,strlen($line)));
+                $sonum = sonumero(substr($line, 10, strlen($line)));
                 if (round($sonum) > 0) {
                     $line = troca($line, $sonum, '{p.' . $sonum);
                 }
 
+                if ($clast == ',') {
+                    $continue = true;
+                }
 
+                if ($cfirst_low == $cfirst)
+                    {
+                    //$continue = true;
+                    }
+                //echo '<br>'.$line." [$continue]";
                 /*********************** Guada dados em arquivo ************/
-                if (strlen($line) > 0) {
+                if ((strlen($line) > 0) and (!$continue)) {
                     array_push($lr, $line);
+                    $continue = false;
                 }
             }
         }
+        $method = get("method");
 
+        switch ($method) {
+            case '2':
+                $this->method02($lr);
+                break;
+            default:
+                $this->method01($lr);
+                break;
+        }
+
+        $t = '';
+        for($r=0;$r < count($lr);$r++)
+            {
+                $t .= $lr[$r].cr();
+            }
+
+        return $txt2;
+    }
+
+    function remove_captitulo($ln)
+    {
+        $v = ['CAPITULO', 'CAPÍTULO'];
+        for ($r = 1; $r < 30; $r++) {
+            foreach ($v as $id => $term) {
+                if (strpos(' ' . $ln, $term . ' ' . $r)) {
+                    $ln = troca($ln, $term . ' ' . $r, '');
+                    return $ln;
+                }
+            }
+        }
+        return $ln;
+    }
+
+    /********* TITULO MISTURADO COM AUTORES */
+    function method02($lr)
+    {
+        $ln = [];
+        foreach ($lr as $id => $line) {
+            array_push($ln,$line);
+        }
+
+        $this->json($ln);
+        return '';
+    }
+
+    /********* TITULO EM CAIXA ALTA */
+    function method01($lr)
+    {
+        $ln = [];
+        $xln = '';
+        $title = false;
+        $author = false;
+        $xpag = 'X';
+        foreach ($lr as $id => $line) {
+            $line = troca($line, '. ', '');
+            $pos = strpos($line, ' ');
+            if ($pos > 0) {
+                $first = trim(substr($line, 0, $pos));
+            } else {
+                $first = $line;
+            }
+            $ufirst = mb_strtoupper($first);
+            $upper = ($first == $ufirst);
+            $pag = (strpos($line, '{p.') > 0);
+            if ($upper) {
+                if ($author == true) {
+                    array_push($ln, $xln);
+                    $xln = '';
+                }
+                if ($title == true) {
+                    if ($pag == $xpag) {
+                        array_push($ln, $xln);
+                        $xln = $line;
+                    } else {
+                        $xln .= ' ' . $line;
+                    }
+                    $title = true;
+                } else {
+                    $title = true;
+                    $xln = $line;
+                }
+                $author = false;
+            } else {
+                if ($author == true) {
+                    $xln .= ';' . $line;
+                } else {
+                    $xln .= '[' . $line;
+                }
+                $title = false;
+                $author = true;
+            }
+            $xpag = $pag;
+            //echo '<br>' . $line . " A[$author] T[$title] U[$upper] P[$pag]";
+        }
+        if ($xln != '') {
+            array_push($ln, $xln);
+        }
+        for ($r = 0; $r < count($ln); $r++) {
+            if (!strpos($ln[$r], '[')) {
+                $ln[$r] .= '[';
+            }
+        }
+
+        $this->json($ln);
+        return '';
+    }
+
+    function json($lr)
+    {
         $w = array();
         $xpag = 0;
         $autor = array();
@@ -174,16 +316,16 @@ class Sumary extends Model
             }
             /************************** Titulo */
             if ($pos = strpos($titulo, '[')) {
-                 $titulo = substr($titulo, 0, $pos);
+                $titulo = substr($titulo, 0, $pos);
             }
             $titulo1 = mb_strtolower($titulo);
-            $titulo = (mb_substr($titulo,0,1).mb_substr($titulo1,1,strlen($titulo1)));
+            $titulo = (mb_substr($titulo, 0, 1) . mb_substr($titulo1, 1, strlen($titulo1)));
+            $titulo = troca($titulo, '.', '');
             $titulo = trim($titulo);
 
-            if (substr($titulo,strlen($titulo)-1,1) == '.')
-                {
-                    $titulo = substr($titulo,0,strlen($titulo)-1);
-                }
+            if (substr($titulo, strlen($titulo) - 1, 1) == '.') {
+                $titulo = substr($titulo, 0, strlen($titulo) - 1);
+            }
 
             /************************** AUTORES */
             if ($pos = strpos($ln, '[')) {
@@ -193,15 +335,12 @@ class Sumary extends Model
                 $at = troca($at, '[', '');
                 $at = troca($at, ']', '');
 
-                if (strpos($at,';'))
-                    {
-
-                    } else {
-                        if (strpos($at, ','))
-                            {
-                                $at = troca($at,',',';');
-                            }
+                if (strpos($at, ';')) {
+                } else {
+                    if (strpos($at, ',')) {
+                        $at = troca($at, ',', ';');
                     }
+                }
                 //$at = troca($at,'.','');
                 $au = explode(';', $at);
                 foreach ($au as $id => $nome) {
@@ -209,12 +348,10 @@ class Sumary extends Model
                         $nome = trim(substr($nome, 0, strpos($nome, '{')));
                     }
                     $nome = trim($nome);
-                    if (substr($nome,strlen($nome)-1,1) == '.')
-                        {
-                            $nome = substr($nome,0,strlen($nome)-1);
-                        }
-                    if (strlen(trim($nome)) > 0)
-                    {
+                    if (substr($nome, strlen($nome) - 1, 1) == '.') {
+                        $nome = substr($nome, 0, strlen($nome) - 1);
+                    }
+                    if (strlen(trim($nome)) > 0) {
                         $nome = nbr_author($nome, 1);
                         $au[$id] = $nome;
                     }
@@ -234,6 +371,6 @@ class Sumary extends Model
             array_push($w, $work);
         }
         $this->json = json_encode($w);
-        return $txt2;
+        return "";
     }
 }
