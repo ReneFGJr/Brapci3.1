11b2e19d0ec4b08de3b31f4b143fbaeff056203c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-05-07 08:32:31 -0300

    Update Person.php

--- app/Views/Authority/Person.php
+++ app/Views/Authority/Person.php
@@ -1,5 +1,22 @@
 <div class="container">
     <div class="row">
+        <div class="col-1">
+        </div>
+        <div class="col-2">
+            <?= $photo; ?>
+        </div>
+        <div class="col-8 border-start border-secondary">
+            <?= h($Identifier, 2); ?>
+        </div>
+        <div class="col-1">
+            <a href="<?= PATH . '/autoridade/v/' . $ID; ?>" class="btn btn-outline-secondary">brp:<?= $ID; ?></a>
+        </div>
+    </div>
+</div>
+
+
+<div class="container">
+    <div class="row">
         <div class="col-1 border-5 bg-authority border-end small">
             <?= $Class; ?>
         </div>
