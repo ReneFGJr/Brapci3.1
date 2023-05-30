f27a433f53aa580015603cf873ef426f4aebe667
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-05-29 21:56:53 -0300

    v0.23.050.29

---
+++ app/Models/Api/Endpoint/DoiLattesAuthor.php
@@ -0,0 +1,71 @@
+<?php
+/*
+@category API
+@package Brapci Identificação de Author pelo DOI
+@name
+@author Rene Faustino Gabriel Junior <renefgj@gmail.com>
+@copyright 2022 CC-BY
+@access public/private/apikey
+@example $PATH/api/doiToFormation/?doi=10.1086/430801
+@abstract API para determinar o autor e formacao
+*/
+
+namespace App\Models\Api\Endpoint;
+
+use App\Database\Migrations\LattesDados;
+use App\Models\LattesExtrator\LattesProducao;
+use CodeIgniter\Model;
+
+class DoiLattesAuthor extends Model
+{
+    protected $DBGroup          = 'default';
+    protected $table            = 'doilattesauthors';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID         = 0;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = false;
+    protected $protectFields    = true;
+    protected $allowedFields    = [];
+
+    // Dates
+    protected $useTimestamps = false;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert   = [];
+    protected $afterInsert    = [];
+    protected $beforeUpdate   = [];
+    protected $afterUpdate    = [];
+    protected $beforeFind     = [];
+    protected $afterFind      = [];
+    protected $beforeDelete   = [];
+    protected $afterDelete    = [];
+
+    function index($d1 = '', $d2 = '', $d3 = '')
+    {
+        $name = get("doi");
+        if ($name != '') {
+            return $this->doiAuthor($name);
+        }
+        return "NnN";
+    }
+
+    function doiAuthor($name)
+        {
+            $LattesProducao = new \App\Models\LattesExtrator\LattesProducao();
+            $dt = $LattesProducao->where('lp_doi',$name)->findAll();
+            pre($dt);
+            exit;
+        }
+}
