eb1347cac19de26c62b413fe2f38d6b72a834786
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-06-23 07:18:06 -0300

    Update Index.php

--- app/Models/Find/BooksOld/Index.php
+++ app/Models/Find/BooksOld/Index.php
@@ -41,59 +41,89 @@ class Index extends Model
     protected $afterDelete    = [];
 
     function harvesting($id)
-        {
-            $Data = new \App\Models\Find\BooksOld\Data();
-            $Expression = new \App\Models\Find\Books\Db\Expression();
-            $dt = $Expression->where('be_status',-1)->first();
+    {
+        $Data = new \App\Models\Find\BooksOld\Data();
+        $Expression = new \App\Models\Find\Books\Db\Expression();
+        $dt = $Expression->where('be_status', -1)->first();
 
-            if ($dt != '')
-                {
-                    $idx = $dt['be_rdf'];
-                    $dd = $Data
-                            ->join('rdf_name', 'd_literal = id_n', 'LEFT')
-                            ->where('d_r1',$idx)
-                            ->where('d_literal <> 0')
-                            ->findAll();
-                    pre($dd,false);
+        if ($dt != '') {
+            $idx = $dt['be_rdf'];
+            $dd = $Data
+                ->join('rdf_name', 'd_literal = id_n', 'LEFT')
+                ->where('d_r1', $idx)
+                ->where('d_literal <> 0')
+                ->findAll();
+            pre($dd, false);
 
-                    $dd = $Data
-                        ->join('rdf_class', 'd_p = id_c')
-                        ->where('d_r1', $idx)
-                        ->where('d_r2 <> 0')
-                        ->findAll();
-                    pre($dd, false);
+            $dd = $this->leData($idx);
+
+            $t_author = [];
+            $t_expre = [];
+
+            foreach ($dd as $idl => $linel) {
+                $class = trim($linel['c_class']);
+                $idl = $linel['d_r2'];
+                switch ($class) {
+                    case 'isAppellationOfExpression':
+                        array_push($t_expre, $idl);
+                        break;
+
+                    case 'hasAuthor':
+                        array_push($t_author,$idl);
+
+                        break;
+
+                    default:
+                        echo "OPS";
+                        echo h($class);
+                        exit;
                 }
+            }
+
+            pre($t_author,false);
+            pre($dd, false);
         }
+    }
+
+    function leData($idx)
+    {
+        $Data = new \App\Models\Find\BooksOld\Data();
+        $dd = $Data
+            ->join('rdf_class', 'd_p = id_c')
+            ->where('d_r1', $idx)
+            ->where('d_r2 <> 0')
+            ->findAll();
+        return $dd;
+    }
 
     function inport()
-        {
-            $Books = new \App\Models\Find\Books\Db\Books();
-            $cp = 'id_cc, cc_library, id_c, c_class, d_r2, n_name, n_lang';
-            $db = $this
-                    ->select($cp)
-                    ->join('rdf_data','id_cc = d_r1')
-                    ->join('rdf_class','d_p = id_c')
-                    ->join('rdf_name', 'd_literal = id_n','LEFT')
-                    ->where('cc_class',16)
-                    ->where('id_c', 5)
-                    ->findAll(0,10);
-           foreach($db as $id=>$line)
-            {
-                $title = trim($line['n_name']);
-                $id = $line['id_cc'];
-                $da['be_title'] = $title;
-                $da['be_rdf'] = $id;
-                $da['be_status'] = -1;
-                $da['be_cover'] = '';
-                $da['be_authors'] = '';
-                $da['be_isbn13'] = '';
-                $da['be_isbn10'] = '';
-                $da['be_type'] = '';
-                $da['be_lang'] = 1;
-                $dd = $Books->register($id,$da);
-            }
-            $url = PATH . 'admin/find/harvesting/0';
-            $sx = anchor($url,$url);
-            return $sx;
+    {
+        $Books = new \App\Models\Find\Books\Db\Books();
+        $cp = 'id_cc, cc_library, id_c, c_class, d_r2, n_name, n_lang';
+        $db = $this
+            ->select($cp)
+            ->join('rdf_data', 'id_cc = d_r1')
+            ->join('rdf_class', 'd_p = id_c')
+            ->join('rdf_name', 'd_literal = id_n', 'LEFT')
+            ->where('cc_class', 16)
+            ->where('id_c', 5)
+            ->findAll(0, 10);
+        foreach ($db as $id => $line) {
+            $title = trim($line['n_name']);
+            $id = $line['id_cc'];
+            $da['be_title'] = $title;
+            $da['be_rdf'] = $id;
+            $da['be_status'] = -1;
+            $da['be_cover'] = '';
+            $da['be_authors'] = '';
+            $da['be_isbn13'] = '';
+            $da['be_isbn10'] = '';
+            $da['be_type'] = '';
+            $da['be_lang'] = 1;
+            $dd = $Books->register($id, $da);
         }
+        $url = PATH . 'admin/find/harvesting/0';
+        $sx = anchor($url, $url);
+        return $sx;
+    }
 }
