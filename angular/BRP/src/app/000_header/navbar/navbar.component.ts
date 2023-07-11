b70ef4bf4a695d53c0ebe2427cfc0b3b4eb6a16e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-07-11 06:55:40 -0300

    v0.23.07.11

--- angular/BRP/src/app/000_header/navbar/navbar.component.ts
+++ angular/BRP/src/app/000_header/navbar/navbar.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
+
 export class NavbarComponent {
 
+  public event: any | any;
+
+  public name = 'Hello';
+
+  constructor() {
+    document.addEventListener('click', (clickEvent: MouseEvent) => {
+      console.log('Click Event Details: ', clickEvent)
+    })
+
+    document.addEventListener('scroll', (scr: any) => {
+      console.log(document.documentElement.scrollTop);
+    })
+  }
+
+    onScroll(): void {
+      console.log("H")
+      console.log(document.documentElement.scrollTop);
+    }
 }
