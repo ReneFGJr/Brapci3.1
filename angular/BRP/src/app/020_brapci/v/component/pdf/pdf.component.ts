910d2b176f8e50ee55ee9c6134a47d6a1acbe5b6
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-07-20 14:32:14 -0300

    v0.23.07.20

--- angular/BRP/src/app/020_brapci/v/component/pdf/pdf.component.ts
+++ angular/BRP/src/app/020_brapci/v/component/pdf/pdf.component.ts
@@ -8,8 +8,16 @@ import { BrapciService } from 'src/app/020_brapci/service/brapci.service';
 })
 export class PdfComponent {
   @Input() public url:string='';
+  @Input() public id: string = '';
 
   constructor(
     public brapciService: BrapciService
   ) {}
+
+  download()
+    {
+      alert("download " +this.id);
+      alert("download " + this.url);
+      window.open(this.url, '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+    }
 }
