466b887aa393d9962e3b4f865a98bd13b1cb98f4
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-01 08:21:46 -0300

    Update Index.php

--- app/Models/Gev3nt/Index.php
+++ app/Models/Gev3nt/Index.php
@@ -51,7 +51,7 @@ class Index extends Model
         $dt = $this->getId($id);
         $dt['ID'] = $id;
 
-        $cp = 'e_sigla, e_name, es_event, es_name, id_es, es_hora_ini, es_data';
+        $cp = 'e_sigla, e_name, es_event, es_name, id_es, es_hora_ini, es_data, es_cpf, an_nome';
         $dt['subscribed'] = $this
             ->select($cp . ', count(*) as total')
             ->join('event_sections', 'es_event = id_e')
