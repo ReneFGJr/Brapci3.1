b8bfd9df78d79988c83df2f7529be6d0b1103b8a
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-02 13:48:47 -0300

    Update BooksManifestation.php

--- app/Models/Find/Books/Db/BooksManifestation.php
+++ app/Models/Find/Books/Db/BooksManifestation.php
@@ -53,6 +53,8 @@ class BooksManifestation extends Model
                 ->where('d_r1',$id)
                 ->orderBy('c_order, c_class')
                 ->findAll();
+
+                echo $this->getlastquery();
             return $dt;
         }
 
