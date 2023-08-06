be41059d537d9cab2165984ba7736e46541065c3
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-06 09:44:42 -0300

    Update Cover.php

--- app/Models/Find/Books/Db/Cover.php
+++ app/Models/Find/Books/Db/Cover.php
@@ -44,41 +44,42 @@ class Cover extends Model
     function saveDataCover($isbn, $data)
     {
 
-            $RSP = [];
-            $dir = substr($isbn, 0, 3) . '/' . substr($isbn, 3, 4) . '/' . substr($isbn, 7, 3) . '/' . substr($isbn , 10,3);
-            $dir = '_repository/book/'.$dir;
-            list($type, $data) = explode(';', $data);
-            list(, $data)      = explode(',', $data);
-            $data = base64_decode($data);
+        $RSP = [];
+        $dir = substr($isbn, 0, 3) . '/' . substr($isbn, 3, 4) . '/' . substr($isbn, 7, 3) . '/' . substr($isbn, 10, 3);
+        $dir = '_repository/book/' . $dir;
+        list($type, $data) = explode(';', $data);
+        list(, $data)      = explode(',', $data);
+        $data = base64_decode($data);
 
-            $RSP['dir'] = $dir;
-            $RSP['type'] = $type;
-            $RSP['isbn'] = $isbn;
-            $RSP['len'] = strlen($data);
+        $RSP['dir'] = $dir;
+        $RSP['type'] = $type;
+        $RSP['isbn'] = $isbn;
+        $RSP['len'] = strlen($data);
 
-            dircheck($dir);
-            $ext = '';
-            switch($type)
-                {
-                    case 'data:image/png':
-                        $ext = '.png';
-                        break;
-                    case 'data:image/jpg':
-                        $ext = '.jpg';
-                        break;
-                    default:
-                        $RSP['message'] = 'Formato inválido '.$type;
-                        break;
-                }
-            if ($ext != '')
-                {
-                    $BookExpression = new \App\Models\Find\Books\Db\BooksExpression();
-                    file_put_contents($dir . '/' . $isbn . $ext, $data);
-                    $RSP['cover'] = URL.'/'.$dir . '/'. $isbn . $ext;
-                    $dt = [];
-                    $dt['be_cover'] = URL . '/' . $dir . '/' . $isbn . $ext;
-                    $BookExpression->set($dt)->where('be_isbn13',$isbn)->update();
-                }
+        dircheck($dir);
+        $ext = '';
+        switch ($type) {
+            case 'data:image/png':
+                $ext = '.png';
+                break;
+            case 'data:image/jpg':
+                $ext = '.jpg';
+                break;
+            case 'data:image/jpeg':
+                $ext = '.jpg';
+                break;
+            default:
+                $RSP['message'] = 'Formato inválido ' . $type;
+                break;
+        }
+        if ($ext != '') {
+            $BookExpression = new \App\Models\Find\Books\Db\BooksExpression();
+            file_put_contents($dir . '/' . $isbn . $ext, $data);
+            $RSP['cover'] = URL . '/' . $dir . '/' . $isbn . $ext;
+            $dt = [];
+            $dt['be_cover'] = URL . '/' . $dir . '/' . $isbn . $ext;
+            $BookExpression->set($dt)->where('be_isbn13', $isbn)->update();
+        }
         return $RSP;
     }
 }
