529b5595e62c2718f406bd983dd95fcd942a49d2
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-09 17:41:31 -0300

    Update footer.php

--- app/Views/DCI/Headers/footer.php
+++ app/Views/DCI/Headers/footer.php
@@ -6,6 +6,7 @@ if (!isset($bg)) {
     $bg = 'bg-primary';
 }
 ?>
+XXXXXXX
 <footer class="<?= $bg; ?> text-white text-center text-lg-start" style="margin-top: 120px;">
     <!-- Grid container -->
     <div class="container-fluid p-4">
