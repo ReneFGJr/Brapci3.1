e5eeea2d120db9eaaa4efdd747b971b259dc0304
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-24 08:43:47 -0300

    Update IssuesWorks.php

--- app/Models/Base/IssuesWorks.php
+++ app/Models/Base/IssuesWorks.php
@@ -15,7 +15,7 @@ class IssuesWorks extends Model
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
     protected $allowedFields    = [
-        'id_siw', 'siw_journal', 'siw_issue',
+        'id_siw', 'siw_journal', 'siw_issue','siw.author','siw_title','siw_publish',
         'siw_section', 'siw_work_rdf', 'update_at',
     ];
 
