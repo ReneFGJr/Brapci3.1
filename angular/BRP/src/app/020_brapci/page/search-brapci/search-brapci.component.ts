5fc480b51f031cadd05d99adbbee41f8371e88ab
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-25 13:23:04 -0300

    v0.23.08.25

--- angular/BRP/src/app/020_brapci/page/search-brapci/search-brapci.component.ts
+++ angular/BRP/src/app/020_brapci/page/search-brapci/search-brapci.component.ts
@@ -4,27 +4,29 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 @Component({
   selector: 'app-search-brapci',
   templateUrl: './search-brapci.component.html',
-  styleUrls: ['./search-brapci.component.scss']
+  styleUrls: ['./search-brapci.component.scss'],
 })
 export class SearchBrapciComponent {
-  public filters:boolean = false;
-  public advanceSearch:string = '';
+  public filters: boolean = false;
+  public advanceSearch: string = '';
   public term: string = '';
   public year_start: number = 1962;
-  public year_end: number = new Date().getFullYear()+1;
+  public year_end: number = new Date().getFullYear() + 1;
   public APIversion: string = '1';
 
-  constructor(
-    private fb: FormBuilder,
-  ) {}
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
     this.createForm();
   }
 
-  clickFilters()
+  clickFilters() {
+    this.filters = !this.filters;
+  }
+
+  clickadvanceSearch()
     {
-      this.filters = !this.filters
+
     }
 
   searchForm: FormGroup | any;
@@ -38,14 +40,13 @@ export class SearchBrapciComponent {
   }
 
   onSearch() {
-    if (this.searchForm.valid)
-      {
-        console.log("OK");
-        let term = this.searchForm.value.term;
-        //this.bannerComponent.search(term);
-      } else {
-        console.log("NÃO OK");
-      }
+    if (this.searchForm.valid) {
+      console.log('OK');
+      let term = this.searchForm.value.term;
+      //this.bannerComponent.search(term);
+    } else {
+      console.log('NÃO OK');
+    }
   }
   onKeyPress() {}
 }
