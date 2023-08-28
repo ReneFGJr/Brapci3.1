5aaa685b1e8e1851e67d8012de54dade50f4b83b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-28 06:19:09 -0300

    v0.23.08.28

--- angular/BRP/src/app/020_brapci/page/v/article/article.component.ts
+++ angular/BRP/src/app/020_brapci/page/v/article/article.component.ts
@@ -3,18 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-brapci-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
 })
 export class ArticleComponent {
-  @Input() public data:Array<any> | any
-  public url:string = '';
+  @Input() public data: Array<any> | any;
+  public url: string = '';
+  public rdf: string = '/assets/img/icone_rdf.png';
 
   ngOnInit(): void {
     //this.url = this.data.id;
     //console.log(this.data);
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-
   }
-
 }
