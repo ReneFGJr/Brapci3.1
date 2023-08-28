9a962dcef53fe9c78b6aa72b1338792282fe3d5b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-28 07:25:01 -0300

    v0.23.08.28

--- angular/BRP/src/app/010_thema/header/theme-footer/theme-footer.component.ts
+++ angular/BRP/src/app/010_thema/header/theme-footer/theme-footer.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-theme-footer',
   templateUrl: './theme-footer.component.html',
-  styleUrls: ['./theme-footer.component.scss']
+  styleUrls: ['./theme-footer.component.scss'],
 })
 export class ThemeFooterComponent {
+  public brapci_data: number = 2010;
 
+  ngOnInit() {
+    this.brapci_data = new Date().getFullYear();
+    console.log(this.brapci_data); // output 2020
+  }
 }
