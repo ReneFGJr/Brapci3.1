45dc7ebeebacad97b83e8a4a1ab5db989a274458
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-08-29 21:43:59 -0300

    v0.23.09.29

--- angular/BRP/src/app/app.module.ts
+++ angular/BRP/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -10,13 +10,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ThemeModule } from './010_thema/theme.module';
 
 import { CoreBrapciModule } from './020_brapci/core-brapci.module';
+
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(ptBr);
+
 //import { RouterModule } from '@angular/router';
 //import { CoreBrapciComponent } from './200_brapci/core-brapci/core-brapci.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
+  declarations: [AppComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
@@ -24,11 +27,9 @@ import { CoreBrapciModule } from './020_brapci/core-brapci.module';
     NgbModule,
     ThemeModule,
     CoreBrapciModule,
-],
-  providers: [
-
-    ],
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: 'pt-BR' }],
   bootstrap: [AppComponent],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
