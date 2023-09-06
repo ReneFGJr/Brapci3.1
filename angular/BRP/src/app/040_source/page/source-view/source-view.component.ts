09b7ffe8f971883fcdd7a2067172ff6d4855495d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-09-05 21:34:31 -0300

    v0.23.09.05

--- angular/BRP/src/app/040_source/page/source-view/source-view.component.ts
+++ angular/BRP/src/app/040_source/page/source-view/source-view.component.ts
@@ -6,29 +6,29 @@ import { BrapciService } from 'src/app/020_brapci/service/brapci.service';
 @Component({
   selector: 'app-source-view',
   templateUrl: './source-view.component.html',
-  styleUrls: ['./source-view.component.scss']
+  styleUrls: ['./source-view.component.scss'],
 })
 export class SourceViewComponent {
-  public sub:Array<any>|any
-  public data:Array<any>|any
-  public id:number = 0
+  public sub: Array<any> | any;
+  public source: Array<any> | any;
+  public id: number = 0;
 
   constructor(
     private brapciService: BrapciService,
-    private route: ActivatedRoute,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.sub = this.route.params.subscribe(params => {
-      this.id = + params['id']; // (+) converts string 'id' to a number
+    this.sub = this.route.params.subscribe((params) => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
 
-      this.brapciService.getId(this.id).subscribe(
-        res => {
-          this.data = res;
+      this.brapciService.source(this.id).subscribe(
+        (res) => {
+          this.source = res;
+          console.log(this.source);
         },
-        error => error
-      )
+        (error) => error
+      );
     });
   }
-
 }
--- angular/BRP/src/app/040_source/source.module.ts
+++ angular/BRP/src/app/040_source/source.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { SourceRoutingModule } from './source-routing.module';
 import { SourceComponent } from './page/source/source.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ThemeModule } from '../010_thema/theme.module';
 
 @NgModule({
-  declarations: [
-    SourceComponent
-  ],
+  declarations: [SourceComponent],
   imports: [
     CommonModule,
-    SourceRoutingModule
-  ]
+    SourceRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    RouterModule,
+    ThemeModule,
+  ],
 })
-export class SourceModule { }
+export class SourceModule {}
