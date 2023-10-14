3d9d7d90dda2d98bc30d1c2375501d1b4d1d121f
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-14 16:28:27 -0300

    Update Issues.php

--- app/Models/Base/Issues.php
+++ app/Models/Base/Issues.php
@@ -201,6 +201,7 @@ class Issues extends Model
 
         $da['is_oai_update'] = date("Y-m-d H:i:s");
         $dt = $this->where('is_issue', $da['is_source_issue'])->first();
+        echo '<hr>'.$this->getlastquery().'<hr>';
         echo "=1=1=1=1=1=";
         pre($dt,false);
         echo "=2=22222222=";
