492e7eb4084f75ca905f6dcec5a05658125cbb48
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-14 13:59:00 -0300

    Update Metadata.php

--- app/Models/Base/Metadata.php
+++ app/Models/Base/Metadata.php
@@ -410,6 +410,7 @@ class Metadata extends Model
                         }
 
                     $Is = $RDF->extract($dt, 'hasIssueOf');
+                    pre($dt);
                     echo h('US=>'.$Is[0]);
                     $dti = $Issue->getIssue($Is[0]);
 
