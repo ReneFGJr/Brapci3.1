e64b9389f3f117393211bfe7357d2fbc8a607a83
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-10-21 22:27:41 -0300

    Update Metadata.php

--- app/Models/Base/Metadata.php
+++ app/Models/Base/Metadata.php
@@ -255,7 +255,7 @@ class Metadata extends Model
                         $ds = $Source->where('jnl_frbr',$ddv2)->first();
                         if ($ds != '')
                         {
-                            $this->lets('id_jnl', $ds['id_jnl']);
+                            $this->metadata['id_jnl'] = $ddv2;
                         }
                         break;
                     case 'hasLanguageExpression':
