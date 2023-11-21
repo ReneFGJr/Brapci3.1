354c2ecaa08197e679cf7d440c603c827070d784
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-11-21 10:14:44 -0300

    Update RDFrules.php

--- app/Models/RDF2/RDFrules.php
+++ app/Models/RDF2/RDFrules.php
@@ -62,13 +62,13 @@ class RDFrules extends Model
         if (!$this->rule01($dt)) {
             $ok = false;
         }
-        echo "R1";
+
         if (($ok == true) and (!$this->rule02($dt))) {
             $ok = false;
         }
-        echo "R2";
+
         if ($ok == false) {
-            pre($this->message);
+            pre($this->message,false);
         }
 
         return $ok;
