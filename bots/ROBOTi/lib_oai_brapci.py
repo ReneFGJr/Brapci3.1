13026d374f0e59c9af9597a733169fbf33464688
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-11-24 09:38:41 -0300

    Update lib_oai_brapci.py

--- bots/ROBOTi/lib_oai_brapci.py
+++ bots/ROBOTi/lib_oai_brapci.py
@@ -17,8 +17,8 @@ def updateIDENTIFY(ID):
 def getIDENTIFY():
     now_time = datetime.datetime.now()
     month = now_time.month
-    query = "select id_jnl,jnl_url_oai,jnl_name from brapci.source_source "
-    query += "where (jnl_historic = 0 and jnl_active = 1 and jnl_url_oai <> '')"
+    query = f"select id_jnl,jnl_url_oai,jnl_name from brapci.source_source \n"
+    query += f"where (jnl_historic = 0 and jnl_active = 1 and jnl_url_oai <> '') \n"
     query += f" and ((month(`update_at`) <> {month}) or (update_at is null)) "
     query += f" and (jnl_oai_status <> '100') "
     query += "order by update_at"
