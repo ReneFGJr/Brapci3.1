d2eee0f48a64b89b4c5ae2d399d0b5e9a8934bb0
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-12-24 16:44:31 -0300

    Update oaipmh_request.py

--- bots/ROBOTi/oaipmh_request.py
+++ bots/ROBOTi/oaipmh_request.py
@@ -8,7 +8,7 @@ from colorama import Fore
 
 urllib3.disable_warnings()
 
-xheaders = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
         'AppleWebKit/537.36 (KHTML, like Gecko) '\
         'Chrome/75.0.3770.80 Safari/537.36'}
 zheaders = {
@@ -18,7 +18,7 @@ zheaders = {
     "User-Agent": "python-requests/2.26.0",
   }
 
-headers = {
+xheaders = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
