e7fe3b2fd957cda3764b9af3ef7f536991db4dd9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-12-24 15:32:19 -0300

    Update oaipmh_ListIdentifiers.py

--- bots/ROBOTi/oaipmh_ListIdentifiers.py
+++ bots/ROBOTi/oaipmh_ListIdentifiers.py
@@ -71,6 +71,8 @@ def xml_identifies(xml,setSpec):
 
         ############################ Register
         print("deleted",deleted)
+        print("spec",spec)
+        print(setSpec)
         identifiers[id] = {'setSpec':setSpec[spec],'date':date,'deleted':deleted}
 
     return identifiers
