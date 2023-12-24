5314137f95195ececdf9747e2cbafa85936e202f
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-12-24 16:32:33 -0300

    Update oaipmh_request.py

--- bots/ROBOTi/oaipmh_request.py
+++ bots/ROBOTi/oaipmh_request.py
@@ -12,12 +12,11 @@ xheaders = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '\
         'AppleWebKit/537.36 (KHTML, like Gecko) '\
         'Chrome/75.0.3770.80 Safari/537.36'}
 headers = {
-    'authority': 'www.google.com',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-    'accept-language': 'en-US,en;q=0.9',
-    'cache-control': 'max-age=0',
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
-}
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Host": "brapci.inf.br",
+    "User-Agent": "python-requests/2.26.0",
+  }
 
 def get(LINK):
     status_code = '000'
