cb3c8440872783f5bdc216c37aaa283230c7eb7f
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-12-26 17:27:53 -0300

    Update mod_language.py

--- bots/ROBOTi/mod_language.py
+++ bots/ROBOTi/mod_language.py
@@ -19,6 +19,8 @@ def check(lg):
         lg = 'fr'
     elif (lg == 'fr-CA'):
         lg = 'fr'
+    elif (lg == 'fra'):
+        lg = 'fr'
     else:
         print("ERRO LANGUAGE ",lg)
         quit()
