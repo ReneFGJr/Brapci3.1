78c05006e7ca0325a55ae85c1a624c96a03959bd
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2023-12-27 17:31:21 -0300

    Update Register.php

--- app/Models/ElasticSearch/Register.php
+++ app/Models/ElasticSearch/Register.php
@@ -595,8 +595,10 @@ class Register extends Model
         /*********************** CONVERT DADOS */
         echo h("Convertendo Dados",4);
         $data = $this->data_convert_elastic($xdata);
-        pre($data);
+
         echo h("Checando Dados convertidos", 4);
+        pre($data);
+
         $this->check($data, true, $id);
         echo h("Fim da Checagem", 4);
 
