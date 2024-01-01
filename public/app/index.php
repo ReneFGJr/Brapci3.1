203c61610230c1bcb26804e06a3a16782bb9dcb2
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-01 10:11:25 -0300

    Update index.php

--- public/app/index.php
+++ public/app/index.php
@@ -11,6 +11,43 @@ print_r($_SERVER);
 $path = $_SERVER['PATH_INFO'];
 $pt = explode('/',$path);
 echo "<h1>PATH</h1>";
-print_r($pt);
+if (isset($pt[1]))
+{
+    switch($pt[1])
+        {
+            case 'res':
+                res($pt);
+                break;
+        }
+}
+
+function res($pt)
+    {
+        if (isset($pt[2]))
+            {
+                switch($pt[2])
+                    {
+                        case 'v':
+                            if (isset($pt[3]))
+                                {
+                                    v($pt[3]);
+                                } else {
+                                    e404();
+                                }
+                        default:
+                            e404();
+                    }
+            }
+    }
+
+function v($id)
+    {
+        echo "===========".$id;
+    }
+
+function e404()
+    {
+        echo "Página não localizada 404";
+    }
 ?>
 </pre>
