13fb2c09068e9c0d4439b7a1c41a8cae4da43ff4
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-03 10:35:41 -0300

    Update Brapci.php

--- app/Models/Api/Endpoint/Brapci.php
+++ app/Models/Api/Endpoint/Brapci.php
@@ -402,7 +402,7 @@ class Brapci extends Model
 
         foreach ($dt as $id => $line) {
             $ABNT = new \App\Models\Metadata\Abnt();
-            $type = $line['type'];
+            $type = $line['CLASS'];
             $ln = [];
             $ln['title'] = $line['ldl_title'];
             $ln['Authors'] = explode(';', $line['ldl_authors']);
