8d5f0868850845fd245181fc3732e7799e9e4f08
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-03 11:35:05 -0300

    Update Abnt.php

--- app/Models/Metadata/Abnt.php
+++ app/Models/Metadata/Abnt.php
@@ -50,7 +50,7 @@ class Abnt extends Model
 			$legend = $this->ref_legend($dt['Issue']);
 			$ref = $authors . '. ' . $title . '. ' . $legend;
 			$ref .= '. Acesso em:'.date("d").'/'. mes_abreviado(date("m")).'/'.date("Y");
-			$ref .= '. Disponível em:'.$URL.$dt['ID'];
+			$ref .= '. Disponível em: '.anchor($URL.$dt['ID']);
 			$ref = troca($ref,'..','.');
 			return($ref);
 		}
