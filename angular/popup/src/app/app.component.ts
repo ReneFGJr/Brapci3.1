8c20509b97762edbe24ab42e40d792ff4f454410
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-05 08:40:24 -0300

    v0.24.01.05

--- angular/popup/src/app/app.component.ts
+++ angular/popup/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  busy: number = 0;
+  search: any = '';
   title = 'popup';
+  btn_new: string = 'false';
+  btn_cancel: string = '';
+  btn_aplly: string = 'false';
+
+  constructor(private formBuilder: FormBuilder) {}
+  name = new FormControl('', [Validators.required, Validators.maxLength(15)]);
+
+  keyUp() {
+    if (this.busy == 0) {
+      this.busy = 1;
+      this.search = this.name.value;
+
+      if (this.search.length > 2) {
+        console.log(this.search);
+      }
+      this.busy = 0;
+    }
+  }
 }
--- angular/popup/src/app/app.module.ts
+++ angular/popup/src/app/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
