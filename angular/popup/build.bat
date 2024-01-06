d3d4cf191d3b1f782ffc9b282abdc4d979f03d70
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-06 13:05:40 -0300

    v0.24.01.06

--- angular/popup/build.bat
+++ angular/popup/build.bat
@@ -1,2 +1,2 @@
 rem ng build --output-path ../../public/app --base-href './'.
-ng build --base-href 'https://cip.brapci.inf.br/app/popup/' --output-path ../../public/app/popup
+ng build  --output-path ../../public/app/popup --base-href './'.
