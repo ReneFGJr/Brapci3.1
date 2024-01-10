741d92928e26da609814b83558f8ef840c541fc8
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-10 05:51:46 -0300

    A

--- public/app/index.php
+++ public/app/index.php
@@ -1,40 +1,19 @@
 <?php
 $path = $_SERVER['PATH_INFO'];
 $pt = explode('/', $path);
-
+echo "<h1>PATH</h1>";
 if (isset($pt[1])) {
     switch ($pt[1]) {
         case 'res':
             res($pt);
             break;
-        default:
-          echo "<h1>PATH</h1>";
-          pre($pt, false);
     }
-}
-
-function indice($pt)
-{
-  if (!isset($pt[2]))
-    {
-      $pt[2] = 'subject';
-    }
-  if (!isset($pt[3]))
-    {
-      $pt[3] = 'A';
-    }
-  header("Location: https://brapci.inf.br/#/indexs/".$pt[2]."/" . $pt[3]);
-  die();
 }
 
 function res($pt)
 {
-  pre($pt);
     if (isset($pt[2])) {
         switch ($pt[2]) {
-            case 'indice':
-              indice($pt);
-              break;
             case 'v':
                 if (isset($pt[3])) {
                     v($pt[3]);
@@ -57,5 +36,4 @@ function v($id)
 function e404()
 {
     echo "Página não localizada 404";
-    pre($_SERVER['PATH_INFO']);
 }
