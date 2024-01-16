11abdd1b7a0f7eace5c03ae993b85c5ec10be2ea
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-16 12:11:12 -0300

    Update mod_article.py

--- bots/ROBOTi/mod_article.py
+++ bots/ROBOTi/mod_article.py
@@ -243,7 +243,12 @@ def create_article(rg,data,jnl):
             if (k == 'section'):
                 #ok = True
                 S = data[i][k]
-                print(S['section']);
+                ids = S['section']
+                print("Section",S['section']);
+                qr = "select * from brapci_oaipmh.oai_setspec "
+                qr += f"where id_s = {ids}"
+                row = database.query($qr);
+                print("ROW",row)
                 quit()
                 #mod_journal.register(IDC,J)
 
