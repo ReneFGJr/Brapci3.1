accfcb84df43cad3f1c47134a81e41a9186e5c7c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-19 16:26:54 -0300

    Update Download.php

--- app/Models/Base/Download.php
+++ app/Models/Base/Download.php
@@ -305,7 +305,7 @@ class Download extends Model
 
                 }
 
-            pre($txt,false);
+            pre(substr($txt,0,100),false);
 
             /********************* IFRAME */
             if (substr($txt, 0, 2) == '<iframe') {
