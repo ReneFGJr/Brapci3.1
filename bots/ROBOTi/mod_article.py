8fb6116fe9d0f7c0eef7736aee8e58ddb24e8f82
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-20 08:44:11 -0300

    Update mod_article.py

--- bots/ROBOTi/mod_article.py
+++ bots/ROBOTi/mod_article.py
@@ -61,6 +61,7 @@ def process(rg):
         print("ERRO ARTICLE",e)
 
 def check_method01(id,jnl):
+    print("Method 01")
     jnl = str(jnl)
     while (len(jnl) < 5):
         jnl = "0"+jnl
@@ -74,6 +75,8 @@ def check_method01(id,jnl):
     qr += f"where n_name = '{ID}' or n_name = '{id}'"
     qr += "group by id_cc"
     row = database.query(qr)
+
+    print("ROW",len(row))
 
     if row == []:
         return 0
