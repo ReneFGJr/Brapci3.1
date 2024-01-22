5fa383bc88da0f032f1246a56636e15dba549827
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-22 12:11:46 -0300

    Update mod_ontology.py

--- bots/ROBOTi/mod_ontology.py
+++ bots/ROBOTi/mod_ontology.py
@@ -2,7 +2,7 @@ import database
 
 def checkData():
     qr = "select * "
-    qr = "select id_d, c2.cc_class as class2, cr_range "
+    qr = "select id_d, C2.cc_class as class2, cr_range "
     qr += "FROM brapci_rdf.rdf_data "
     qr += "inner Join brapci_rdf.rdf_class_range ON cr_property = d_p "
     qr += "Inner Join brapci_rdf.rdf_concept as C2 ON C2.id_cc = d_r2 "
@@ -12,6 +12,7 @@ def checkData():
     row = database.query(qr)
 
     qu = ""
+    n = 0
     for item in row:
         ID = item[0]
         C2 = item[1]
@@ -19,7 +20,11 @@ def checkData():
 
         if C2 == C3:
             qu = f"update brapci_rdf.rdf_data set d_trust = 1 where id_d = {ID}"
-            print(".", end=' ')
+            print(".", end='')
+            n = n + 1
+            if (n > 50):
+                n = 0
+                print("")
             database.update(qu)
         else:
             print(ID,C2,C3)
