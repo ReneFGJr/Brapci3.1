6d066350bbe28aeac5613a09c7312cf207f85525
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-26 12:54:04 -0300

    v0.24.01.26

--- angular/BRP/src/app/020_brapci/page/v/article/article.component.ts
+++ angular/BRP/src/app/020_brapci/page/v/article/article.component.ts
@@ -2,18 +2,20 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-brapci-article',
-  templateUrl: './article.component.html'
+  templateUrl: './article.component.html',
 })
 export class ArticleComponent {
   @Input() public data: Array<any> | any;
   public url: string = '';
   public rdf: string = '/assets/img/icone_rdf.png';
-  public header:Array<any>|any = null
+  public header: Array<any> | any = null;
+  public langs: Array<any> = ['pt', 'en', 'es', 'fr'];
+  objectKeys = Object.keys
 
   ngOnInit(): void {
-    console.log(this.data)
-    this.header = []
-    this.header = {'title':'Artigo'}
+    console.log(this.data);
+    this.header = [];
+    this.header = { title: 'Artigo' };
     //this.url = this.data.id;
     //console.log(this.data);
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
