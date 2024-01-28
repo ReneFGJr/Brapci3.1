d5aa6e72586f70ea079e1635f2a20ba6d8f21342
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-28 10:29:01 -0300

    Update Download.php

--- app/Models/Base/Download.php
+++ app/Models/Base/Download.php
@@ -310,7 +310,7 @@ class Download extends Model
             /********************* IFRAME */
             if ($pos = strpos($txt, 'class="download"') > 0) {
                 $txt = substr($txt,$pos-100,100);
-                $txt = substr($txt,strpos($txt,'http',100));
+                $txt = substr($txt,strpos($txt,'http'),100);
                 pre($txt);
 
             }
