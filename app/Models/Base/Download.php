06897dc19586ef122b249309390145758164f433
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-01-28 11:46:18 -0300

    Update Download.php

--- app/Models/Base/Download.php
+++ app/Models/Base/Download.php
@@ -281,6 +281,7 @@ class Download extends Model
     function ocs_2($url)
     {
         $Otxt = 'VAZIO';
+        echo '===>'.$url;
 
         if (strpos($url, 'article/view')) {
             $txt = read_link($url);
