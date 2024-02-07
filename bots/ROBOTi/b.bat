d9082f1498932a8b8f7694f9375eb03fc66ec1c9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-02-07 11:04:59 -0300

    Update b.bat

--- bots/ROBOTi/b.bat
+++ bots/ROBOTi/b.bat
@@ -1,4 +1,32 @@
-cls
-python ROBOTi.py 110
-timeout /t 3
-b.bat
+
+http://admin/page/edit/about/up?id=7
+http://admin/page/edit/about/up?id=7
+http://brp//admin/page/edit/about/up?id=7
+
+FNDE
+cecampe - nordeste
+
+https://funetec.com/editais/visualizar/52b73c48e5d8abb91534b21464df8a0d_415.pdf
+
+BED-096
+https://www.cecampe.ufpb.br/paineisdeinformacoes
+
+https://sig-arq.ufpb.br/arquivos/202310605293225969979e9b885c4976d/RESOLUO_01_2023_ESTGIO_PS_DOUTORADO.pdf
+
+
+https://buscatextual.cnpq.br/buscatextual/busca.do?metodo=forwardPaginaResultados&registros=10;10&query=%2Bidx_area_atua%3A%22Ci%EAncia+da+Informa%E7%E3o%22+%2Bidx_sub_area_atua%3A%22Arquivologia%22%2Bidx_particao%3A1+%2Bidx_nacionalidade%3Ab&analise=cv&tipoOrdenacao=null&paginaOrigem=index.do&mostrarScore=true&mostrarBandeira=false&modoIndAdhoc=null
+
+https://buscatextual.cnpq.br/buscatextual/busca.do?metodo=forwardPaginaResultados&registros=10;100&query=%28%2Bidx_assunto%3A%28%22dados+de+pesquisa%22%29++%2Bidx_nacionalidade%3Ae%29+or+%28%2Bidx_assunto%3A%28%22dados+de+pesquisa%22%29++%2Bidx_nacionalidade%3Ab+%5E500+%29&analise=cv
+&tipoOrdenacao=null&paginaOrigem=index.do&mostrarScore=true&mostrarBandeira=true&modoIndAdhoc=null
+
+
+https://sig-arq.ufpb.br/arquivos/202310605293225969979e9b885c4976d/RESOLUO_01_2023_ESTGIO_PS_DOUTORADO.pdf
+
+I – solicitação de inscrição; 
+II - carta de aceitação do Supervisor; 
+III - cópia do diploma de Doutorado expedido por instituição nacional ou estrangeira; 
+IV - currículo Lattes atualizado até o momento da candidatura; e
+
+
+
+
