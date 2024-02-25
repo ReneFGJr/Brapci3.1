4b772ac3647cfbcf61069de0141f3b778bddffb6
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-02-25 15:57:29 -0300

    Update news.component.ts

--- angular/BRP/src/app/020_brapci/widget/news/news.component.ts
+++ angular/BRP/src/app/020_brapci/widget/news/news.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { BrapciService } from 'src/app/000_core/010_services/brapci.service';
 
 @Component({
   selector: 'app-brapci-news',
   templateUrl: './news.component.html',
 })
 export class NewsComponent {
-  label_news:string = 'Novidades';
+  label_news: string = 'Novidades';
+  text: string = ''
+  result: Array<any> | any = []
+
+  constructor(
+    private brapciService: BrapciService
+  ) {}
+
+  ngOnInit() {
+      let dt:Array<any> | any = {}
+      let url='brapic/news'
+      this.brapciService.api_post(url,dt).subscribe((res) => {
+        console.log(res)
+      });
+  }
 }
