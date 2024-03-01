b1f7e840504eb2a4fa89e4c76ddb526c2c35e13f
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-01 07:52:37 -0300

    Update Index.php

--- app/Models/ElasticSearch/Index.php
+++ app/Models/ElasticSearch/Index.php
@@ -50,7 +50,7 @@ class Index extends Model
 
 		switch ($d1) {
 			case 'problems':
-				$sx .= $this->problems($type,$d3);
+				$sx .= $this->problems($type, $d3);
 				break;
 			case 'update_index':
 				$url = PATH . '/admin/elastic/update/';
@@ -109,37 +109,35 @@ class Index extends Model
 				$sx .= '</table>';
 				break;
 			default:
-				$sx .= bsmessage("Command not found '".$d1."'");
+				$sx .= bsmessage("Command not found '" . $d1 . "'");
 				break;
 		}
 		return $sx;
 	}
 
-	function problems($d1,$d2)
-		{
-			$RDF = new \App\Models\Rdf\RDF();
-			$Register = new \App\Models\ElasticSearch\Register();
-			$dt = $Register->where('year < 1950')->findAll(5);
-			$sx = 'Total '.count($dt).' problems with year';
-			$sx .= '<ol>';
-			foreach($dt as $id=>$line)
-				{
-					if ($line['ldl_title'] == '::Sem ´título::')
-						{
-							$ida = $line['article_id'];
-							$RDF->exclude($ida);
-							$Register->where('id',$line['id'])->delete();
-							echo h("DELETADO");
-						}
-					pre($line,false);
-					$url = $line['article_id'];
-					$sx .= '<li>';
-					$sx .= anchor('v/'.$line['article_id']) . ' - ' .$line['type'];
-					$sx .= '</li>';
-				}
-			$sx .= '</ol>';
-			return $sx;
+	function problems($d1, $d2)
+	{
+		$RDF = new \App\Models\Rdf\RDF();
+		$Register = new \App\Models\ElasticSearch\Register();
+		$dt = $Register->where('year < 1950')->findAll(5);
+		$sx = 'Total ' . count($dt) . ' problems with year';
+		$sx .= '<ol>';
+		foreach ($dt as $id => $line) {
+			if ($line['ldl_title'] == '::Sem ´título::') {
+				$ida = $line['article_id'];
+				$RDF->exclude($ida);
+				$Register->where('id', $line['id'])->delete();
+				echo h("DELETADO");
+			}
+			pre($line, false);
+			$url = $line['article_id'];
+			$sx .= '<li>';
+			$sx .= anchor('v/' . $line['article_id']) . ' - ' . $line['type'];
+			$sx .= '</li>';
 		}
+		$sx .= '</ol>';
+		return $sx;
+	}
 
 	function paginations($dt)
 	{
@@ -202,33 +200,31 @@ class Index extends Model
 		return $sx;
 	}
 
-	function show_works($dt,$type)
-		{
-			$sx = '';
-			if (isset($dt['error']))
-				{
-					$sx .= $dt['error'];
-					return $sx;
-				}
-
-			switch($type)
-				{
-					case 'book':
-						$sx .= $this->show_works_books($dt);
-						break;
-					case 'benancib':
-						$sx .= $this->show_works_benancib($dt);
-						break;
-					case 'journal':
-						$sx .= $this->show_works_article($dt);
-						break;
-					default:
-						$sx .= $this->show_works_benancib($dt);
-						break;
-				}
-
-				return $sx;
+	function show_works($dt, $type)
+	{
+		$sx = '';
+		if (isset($dt['error'])) {
+			$sx .= $dt['error'];
+			return $sx;
 		}
+
+		switch ($type) {
+			case 'book':
+				$sx .= $this->show_works_books($dt);
+				break;
+			case 'benancib':
+				$sx .= $this->show_works_benancib($dt);
+				break;
+			case 'journal':
+				$sx .= $this->show_works_article($dt);
+				break;
+			default:
+				$sx .= $this->show_works_benancib($dt);
+				break;
+		}
+
+		return $sx;
+	}
 
 	function show_works_books($dt)
 	{
@@ -238,8 +234,8 @@ class Index extends Model
 		$RDF = new \App\Models\Rdf\RDF();
 		$COVER = new \App\Models\Base\Cover();
 		$sx = '';
-		if ($dt['total']==0) {
-			$sx .= bs(bsc(bsmessage(lang('brapci.nothing_locate_found'),3)));
+		if ($dt['total'] == 0) {
+			$sx .= bs(bsc(bsmessage(lang('brapci.nothing_locate_found'), 3)));
 			return $sx;
 		}
 
@@ -251,30 +247,29 @@ class Index extends Model
 		for ($r = 0; $r < count($dt['works']); $r++) {
 			$line = $dt['works'][$r];
 
-			switch($line['type'])
-				{
-					case 'Book':
-						$bg = 'btn-primary';
-						$cover = '<img src="' . URL . '/' . $COVER->book($line['id']) . '" class="img-fluid img-thumb" >';
-						$cover = '<a href="' . PATH . '/books/v/' . $line['id'] . '">' . $cover . '</a>';
-						break;
-					case 'BookChapter':
-						$bg = 'bg-brapcilivros';
-						$cover = '<img src="' . URL . '/' . $RDF->c($line['id'],'cover') . '" class="img-fluid" >';
-						$cover = '<a href="' . PATH . '/books/v/' . $line['id'] . '">' . $cover . '</a>';
-						break;
-				}
+			switch ($line['type']) {
+				case 'Book':
+					$bg = 'btn-primary';
+					$cover = '<img src="' . URL . '/' . $COVER->book($line['id']) . '" class="img-fluid img-thumb" >';
+					$cover = '<a href="' . PATH . '/books/v/' . $line['id'] . '">' . $cover . '</a>';
+					break;
+				case 'BookChapter':
+					$bg = 'bg-brapcilivros';
+					$cover = '<img src="' . URL . '/' . $RDF->c($line['id'], 'cover') . '" class="img-fluid" >';
+					$cover = '<a href="' . PATH . '/books/v/' . $line['id'] . '">' . $cover . '</a>';
+					break;
+			}
 
-			$TAG = '<span class="btn small pt-0 pb-0 '.$bg.'">'.$line['type']. '</span><br>';
+			$TAG = '<span class="btn small pt-0 pb-0 ' . $bg . '">' . $line['type'] . '</span><br>';
 			$score = '<br><span style="font-size: 0.6em;">(Score: ' . number_format($line['score'], 3, '.', ',') . ')</span>';
 			//$score .= '<br>'.$line['id'];
 			$tb = '<table width="100%">';
 			$tb .= '<tr>';
-			$tb .= '<td width="20%" class="p-2">'.$cover.'</td>';
-			$tb .= '<td width="80%" valign="top" class="p-2">' . $TAG . $RDF->c($line['id']) . $score. '</td>';
+			$tb .= '<td width="20%" class="p-2">' . $cover . '</td>';
+			$tb .= '<td width="80%" valign="top" class="p-2">' . $TAG . $RDF->c($line['id']) . $score . '</td>';
 			$tb .= '</tr>';
 			$tb .= '</table>';
-			$sx .= bsc($tb,6);
+			$sx .= bsc($tb, 6);
 		}
 		$sx = bs($sx);
 		return $sx;
@@ -301,22 +296,21 @@ class Index extends Model
 		for ($r = 0; $r < count($dt['works']); $r++) {
 			$line = $dt['works'][$r];
 			$sz = 11;
-			if (isset($line['jnl']))
-				{
-					$cover = '<img src="'.$Cover->cover($line['jnl']). '" class="img-fluid mb-3 text-end" >';
-					$sz--;
-					$sx .= bsc($cover,1);
-				}
+			if (isset($line['jnl'])) {
+				$cover = '<img src="' . $Cover->cover($line['jnl']) . '" class="img-fluid mb-3 text-end" >';
+				$sz--;
+				$sx .= bsc($cover, 1);
+			}
 
 			if (isset($line['jnl'])) {
 				$year = $line['year'];
 			}
 			$txt = '';
 			$txt .= $MARK->mark($line['id']);
-			$txt .= '<a href="'.PATH.'/v/'.$line['id'].'" class="href">'.$RDF->c($line['id']).'</a>'.cr();
-			$txt .= ' <sup>(Score: ' . number_format($line['score'], 3, '.', ','). ')</sup>';
-			$sx .= bsc($txt , $sz, 'mb-3');
-			$sx .= bsc($year,1,'text-end');
+			$txt .= '<a href="' . PATH . '/v/' . $line['id'] . '" class="href">' . $RDF->c($line['id']) . '</a>' . cr();
+			$txt .= ' <sup>(Score: ' . number_format($line['score'], 3, '.', ',') . ')</sup>';
+			$sx .= bsc($txt, $sz, 'mb-3');
+			$sx .= bsc($year, 1, 'text-end');
 		}
 		$sx = bs($sx);
 		return $sx;
@@ -343,86 +337,81 @@ class Index extends Model
 		return $sx;
 	}
 
-	function show_error($d1,$d2,$d3)
-		{
-			$RDF = new \App\Models\RDF2\RDF();
-			$sx = '';
-			switch($d1)
-				{
-					case 'year_without':
-						$sx .= h(msg('brapci.year_without'),3);
-						$API = new \App\Models\ElasticSearch\Register();
-						$sx .= $API->whithout_year();
-					break;
-					case 'erros':
-						$API = new \App\Models\ElasticSearch\Register();
-						switch($d2)
-							{
-								case 'pdf':
-									$sx .= h('PDF Erros',2);
-									$dt = $API->where('PDF',0)->findAll();
-									$sx .= h('Total: ' . count($dt), 6);
-									foreach($dt as $id=>$line)
-										{
-											$link = '<a href="'.
-												'https://brapci.inf.br/#/v/'. $line['ID'].
-												'" target="_blank">';
-											$linka = '</a>';
-											$name = $line['CLASS'].': '.$line['ID'];
-											$name .= ' - '.$line['LEGEND'];
-											$sx .= '<li>'.$link.$name.$linka.'</li>'.cr();
-										}
-								break;
-							}
-					break;
+	function show_error($d1, $d2, $d3)
+	{
+		$RDF = new \App\Models\RDF2\RDF();
+		$sx = '';
+		switch ($d1) {
+			case 'year_without':
+				$sx .= h(msg('brapci.year_without'), 3);
+				$API = new \App\Models\ElasticSearch\Register();
+				$sx .= $API->whithout_year();
+				break;
+			case 'erros':
+				$API = new \App\Models\ElasticSearch\Register();
+				switch ($d2) {
+					case 'pdf':
+						$sx .= h('PDF Erros', 2);
+						$dt = $API->where('PDF', 0)->findAll();
+						$sx .= h('Total: ' . count($dt), 6);
+						foreach ($dt as $id => $line) {
+							$link = '<a href="' .
+								'https://brapci.inf.br/#/v/' . $line['ID'] .
+								'" target="_blank">';
+							$linka = '</a>';
+							$name = $line['CLASS'] . ': ' . $line['ID'];
+							$name .= ' - ' . $line['LEGEND'];
+							$sx .= '<li>' . $link . $name . $linka . '</li>' . cr();
+						}
+						break;
 				}
-			$sx = bs(bsc($sx,12));
-			return $sx;
+				break;
 		}
+		$sx = bs(bsc($sx, 12));
+		return $sx;
+	}
+	/*************************************** EXPORT TO CSV */
+	function export($type)
+	{
+		$Register = new \App\Models\ElasticSearch\Register();
+		$user = get('user');
+		$row = explode(',', get('row'));
+		$dd = [];
+		$dd['type'] = $type;
+		$dd['user'] = $user;
+		$dd['item'] = $row;
 
-		function export($type)
-			{
-				$Register = new \App\Models\ElasticSearch\Register();
-				$user = get('user');
-				$row = explode(',',get('row'));
-				$dd = [];
-				$dd['type'] = $type;
-				$dd['user'] = $user;
-				$dd['item'] = $row;
-
-				$cp = '*';
-				$Register->select($cp);
-				/*
+		$cp = '*';
+		$Register->select($cp);
+		/*
 				foreach($row as $id=>$line)
 					{
 						$Register->Orwhere('ID',$line);
 					}
 				*/
-				$dt = $Register->findALl(10);
+		$dt = $Register->findALl(10);
 
-				$sx = '';
-				$sep = ';';
-				$fld = ['ID','CLASS','YEAR','AUTHORS','TITLE','SESSIONS'];
-				foreach($dt as $i=>$line)
-					{
-						foreach($fld as $name)
-							{
-								$vlr = $line[$fld];
-								if ($vlr == sonumero($vlr))
-									{
-										$sx .= $line[$fld] . $sep;
-									} else {
-										$sx .= '"'.$line[$fld].'"' . $sep;
-									}
-
-							}
-						$sx .= chr(13);
+		$sx = '';
+		$sep = ';';
+		$fld = ['ID', 'CLASS', 'YEAR', 'AUTHORS', 'TITLE', 'SESSIONS'];
+		foreach ($dt as $i => $line) {
+			foreach ($fld as $name) {
+				if (isset($line[$fld])) {
+					$vlr = $line[$fld];
+					if ($vlr == sonumero($vlr)) {
+						$sx .= $line[$fld] . $sep;
+					} else {
+						$sx .= '"' . $line[$fld] . '"' . $sep;
 					}
-
-				header("Content-type: text/csv");
-				echo $sx;
-				exit;
+				} else {
+					$sx .= 'null' . $sep;
+				}
 			}
+			$sx .= chr(13);
+		}
 
-
+//		header("Content-type: text/csv");
+		echo $sx;
+		exit;
+	}
 }
