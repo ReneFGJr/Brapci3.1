8e4723011698c33ed4abb47d5e8e73719d292782
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-02-29 21:14:38 -0300

    Update Index.php

--- app/Models/ElasticSearch/Index.php
+++ app/Models/ElasticSearch/Index.php
@@ -398,7 +398,21 @@ class Index extends Model
 					}
 				$dt = $Register->findALl(10);
 				$dd['data'] = $dt;
-				echo json_encode($dd);
+
+				$sx = '';
+				$sep = ';';
+				foreach($dt as $i=>$line)
+					{
+						$sx .= $line['ID'] . $sep;
+						$sx .= $line['CLASS'].$sep;
+						$sx .= $line['YEAR'] . $sep;
+						$sx .= $line['TITLE'] . $sep;
+						$sx .= $line['AUTHORS'] . $sep;
+						$sx .= $line['SESSION'] . $sep;
+						$sx .= chr(13);
+					}
+				header("Content-type: text/csv");
+				echo $sx;
 				exit;
 			}
 
