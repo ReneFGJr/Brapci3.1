c4d79aa5e45c8e9a524049effd4421d15f8b51ac
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-18 19:35:11 -0300

    Update RDFmetadata.php

--- app/Models/RDF2/RDFmetadata.php
+++ app/Models/RDF2/RDFmetadata.php
@@ -218,7 +218,7 @@ class RDFmetadata extends Model
             $dr['coauthors'] = $coath;
 
             /************ Grafico Coautorias */
-            krsort($coauthors);
+            arsort($coauthors);
             pre($coauthors);
             $graph = [];
             $graph['labels'] = ['A1','B1','C1','D1','E1'];
