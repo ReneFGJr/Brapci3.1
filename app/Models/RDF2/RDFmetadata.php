9de42f7b278da60a286c95e99d872da40fc28e07
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-19 18:12:21 -0300

    Update RDFmetadata.php

--- app/Models/RDF2/RDFmetadata.php
+++ app/Models/RDF2/RDFmetadata.php
@@ -225,6 +225,7 @@ class RDFmetadata extends Model
 
             $dr['chart_years'] = [];
             $dr['chart_years']['labels'] = $prod_label;
+            $dt['chart_years']['data'] = $ds;
 
 
             /********** Ordena coauthors */
