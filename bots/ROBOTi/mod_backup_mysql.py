080a61164501b89b2aba790a4fee8affcdf223ac
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-20 12:42:24 -0300

    Update mod_backup_mysql.py

--- bots/ROBOTi/mod_backup_mysql.py
+++ bots/ROBOTi/mod_backup_mysql.py
@@ -15,11 +15,12 @@ def main():
 
     for data in row:
         print(data[0])
+        db = data[0]
 
         #cmd = "mysqldump -u "+config['user']+ " -p "+config['password']+" "+data[0]+" > /backup/"+data[0]+"_"+date+".sql"
-        cmd = "mysqldump "+str(data[0])+" > /backup/"+str(data[0])+"_20240101.sql"
+        cmd = f"mysqldump {db} > /backup/{db}_{date}.sql"
 
-        result = subprocess.run([sys.executable, "-c", cmd], capture_output=True, text=True, timeout=60)
+        result = subprocess.run([sys.executable, "-c", cmd], capture_output=True, shell=True, text=True, timeout=60)
         print("stdout:", result.stdout)
         print("stderr:", result.stderr)
 
