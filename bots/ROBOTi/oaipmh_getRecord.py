7e50f39d4f95e8987091545432eb0a621b4f0d14
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-20 09:43:15 -0300

    Update oaipmh_getRecord.py

--- bots/ROBOTi/oaipmh_getRecord.py
+++ bots/ROBOTi/oaipmh_getRecord.py
@@ -185,7 +185,7 @@ def process(rg):
                         try:
                             lg = mod_language.check(reg['@xml:lang'])
                         except Exception as e:
-                            print("+++++++++++++++++++"+reg)
+                            print("String Source: "+reg)
                         else:
                             sourceName = mod_issue.decode(reg['#text'],lg,source)
                             dc_source = sourceName
@@ -193,7 +193,7 @@ def process(rg):
                     try:
                         lg = mod_language.check(TIT['@xml:lang'])
                     except Exception as e:
-                         print("+++++++++++++++++++"+reg)
+                         print("String Source: "+TIT)
                     else:
                         sourceName = mod_issue.decode(TIT['#text'],lg,source)
                         dc_source = sourceName
