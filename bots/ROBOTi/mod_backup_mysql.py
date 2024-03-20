347e8a8c0cf3c34d86c047426818400352695338
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-20 12:28:33 -0300

    A

---
+++ bots/ROBOTi/mod_backup_mysql.py
@@ -0,0 +1,23 @@
+import database
+import subprocess
+import sys
+import env
+
+def main():
+    config = env.db()
+    #print(config)
+    #return 0
+
+    qr = "SHOW DATABASES;"
+    row = database.query(qr)
+
+    for data in row:
+        print(data[0])
+
+        cmd = "mysqldump -u "+config['user']+ " -p "+config['password']+" "+data[0]+" > /backup/"+data[0]+"_20240101.sql"
+
+        result = subprocess.run([sys.executable, "-c", cmd], capture_output=True, text=True, timeout=60)
+        print("stdout:", result.stdout)
+        print("stderr:", result.stderr)
+
+main()
