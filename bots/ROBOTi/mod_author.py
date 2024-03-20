799fe4bce0b5c8c2fa9a4859c0d35841110a98f1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-20 08:56:08 -0300

    Update mod_author.py

--- bots/ROBOTi/mod_author.py
+++ bots/ROBOTi/mod_author.py
@@ -15,6 +15,8 @@ def check_duplicate():
     qr += " and id_cc = cc_use "
     qr += " order by n_name, id_cc"
 
+    print(qr)
+
     row = database.query(qr)
     lastName = 'x'
     for reg in row:
