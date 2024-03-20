7c2605310d2705f46bc41fa5f6ebfe0566ed0a18
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-19 21:56:45 -0300

    Update Export.php

--- app/Models/Base/Export.php
+++ app/Models/Base/Export.php
@@ -364,6 +364,7 @@ class Export extends Model
         /********************************** CRIA METADADOS EXPORTACAO */
         /**************************************************************/
         $limit = 1000;
+        $offset = 0;
         $sx .= "<br>OFFSET: $offset - LIMIT $limit ";
         $sx .= $this->export_data($class, $type, $offset, $limit);
 
