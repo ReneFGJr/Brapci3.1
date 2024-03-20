da9de5ee80e4973d184ca3a8744eca2cc8631512
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-20 12:47:36 -0300

    Update mod_backup_mysql.py

--- bots/ROBOTi/mod_backup_mysql.py
+++ bots/ROBOTi/mod_backup_mysql.py
@@ -9,20 +9,20 @@ def main():
     #print(config)
     #return 0
     date = datetime.date.today()
-    date = date.strftime('%Y%m%d')
+    date = date.strftime('%Y-%m-%d')
 
     qr = "SHOW DATABASES;"
     row = database.query(qr)
 
     for data in row:
-        print(data[0])
+        print("============"+data[0])
         db = data[0]
 
         #cmd = "mysqldump -u "+config['user']+ " -p "+config['password']+" "+data[0]+" > /backup/"+data[0]+"_"+date+".sql"
         cmd = f"mysqldump {db} > /backup/{db}_{date}.sql"
 
         #result = subprocess.run([sys.executable, "-c", cmd], capture_output=True, shell=True, text=True, timeout=60)
-        result = subprocess.run([sys.executable, "-c", cmd], capture_output=True, text=True, timeout=60)
+        result = subprocess.run([sys.executable, "-c", cmd], capture_output=True, shell=True, text=True, timeout=60)
         print("stdout:", result.stdout)
         print("stderr:", result.stderr)
 
