e9b6479ea52103fa2952b9e523afd985d8c5262c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-21 12:38:09 -0300

    Update RDFmetadata.php

--- app/Models/RDF2/RDFmetadata.php
+++ app/Models/RDF2/RDFmetadata.php
@@ -370,7 +370,7 @@ class RDFmetadata extends Model
             $RDFconcept = new \App\Models\RDF2\RDFconcept();
             $dt = $RDFconcept
                 ->select('n_name')
-                ->join('brapci_rdf.brapci_literal', 'id_n = cc_pref_term ')
+                ->join('brapci_rdf.rdf_literal', 'id_n = cc_pref_term ')
                 ->where('cc_use',$ID)
                 ->groupBy('n_name')
                 ->findAll();
