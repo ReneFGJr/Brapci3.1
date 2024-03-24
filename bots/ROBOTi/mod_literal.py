97abbd0872e432e1da5167ab5441a7f74e4acd3c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-24 11:25:10 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -13,7 +13,7 @@ def check_double_name():
         id = ln[0]
         qru = f"update brapci_rdf.rdf_literal set n_name = '{name}' where id_n = {id}"
         #database.update(qru)
-        print(name, name1, '|', name2)
+        print(name, name1, '|', name2,' | sz',sz)
 
 def check_trim():
     qr = f"select id_n,n_name from brapci_rdf.rdf_literal where (n_name like ' %')"
