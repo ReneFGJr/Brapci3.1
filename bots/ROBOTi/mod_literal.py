dfe0593eed2996403c8b060d7c810bb507f7a3a9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-03-24 11:47:36 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -4,7 +4,7 @@ def check_double_name():
     qr = f"select id_n,n_name, length(n_name)"
     qr += "from brapci_rdf.rdf_literal "
     #qr += "where (n_name <> '')"
-    qr += "where (n_name like '%Ana Maria Mielnickuz%')"
+    qr += "where (n_name like '%Ana Maria Miel%')"
     row = database.query(qr)
     for ln in row:
         name = ln[1]
