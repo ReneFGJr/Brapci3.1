7b36e98de249ee1e6dff32a2a60298a377a54a4f
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-01 07:12:31 -0300

    A

--- angular/BRP/src/app/930_tools/page/amostra/price.component.ts
+++ angular/BRP/src/app/930_tools/page/amostra/price.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-price',
-  templateUrl: './price.component.html'
+  templateUrl: './price.component.html',
 })
 export class PriceComponent {
+  constructor(private fb: FormBuilder) {}
+  myForm: FormGroup | any;
+  amostra: number = 0;
+  assimetria = '/assets/img/amostra_assimetria.png';
 
+  ngOnInit() {
+    this.myForm = this.fb.group({
+      populacao: ['1000', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if (this.myForm.value['populacao'] != '') {
+      let populacao = this.myForm.value['populacao'];
+      this.amostra = Math.sqrt(populacao);
+    }
+  }
 }
