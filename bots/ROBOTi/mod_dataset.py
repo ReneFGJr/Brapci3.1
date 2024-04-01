3943a14d34072da01d7ab79637b769dd50c31b35
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-01 18:05:31 -0300

    Update mod_dataset.py

--- bots/ROBOTi/mod_dataset.py
+++ bots/ROBOTi/mod_dataset.py
@@ -1,10 +1,17 @@
 import database
 
 def check_duplicate():
-    qr = "select * from brapci_elastic.dataset order by TITLE, AUTHORS "
+    qr = "select JOURNAL, TITLE, AUTHORS, ID from brapci_elastic.dataset order by JOURNAL, TITLE, AUTHORS, ID "
     row = database.query(qr)
 
     last = ''
 
     for item in row:
-        print(item)
+        name = item[1]+item[2]
+        ID = item[3]
+
+        if (name == last):
+            {
+                print(ID,name)
+            }
+        last = name
