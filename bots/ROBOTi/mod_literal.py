9f3643f0f46d5cb6e3ad011ddfab9ffce28e794d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-02 16:15:37 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -29,7 +29,7 @@ def check_double_name():
 
 def check_title():
     prop = 30
-    qr = f"select * from brapci_rdf.rdf_data "
+    qr = f"select id_n, n_name from brapci_rdf.rdf_data "
     qr += " inner join brapci_rdf.rdf_literal on id_n = d_literal"
     qr += " where (n_lang = 'nn')"
     qr += f" and d_p = {prop} "
