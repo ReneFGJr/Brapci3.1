fabab17dd6f1c31ba208055080d43430ebcdf97a
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-03 17:17:09 -0300

    A

--- bots/ROBOTi/TRADUCTOR.py
+++ bots/ROBOTi/TRADUCTOR.py
@@ -1,6 +1,8 @@
 import mod_title
+import mod_PDF
 
 print("TRADUTOR 1.0")
 ID = 257684
 
-mod_title.title(ID)
+#mod_title.title(ID)
+mod_PDF.title(ID)
---
+++ bots/ROBOTi/mod_PDF.py
@@ -0,0 +1,30 @@
+import fitz  # Importa PyMuPDF
+import re
+import database
+import mod_data
+import mod_class
+import mod_literal
+import mod_GoogleTranslate
+
+def title(ID):
+    prop = mod_class.getClass("hasFileStorage")
+    qr = "select n_name, n_lang from brapci_rdf.rdf_data "
+    qr += "inner join brapci_rdf.rdf_literal ON d_literal = id_n"
+    qr += f" where d_r1 = {ID} and d_p = {prop}"
+    row = database.query(qr)
+
+    print(row)
+
+# Função para extrair texto do PDF
+def extrair_texto_pdf(caminho_arquivo):
+    texto = ""
+    with fitz.open(caminho_arquivo) as doc:
+        for pagina in doc:
+            texto += pagina.get_text()
+
+    caminho_arquivo_txt = caminho_arquivo.replace('.pdf','.txt')
+
+    with open(caminho_arquivo_txt, 'w', encoding='utf-8') as arquivo:
+        arquivo.write(texto)
+
+    return texto
