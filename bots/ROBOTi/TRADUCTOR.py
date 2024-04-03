1e5b358c3e2626f92f188e9860b27332c263cf72
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-03 18:00:03 -0300

    Update TRADUCTOR.py

--- bots/ROBOTi/TRADUCTOR.py
+++ bots/ROBOTi/TRADUCTOR.py
@@ -1,10 +1,14 @@
 import mod_translate_title
 import mod_translate_abstract
 import mod_PDF
+import sys
 
 print("TRADUTOR 1.0")
-ID = 257789
 
-mod_translate_title.process(ID)
-mod_translate_abstract.process(ID)
-mod_PDF.convert(ID)
+if (len(sys.argv) > 1):
+    parm = sys.argv
+    ID = parm[1]
+
+    mod_translate_title.process(ID)
+    mod_translate_abstract.process(ID)
+    mod_PDF.convert(ID)
