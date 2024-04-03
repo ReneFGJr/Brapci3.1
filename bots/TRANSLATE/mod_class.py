ab525a51d017729784ca8ee6a44601f6a801545e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-03 15:55:30 -0300

    A

--- bots/TRANSLATE/mod_class.py
+++ bots/TRANSLATE/mod_class.py
@@ -1,18 +1,14 @@
 import database
 import mod_class
 
-def getClass(cl):
-    Class = mod_class.getClass('hasTitle')
-
+def getClass(Class):
     qr = "select * from brapci_rdf.rdf_class "
     qr += f" where (c_class  = '{Class}')"
-
-    print(qr)
 
     row = database.query(qr)
     if row != []:
         ID = row[0][0]
     else:
-        print(f"Class {cl} not found")
+        print(f"Class {Class} not found")
         quit()
     return ID
--- bots/TRANSLATE/mod_title.py
+++ bots/TRANSLATE/mod_title.py
@@ -1,5 +1,7 @@
 import database
+import mod_class
 
 def title(ID):
-    prop =
-    qr = "select * from brapci_rdf.rdf_data where d_r1 = {ID} and d_p = {prop}"
+    prop = mod_class.getClass("hasTitle")
+    qr = f"select * from brapci_rdf.rdf_data where d_r1 = {ID} and d_p = {prop}"
+    print(qr)
