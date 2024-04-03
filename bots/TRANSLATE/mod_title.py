b6128d4cb568c75c88b39dc93bfd968753c82c50
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-03 15:59:48 -0300

    Update mod_title.py

--- bots/TRANSLATE/mod_title.py
+++ bots/TRANSLATE/mod_title.py
@@ -3,8 +3,8 @@ import mod_class
 
 def title(ID):
     prop = mod_class.getClass("hasTitle")
-    qr = f"select n_name, n_lang from brapci_rdf.rdf_data "
+    qr = "select n_name, n_lang from brapci_rdf.rdf_data "
     qr = "innher join brapci_rdf.rdf_data d_literal = id_n"
-    qr += " where d_r1 = {ID} and d_p = {prop}"
+    qr += f" where d_r1 = {ID} and d_p = {prop}"
     row = database.query(qr)
     print(row)
