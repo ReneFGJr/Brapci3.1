ca4030d5b6900083fd855f27d64d909c809892f1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-05 08:45:00 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -67,13 +67,14 @@ def check_title():
         database.update(qu)
 
 def check_trim():
-    qr = f"select id_n,n_name from brapci_rdf.rdf_literal where (n_name like ' %') or (n_name like '%  %')"
+    qr = f"select id_n,n_name from brapci_rdf.rdf_literal where (n_name like ' %') or (n_name like '%  %') or (n_name like '% :%')"
     row = database.query(qr)
     dd=0
     for ln in row:
         name = ln[1]
         name = name.strip().capitalize()
         name = name.replace('  ',' ')
+        name = name.replace(' :',':')
         id = ln[0]
         qru = f"update brapci_rdf.rdf_literal set n_name = '{name}' where id_n = {id}"
         database.update(qru)
