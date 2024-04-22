60b5b152419795e13564eaf08a75e1d224d12b25
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-22 11:47:50 -0300

    Update mod_cited.py

--- bots/ROBOTi/mod_cited.py
+++ bots/ROBOTi/mod_cited.py
@@ -2,6 +2,7 @@ import database
 
 def cited():
     qr = "select * from brapci_rdf.rdf_literal "
+    qr += "inner join brapci_rdf.rdf_data ON id_n = d_literal "
     qr += "where n_name like '%/*ref*/%'"
     row = database.query(qr)
 
