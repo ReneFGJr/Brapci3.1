e714f782ba6a006dc46e79dc7e9746e7c47df8f1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-26 15:11:37 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -48,6 +48,7 @@ def check_utf8():
             if dados is not None:
                 #dados = dados.encode('utf-8')
                 dados = dados.encode('utf-8')
+                dados = dados.decode('utf-8')
                 if (dados != dados2):
                     print("====================== UTF8")
                     print(dados)
