2b849d23e7b50a430a815e05cf0936db2e5c8278
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-26 14:28:18 -0300

    Update mod_author.py

--- bots/ROBOTi/mod_author.py
+++ bots/ROBOTi/mod_author.py
@@ -76,9 +76,8 @@ def check_remissiva():
 
     qr = "SELECT id_cc, cc_use, id_n, n_name FROM brapci_rdf.rdf_concept "
     qr += "inner join brapci_rdf.rdf_literal ON cc_pref_term = id_n "
-    qr += "inner join brapci_rdf.rdf_data ON ((d_r1 = cc_use) and (d_literal = 0)) "
+    qr += "inner join brapci_rdf.rdf_data ON ((d_r1 = id_cc) and (d_literal = 0)) "
     qr += "WHERE (cc_use <> id_cc) and (d_r2 > 0) and (cc_use <> 0) "
-    qr += " and (d_r1 = id_cc or d_r2 = id_cc)"
     print(qr)
     quit()
     row = database.query(qr)
