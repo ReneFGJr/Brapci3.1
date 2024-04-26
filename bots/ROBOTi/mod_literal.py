b3bec053b1321bcac6fa34f7c2e10d8edbaf9fe8
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-26 16:15:25 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -52,7 +52,7 @@ def check_utf8():
         dados = dados.replace('ã³','ó')
         print(dados)
         print(dados.encode('utf-8'))
-        print(dados.decode('utf-8'))
+        print(dados.encode('utf-8'))
         quit()
 
         dados4 = dados.encode('utf-8')
