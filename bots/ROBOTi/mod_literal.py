ba95206c7af4a5ce844cdd039a69c3446bc20bbc
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-04-27 10:30:03 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -66,6 +66,9 @@ def check_utf8():
             dados = str(dados)
             ok = 0
             if '\\x' in dados:
+                dados = dados.replace('\\x93','-')
+                dados = dados.replace('\\xa1','y')
+
                 dados = dados.replace('\\xe3\\xb3','ó')
                 dados = dados.replace('\\xe3\\x80','.')
                 dados = dados.replace('\\xe3\\x83','ã')
@@ -112,6 +115,8 @@ def check_utf8():
                 dados = dados.replace(' \\xb7','')
                 dados = dados.replace('á\\x9a','ú')
                 dados = dados.replace('á\\xb6','ö')
+
+
 
 
                 dados = dados.replace('i\\xad','í')
