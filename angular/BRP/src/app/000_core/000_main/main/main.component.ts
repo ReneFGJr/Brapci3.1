6cb1b15ece616a23bc3637f11e8fad2296676538
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-14 11:14:48 -0300

    A

--- angular/BRP/src/app/000_core/000_main/main/main.component.ts
+++ angular/BRP/src/app/000_core/000_main/main/main.component.ts
@@ -7,8 +7,15 @@ import { Component } from '@angular/core';
   templateUrl: 'main.component.html',
 })
 export class MainComponent {
-  public header = { title: 'Brapci - Base de Dados em Ciência da Informação' };
+  public header = { title: 'Base de Dados em Ciência da Informação' };
   public cookie: Array<any> | any = [];
+  public data: Array<any> | any = [
+    {
+      description:
+        'A Brapci, sigla para Base de Dados em Ciência da Informação, é uma plataforma digital brasileira dedicada à coleta, preservação e ao acesso de literatura científica na área de Ciência da Informação. Ela abrange uma ampla gama de publicações, incluindo artigos de periódicos, trabalhos de eventos, livros e capítulos de livros, principalmente de fontes brasileiras e américa latina.',
+      keywords: 'Brapci; Arquivologia; Biblioteconomia; Ciência da Informação; Base de Dados'
+    },
+  ];
   public status_ok: number = 0;
   public status_message: string = 'Carragando...';
 
@@ -16,14 +23,14 @@ export class MainComponent {
 
   ngOnInit() {
     console.log('Starting Services');
-      this.brapciService.api_post('status').subscribe(
-        (res) => {
-          this.status_ok = 1;
-        },
-        (error) => {
-          console.log(error)
-          this.status_ok = 9
-        }
-      )
+    this.brapciService.api_post('status').subscribe(
+      (res) => {
+        this.status_ok = 1;
+      },
+      (error) => {
+        console.log(error);
+        this.status_ok = 9;
+      }
+    );
   }
 }
