316f1ed48f7ceb0630aa4028fea4abb84b42fc56
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-21 03:15:53 -0300

    A

--- app/Models/Base/Metadata.php
+++ app/Models/Base/Metadata.php
@@ -255,6 +255,8 @@ class Metadata extends Model
             $M['KEYWORDS'] = $keyWD;
             $M['ABSTRACTS'] = $abstC;
 
+            pre($M);
+
             $this->metadata = $M;
             return $this->metadata;
         }
--- app/Models/ElasticSearch/Register.php
+++ app/Models/ElasticSearch/Register.php
@@ -425,7 +425,6 @@ class Register extends Model
 
     function data_convert_elastic($data)
     {
-        pre($data,false);
         $da = array();
         $da['ID'] = $data['ID'];
         $da['json'] = json_encode($data);
