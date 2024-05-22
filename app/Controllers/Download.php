ab7c37ffe0e67c0a076a86d8f59c5100993818de
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-22 16:37:57 -0300

    A

--- app/Controllers/Download.php
+++ app/Controllers/Download.php
@@ -68,8 +68,29 @@ class Download extends BaseController
 
     function download_export($file)
         {
-            $txt = substr($file,-3,3);
-            pre($txt);
+            $ext = substr($file,-3,3);
+            $filePath = '.tmp/export/'.$file;
+
+            switch($ext)
+                {
+                    case 'ris':
+                        header('Access-Control-Allow-Origin: *');
+                        //header("Access-Control-Allow-Headers: Content-Type");
+                        header('Content-Description: File Transfer');
+                        header('Content-Type: application/x-research-info-systems');
+                        header('Content-Disposition: attachment; filename=' . basename($filePath));
+                        header('Expires: 0');
+                        header('Cache-Control: must-revalidate');
+                        header('Pragma: public');
+                        header('Content-Length: ' . filesize($filePath));
+                        ob_clean();
+                        flush();
+                        readfile($filePath);
+                        exit;
+                        break;
+
+                }
+            exit;
         }
 
     function download_01($dt)
