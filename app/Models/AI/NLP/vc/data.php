980639441dcb084d84d70878ddce729774ef90f0
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 11:32:00 -0300

    Update data.php

--- app/Models/AI/NLP/vc/data.php
+++ app/Models/AI/NLP/vc/data.php
@@ -10,8 +10,21 @@ for ($r=1920;$r < date("Y")+10;$r++)
         $vc[' ' . $r . chr(13)] = ' {date:"' . $r . '"}'.chr(13);
 
 
-        $mes = ['jan','fev','mar','abr','maio','jun','jul','ago','set','out','nov','dez'];
-        $mesN = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+        $mes = ['jan','fev','mar','abr','maio','jun','jul','ago','set','out','nov','dez','mai'];
+        $mesN = ['jan'=>'janeiro',
+                'fev'=>'fevereiro',
+                'mar'=>'março',
+                'abr'=>'abril',
+                'maio'=>'maio',
+                'mai' => 'maio',
+                'jun'=>'junho',
+                'jul'=>'julho',
+                'ago'=>'agosto',
+                'set'=>'setembro',
+                'out'=>'outubro',
+                'nov'=>'novembro',
+                'dez'=>'dezembro'
+                ];
         foreach($mes as $m)
             {
                 $vc[' '.$m.'.'] = ' {date:"'. $mesN[$m] .'"} ';
