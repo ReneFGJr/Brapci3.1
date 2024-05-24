09d3e12152eecf470a09ab89499b3ef9a2a547d9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 11:19:12 -0300

    Update autoridade.php

--- app/Models/AI/NLP/vc/autoridade.php
+++ app/Models/AI/NLP/vc/autoridade.php
@@ -8,11 +8,13 @@ $vc['José Simão de Paula Pinto'] = '{person:"José Simão de Paula Pinto"}';
 $vc['Revista da Escola de Biblioteconomia da UFMG'] = '{journal:"Revista da Escola de Biblioteconomia da UFMG"}';
 $vc['Revista de Biblioteconomia & Comunicação'] = '{journal:"Revista de Biblioteconomia & Comunicação"}';
 $vc['Perspectivas em Ciência da Informação'] = '{journal:"Perspectivas em Ciência da Informação"}';
+$vc['Universidade de São Paulo'] = '{org:"Universidade de São Paulo"}';
 
 $vc['Catálogo Coletivo Nacional de Publicações Seriadas'] = '{org:"Catálogo Coletivo Nacional de Publicações Seriadas"}';
 
 
 $vc['Modelizando práticas para a socialização de informações'] = '{titulo}';
+$vc['Plataforma Informacional do Ecossistema Paulista de Inovação'] = '{titulo}';
 
 $vc['Catálogo Coletivo Nacional de Publicações Seriadas'] = '{org:"Catálogo Coletivo Nacional de Publicações Seriadas"}';
 $vc['(CCN)'] = '{org:"Catálogo Coletivo Nacional de Publicações Seriadas"}';
