fdefbb94dd7865eccd8a31272b2d1a027d9b0f36
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 20:50:27 -0300

    Update metodologia.php

--- app/Models/AI/NLP/vc/metodologia.php
+++ app/Models/AI/NLP/vc/metodologia.php
@@ -9,6 +9,7 @@ $vc[chr(13) . 'Abstract:'] = chr(13) . '{RESUMO}' . chr(13);
 
 $vc['Palavras–chave:'] = '{keywords}';
 $vc['Keywords:'] = '{keywords}';
+$vc['Palavras-chave:'] = '{keywords}';
 
 
 $vc[chr(13) . 'Referências' . chr(13)] = chr(13) . '{structure:"Introducao"}' . chr(13);
