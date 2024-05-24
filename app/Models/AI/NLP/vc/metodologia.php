266dd25e10d0b2549cb2c3d27fe27a17d107a924
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 18:36:26 -0300

    Update metodologia.php

--- app/Models/AI/NLP/vc/metodologia.php
+++ app/Models/AI/NLP/vc/metodologia.php
@@ -14,7 +14,11 @@ $vc[chr(13) . 'Introdução' . chr(13)] = chr(13) . '{INTRODUCAO}' . chr(13);
 $vc[chr(13) . '2 Referencial teórico' . chr(13)] = chr(13) . '{REFERENCIAL_TEORICO}' . chr(13);
 $vc[chr(13) . 'Referencial teórico' . chr(13)] = chr(13) . '{REFERENCIAL_TEORICO}' . chr(13);
 
-$vc[chr(13) . '3 Metodologia' . chr(13)] = chr(13) . '{METODOLOGIA}' . chr(13);
+$vc[chr(13) . '3 Metodologia' . chr(13)] = chr(13) . '{structure:"Metodologia"}' . chr(13);
+$vc[chr(13) . '2 METODOLOGIA' . chr(13)] = chr(13) . '{structure:"Metodologia"}' . chr(13);
+
+$vc[chr(13) . '5 CONSIDERAÇÕES FINAIS' . chr(13)] = chr(13) . '{structure:"Considerações Finais"}' . chr(13);
+
 
 $vc['levantamento bibliográfico'] = '{method:"Levantamento Bibliográfico"}';
 ?>
