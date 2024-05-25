1ed3230cd881229b17c96109a375b48d75e7fc78
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 22:07:28 -0300

    Update subject.php

--- app/Models/AI/NLP/vc/subject.php
+++ app/Models/AI/NLP/vc/subject.php
@@ -1,10 +1,3 @@
 <?php
-$vc = [];
-$vc['Brapci'] = '{sigla:"Brapci"}';
-$vc['BRAPCI'] = '{sigla:"Brapci"}';
-$vc['BASE REFERENCIAL DE ARTIGOS DE PERIÓDICOS EM CIÊNCIA DA INFORMAÇÃO'] = '{sigla:"Brapci"}';
-$vc['Open Archives Iniciative Protocol for Metadata Harvesting'] = '{term:"OAI-PMH"}';
-$vc['OAIPMH'] = '{term:"OAI-PMH"}';
-$vc['OAI-PMH'] = '{term:"OAI-PMH"}';
-$vc['Big Data'] = '{term:"BigData"}';
+require('.tmp/vc/subject.php');
 ?>
