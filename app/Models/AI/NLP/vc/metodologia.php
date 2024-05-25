39c79b214f1f04b955a617be0d8fbfca5a21f182
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 21:00:58 -0300

    Update metodologia.php

--- app/Models/AI/NLP/vc/metodologia.php
+++ app/Models/AI/NLP/vc/metodologia.php
@@ -12,9 +12,9 @@ $vc['Keywords:'] = '{keywords}';
 $vc['Palavras-chave:'] = '{keywords}';
 
 
-$vc[chr(13) . 'Referências' . chr(13)] = chr(13) . '{structure:"Introducao"}' . chr(13);
-$vc[chr(13) . 'REFERÊNCIAS' . chr(13)] = chr(13) . '{structure:"Introducao"}' . chr(13);
-$vc[chr(13) . 'REFERÊNCIAS BIBLIOGRÁFICAS' . chr(13)] = chr(13) . '{structure:"Introducao"}' . chr(13);
+$vc[chr(13) . 'Referências' . chr(13)] = chr(13) . '{structure:"Referencia"}' . chr(13);
+$vc[chr(13) . 'REFERÊNCIAS' . chr(13)] = chr(13) . '{structure:"Referencia"}' . chr(13);
+$vc[chr(13) . 'REFERÊNCIAS BIBLIOGRÁFICAS' . chr(13)] = chr(13) . '{structure:"Referencia"}' . chr(13);
 
 $vc[chr(13) . '1 Introdução' . chr(13)] = chr(13) . '{structure:"Introducao"}' . chr(13);
 $vc[chr(13) . '1 INTRODUÇÃO' . chr(13)] = chr(13) . '{structure:"Introducao"}' . chr(13);
@@ -26,6 +26,13 @@ $vc[chr(13) . 'Referencial teórico' . chr(13)] = chr(13) . '{structure:"Referen
 
 $vc[chr(13) . '3 Metodologia' . chr(13)] = chr(13) . '{structure:"Metodologia"}' . chr(13);
 $vc[chr(13) . '2 METODOLOGIA' . chr(13)] = chr(13) . '{structure:"Metodologia"}' . chr(13);
+$vc[chr(13) . '3 METODOLOGIA' . chr(13)] = chr(13) . '{structure:"Metodologia"}' . chr(13);
+$vc[chr(13) . '4 METODOLOGIA' . chr(13)] = chr(13) . '{structure:"Metodologia"}' . chr(13);
+
+$vc[chr(13) . '2 RESULTADOS' . chr(13)] = chr(13) . '{structure:"Resultados"}' . chr(13);
+$vc[chr(13) . '3 RESULTADOS' . chr(13)] = chr(13) . '{structure:"Resultados"}' . chr(13);
+$vc[chr(13) . '4 RESULTADOS' . chr(13)] = chr(13) . '{structure:"Resultados"}' . chr(13);
+$vc[chr(13) . '5 RESULTADOS' . chr(13)] = chr(13) . '{structure:"Resultados"}' . chr(13);
 
 $vc[chr(13) . '5 CONSIDERAÇÕES FINAIS' . chr(13)] = chr(13) . '{structure:"Considerações Finais"}' . chr(13);
 
