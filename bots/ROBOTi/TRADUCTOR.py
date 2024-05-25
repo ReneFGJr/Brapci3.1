fc04611ca32621f80df7a9ce9481155fa3434b6c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-25 10:23:03 -0300

    Update TRADUCTOR.py

--- bots/ROBOTi/TRADUCTOR.py
+++ bots/ROBOTi/TRADUCTOR.py
@@ -2,6 +2,10 @@ import mod_translate_title
 import mod_translate_abstract
 import mod_PDF
 import sys
+import os
+
+diretorio = '/data/Brapci3.1/bots/ROBOTi'
+os.chdir(diretorio)
 
 print("TRADUTOR 1.0")
 
