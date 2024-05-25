e1776d54b61834d5344ac7d038230da0ceeabdf0
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-24 21:53:19 -0300

    Update Export.php

--- app/Models/Base/Export.php
+++ app/Models/Base/Export.php
@@ -142,7 +142,7 @@ class Export extends Model
                     $pref = $line['Pref'];
                     $ID = $line['ID'];
                     $vc['Big Data'] = '{term:"BigData"}';
-                    $sx .= '$vc["'.$term.'"] = "{term:"'.$pref.':'.$ID.'}";'.cr();
+                    $sx .= '$vc["'.$term.'"] = "{term:"'.$pref.':'.$ID.'}";'.chr(13);
                 }
             $sx .= cr().'?>';
             file_put_contents($file,$sx);
