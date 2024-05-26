270a96b2e86f63ce9b37e9654ac5092e9feeec1d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-26 09:36:04 -0300

    Update metodologia.php

--- app/Models/AI/NLP/vc/metodologia.php
+++ app/Models/AI/NLP/vc/metodologia.php
@@ -26,17 +26,6 @@ $vc['GT-8 – Informação e Tecnologia:'] = '{section:"GT-8"}';
 $vc['GT 9 – Museu, Patrimônio e Informação'] = '{section:"GT-9"}';
 $vc['GT 12 – Informação, Estudos Étnico-Raciais, Gênero e Diversidades:'] = '{section:"GT-12"}';
 $vc['GT 11 – Informação & Saúde'] = '{section:"GT-11"}';
-$vc['GT-11 – Informação'] = '{section:"GT-11"}';
-$vc['GESTÃO DA INFORMAÇÃO E TECNOLOGIAS'] = '{xxx}';
-
-
-$vc['gy 4 – Gestão da Informação e do Conhecimento'] = '{section:"GT-4"}';
-$vc['gt-7 – Produção e Comunicação da Informação em Ciência, Tecnologia & Inovação'] = '{section:"GT-7"}';
-$vc['gt-8 – Informação e Tecnologia'] = '{section:"GT-8"}';
-$vc['gt 9 – Museu, Patrimônio e Informação'] = '{section:"GT-9"}';
-$vc['gt 12 - informacao, estudos etnico-raciais, genero e diversidades'] = '{section:"GT-12"}';
-$vc['gt 11 - informacao & saude'] = '{section:"GT-11"}';
-
 
 $vc[chr(13) . 'Referências' . chr(13)] = chr(13) . '{structure:"Referencia"}' . chr(13);
 $vc[chr(13) . 'REFERÊNCIAS' . chr(13)] = chr(13) . '{structure:"Referencia"}' . chr(13);
