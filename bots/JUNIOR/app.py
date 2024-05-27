4fa049cbb38bbc87887d1347808a903fc39f8e95
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-26 21:23:54 -0300

    A

--- bots/JUNIOR/app.py
+++ bots/JUNIOR/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return mod_embening.separa_silabas("Gabriel")
-    return 'Hello, World!'
+    #return mod_embening.separa_silabas("Gabriel")
+    return 'Hello, World-2!'
 
 if __name__ == '__main__':
     app.run()
--- bots/JUNIOR/mod_embening.py
+++ bots/JUNIOR/mod_embening.py
@@ -2,7 +2,5 @@ import pyphen
 import database
 
 def separa_silabas(palavra):
-    return "XXXXXXXXXXXX"
     dic = pyphen.Pyphen(lang='pt_BR')
-    #return dic.inserted(palavra).split('-')
-    return "XX"
+    return dic.inserted(palavra).split('-')
