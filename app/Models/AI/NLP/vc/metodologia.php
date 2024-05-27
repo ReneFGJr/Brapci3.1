f91c2b46c58f3f1189dd631c6e7aea667ef73355
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-27 10:08:57 -0300

    Update metodologia.php

--- app/Models/AI/NLP/vc/metodologia.php
+++ app/Models/AI/NLP/vc/metodologia.php
@@ -35,11 +35,10 @@ $vc['GT-6 - Informação, Educação e Trabalho'] = '{section:"GT-6"}';
 $vc['GT-7 - Produção e Comunicação da Informação em Ciência, Tecnologia & Inovação'] = '{section:"GT-7"}';
 $vc['GT-7 - Produção e Comunicação da Informação em Ciência, Tecnologia &Inovação'] = '{section:"GT-7"}';
 $vc['GT 7 - produção e comunicação da informação em Ciência, Tecnologia e Inovação'] = '{section:"GT-7"}';
-
 $vc['GT 7: Produção e Comunicação da Informação em CT&I'] = '{section:"GT-7"}';
-
 $vc['GT-8 - Informação e Tecnologia'] = '{section:"GT-8"}';
 $vc['GT 9 - Museu, Patrimônio e Informação'] = '{section:"GT-9"}';
+$vc['GT 10 – Informação e Memória'] = '{section:"GT-10"}';
 $vc['GT 12 - Informação, Estudos Étnico-Raciais, Gênero e Diversidades:'] = '{section:"GT-12"}';
 $vc['GT 11 - Informação & Saúde'] = '{section:"GT-11"}';
 
