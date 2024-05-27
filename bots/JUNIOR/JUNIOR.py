5a3c89c3f3d68bd25cfd71e1e10dab8798f09e52
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-26 22:30:07 -0300

    A

--- bots/JUNIOR/JUNIOR.py
+++ bots/JUNIOR/JUNIOR.py
@@ -3,7 +3,11 @@ import mod_embening
 print("JUNIOR AI v0.1.0")
 print("================")
 
-txt = "Rene Faustino Gabriel Junior"
+# Abrindo o arquivo no modo de leitura
+with open('txt/sample.txt', 'r', encoding='utf-8') as arquivo:
+    # Lendo o conteúdo do arquivo
+    txt = arquivo.read()
+
 txt = txt.lower()
 rx = mod_embening.registrar(txt)
 print(rx)
