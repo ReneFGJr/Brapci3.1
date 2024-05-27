e1f001125d86ad7c5922c109966242fd2f3ec2d7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-26 21:16:10 -0300

    Update mod_embening.py

--- bots/JUNIOR/mod_embening.py
+++ bots/JUNIOR/mod_embening.py
@@ -3,4 +3,5 @@ import database
 
 def separa_silabas(palavra):
     dic = pyphen.Pyphen(lang='pt_BR')
-    return dic.inserted(palavra).split('-')
+    #return dic.inserted(palavra).split('-')
+    return "XX"
