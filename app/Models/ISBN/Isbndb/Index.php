6497ea6e92a3c2dfc2e20fc21568fd58ff6d8de3
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-05-31 09:32:09 -0300

    Update Index.php

--- app/Models/ISBN/Isbndb/Index.php
+++ app/Models/ISBN/Isbndb/Index.php
@@ -55,240 +55,225 @@ class Index extends Model
     }
 
     function form()
-        {
-            $isbn = sonumero(get("isbn"));
-            $sx = '';
-            $sx .= form_open();
-            $sx .= form_input('isbn',$isbn,['class'=>'form-control border border-secondary']);
-            $sx .= form_submit('action','pesquisar',['class'=>'btn btn-outline-primary mt-2']);
-            $sx .= form_close();
+    {
+        $isbn = sonumero(get("isbn"));
+        $sx = '';
+        $sx .= form_open();
+        $sx .= form_input('isbn', $isbn, ['class' => 'form-control border border-secondary']);
+        $sx .= form_submit('action', 'pesquisar', ['class' => 'btn btn-outline-primary mt-2']);
+        $sx .= form_close();
 
-            if ($isbn != '')
-                {
-                    $sx .= '<hr>';
-                    $sx .= 'Buscando API';
-                    $sx .= '<hr>';
-                    $dt = $this->search($isbn);
-                    $dta = (array)json_decode($dt);
+        if ($isbn != '') {
+            $sx .= '<hr>';
+            $sx .= 'Buscando API';
+            $sx .= '<hr>';
+            $dt = $this->search($isbn);
+            $dta = (array)json_decode($dt);
 
-                    if ($dta != '')
-                        {
-                            $Cataloging = new \App\Models\Find\Books\Cataloging();
-                            $data = (array)$this->convert($dta);
-                            $sx .= $Cataloging->import($data);
-                        }
-                }
-            return $sx;
+            if ($dta != '') {
+                $Cataloging = new \App\Models\Find\Books\Cataloging();
+                $data = (array)$this->convert($dta);
+                $sx .= $Cataloging->import($data);
+            }
         }
+        return $sx;
+    }
 
     function dimensoesEX($vlr)
-        {
-            $RSP['peso'] = 0;
-            $RSP['largura'] = 0;
-            $RSP['comprimento'] = 0;
-            $RSP['altura'] = 0;
-            $v = ['length','width','weigth','height'];
-            $n = ['comprimento','largura','peso','altura'];
-            foreach($v as $var)
-                {
-                    if (isset($vlr[$var]))
-                        {
-                            $va = (array)$vlr[$var];
-                            $va = $this->convertUnit($va['value'],$va['unit']).'cm';
-                            $RSP[$n[$var]] = $va;
-                        }
-                }
-            return $RSP;
+    {
+        $RSP['peso'] = 0;
+        $RSP['largura'] = 0;
+        $RSP['comprimento'] = 0;
+        $RSP['altura'] = 0;
+        $v = ['length', 'width', 'weigth', 'height'];
+        $n = [
+            'length' => 'comprimento',
+            'width' => 'largura',
+            'weigth' => 'peso',
+            'heigth' => 'altura'
+        ];
+        foreach ($v as $var) {
+            if (isset($vlr[$var])) {
+                $va = (array)$vlr[$var];
+                $va = $this->convertUnit($va['value'], $va['unit']) . 'cm';
+                $RSP[$n[$var]] = $va;
+            }
         }
-    function convertUnit($v,$unit)
-        {
-            $dv = $v;
-            switch($unit)
-                {
-                    case 'inches':
-                        $dv = round((float)$v * 254) / 100;
-                    break;
-                    case 'pound':
-                        $dv = round((float)$v * 453.592) / 1000;
-                    break;
-                }
-            return $dv;
+        return $RSP;
+    }
+    function convertUnit($v, $unit)
+    {
+        $dv = $v;
+        switch ($unit) {
+            case 'inches':
+                $dv = round((float)$v * 254) / 100;
+                break;
+            case 'pound':
+                $dv = round((float)$v * 453.592) / 1000;
+                break;
         }
+        return $dv;
+    }
     function dimensoes($vlr)
-        {
-            $dim = [];
-            $dm = troca($vlr,', ',',');
-            $dm = explode(',', $vlr);
-            foreach($dm as $idx=>$dmv)
-                {
-                    $dmv = explode(':',$dmv);
-                    if (isset($dmv[1]))
-                        {
-                            if (strpos($dmv[1],'Pounds'))
-                                {
-                                    $dv = trim(troca($dmv[1], 'Inches', ''));
-                                    $dv = round((float)$dv * 453.592) / 1000;
-                                    $dmv[1] = $dv;
-                                }
-                            if (strpos($dmv[1],'Inches'))
-                                {
-                                    $dv = trim(troca($dmv[1], 'Inches',''));
-                                    $dv = round((float)$dv * 254)/100;
-                                    $dmv[1] = $dv;
-                                } else {
-
-                                }
-                            switch(trim($dmv[0]))
-                                {
-                                    case 'Weight':
-                                        $dim['peso'] = $dmv[1];
-                                        break;
-                                    case 'Width':
-                                        $dim['largura'] = $dmv[1];
-                                        break;
-                                    case 'Length':
-                                        $dim['comprimento'] = $dmv[1];
-                                        break;
-                                    case 'Height':
-                                        $dim['altura'] = $dmv[1];
-                                        break;
-                                    default:
-                                        echo "OPS Dimmentions ".$dmv[0];
-                                        exit;
-                                }
-                        }
+    {
+        $dim = [];
+        $dm = troca($vlr, ', ', ',');
+        $dm = explode(',', $vlr);
+        foreach ($dm as $idx => $dmv) {
+            $dmv = explode(':', $dmv);
+            if (isset($dmv[1])) {
+                if (strpos($dmv[1], 'Pounds')) {
+                    $dv = trim(troca($dmv[1], 'Inches', ''));
+                    $dv = round((float)$dv * 453.592) / 1000;
+                    $dmv[1] = $dv;
                 }
-            return $dim;
+                if (strpos($dmv[1], 'Inches')) {
+                    $dv = trim(troca($dmv[1], 'Inches', ''));
+                    $dv = round((float)$dv * 254) / 100;
+                    $dmv[1] = $dv;
+                } else {
+                }
+                switch (trim($dmv[0])) {
+                    case 'Weight':
+                        $dim['peso'] = $dmv[1];
+                        break;
+                    case 'Width':
+                        $dim['largura'] = $dmv[1];
+                        break;
+                    case 'Length':
+                        $dim['comprimento'] = $dmv[1];
+                        break;
+                    case 'Height':
+                        $dim['altura'] = $dmv[1];
+                        break;
+                    default:
+                        echo "OPS Dimmentions " . $dmv[0];
+                        exit;
+                }
+            }
         }
+        return $dim;
+    }
 
     function convert($dta)
-        {
-            $ISBN = new \App\Models\ISBN\Index();
-            $Language = new \App\Models\Language\Lang();
+    {
+        $ISBN = new \App\Models\ISBN\Index();
+        $Language = new \App\Models\Language\Lang();
 
-            $dt = [];
-            $dt['authors'] = [];
+        $dt = [];
+        $dt['authors'] = [];
 
-            pre($dta,false);
+        pre($dta, false);
 
-            foreach($dta as $ida=>$line)
-                {
-                    foreach($line as $prop=>$vlr)
-                        {
-                            if ($prop=='isbn13') { $prop = 'isbn'; }
-                            if ($prop== 'isbn10') { $prop = 'isbn'; }
-                            switch($prop)
-                                {
-                                    case 'binding':
-                                        break;
-                                    case 'msrp':
-                                        break;
-                                    case 'date_published':
-                                        $dt['date'] = $vlr;
-                                        break;
-                                    case 'edition':
-                                        $dt['edicao'] = $vlr;
-                                        break;
-                                    case 'image':
-                                        $dt['cover'] = $vlr;
-                                        break;
-                                    case 'language':
-                                        $dt['language'] = $Language->code($vlr);
-                                        break;
-                                    case 'authors':
-                                        if (!is_array($vlr)) {
-                                            $vlr[0] = $vlr;
-                                        }
-                                        for($r=0;$r < count($vlr);$r++)
-                                            {
-                                                $v = troca($vlr[$r],'_','');
-                                                $v = trim($v);
-                                                if ($v != '')
-                                                    {
-                                                        $v = nbr_author($v,7);
-                                                        array_push($dt['authors'], $v);
-                                                    }
-                                            }
-                                        break;
-                                    case 'publisher':
-                                        if (is_array($vlr))
-                                            {
-                                                $dt['editora'] = $vlr;
-                                            } else {
-                                                $dt['editora'][0] = $vlr;
-                                            }
-                                        break;
-                                    case 'isbn':
-                                        $isbn = $vlr;
-                                        if (strlen($isbn) == 10)
-                                            {
-                                                $dt['isbn13'] = $ISBN->isbn10to13($isbn);
-                                                $dt['isbn10'] = $isbn;
-                                            } elseif (strlen($isbn) == 13) {
-                                                $dt['isbn13'] = $isbn;
-                                                $dt['isbn10'] = $ISBN->isbn13to10($isbn);
-                                            } else {
-                                                echo "ISBN Inválido";
-                                            }
-                                            break;
-                                    case 'title':
-                                        if (strpos($vlr,'(') > 0)
-                                            {
-                                                $vlr = substr($vlr,0, strpos($vlr, '('));
-                                            }
-                                        $dt['title'] = nbr_title(trim($vlr));
-                                        break;
-                                    case 'pages':
-                                        $dt['pages'] = $vlr.' p.';
-                                        break;
-                                    case 'title_long':
-                                        $dt['title_long'] = $vlr;
-                                        break;
-                                    case 'title_long':
-                                        $dt['title_long'] = $vlr;
-                                        break;
-                                    case 'subjects':
-                                        foreach($vlr as $id=>$subj)
-                                            {
-                                                if (!isset($dt['subjects']))
-                                                {
-                                                    $dt['subjects'] = [];
-                                                }
-                                                array_push($dt['subjects'], nbr_title($subj));
-                                            }
-
-                                        break;
-                                    case 'synopsis':
-                                        $dt['abstract'] = $vlr;
-                                        break;
-                                    case 'dimensions':
-                                        $dm = $this->dimensoes($vlr);
-                                        $dt['dimensoes'] = $dm;
-                                        break;
-                                    case 'dimensions_structured':
-                                        /***** Dimensoes */
-                                        $dm = $this->dimensoesEX((array)$vlr);
-                                        $dt['dimensoes_2'] = $dm;
-                                        break;
-                                     default:
-                                        echo 'Não localizado a propriedade ';
-                                        echo $prop;
-                                        echo '==>';
-                                        if (is_array($vlr))
-                                            {
-                                                pre($vlr,false);
-                                            } else {
-                                                pre($vlr, false);
-                                                //echo '<b>'.$vlr.'</b>';
-                                            }
-                                        echo '<br>';
-                                        exit;
-                                        break;
-                                }
-                        }
+        foreach ($dta as $ida => $line) {
+            foreach ($line as $prop => $vlr) {
+                if ($prop == 'isbn13') {
+                    $prop = 'isbn';
                 }
-            return $dt;
+                if ($prop == 'isbn10') {
+                    $prop = 'isbn';
+                }
+                switch ($prop) {
+                    case 'binding':
+                        break;
+                    case 'msrp':
+                        break;
+                    case 'date_published':
+                        $dt['date'] = $vlr;
+                        break;
+                    case 'edition':
+                        $dt['edicao'] = $vlr;
+                        break;
+                    case 'image':
+                        $dt['cover'] = $vlr;
+                        break;
+                    case 'language':
+                        $dt['language'] = $Language->code($vlr);
+                        break;
+                    case 'authors':
+                        if (!is_array($vlr)) {
+                            $vlr[0] = $vlr;
+                        }
+                        for ($r = 0; $r < count($vlr); $r++) {
+                            $v = troca($vlr[$r], '_', '');
+                            $v = trim($v);
+                            if ($v != '') {
+                                $v = nbr_author($v, 7);
+                                array_push($dt['authors'], $v);
+                            }
+                        }
+                        break;
+                    case 'publisher':
+                        if (is_array($vlr)) {
+                            $dt['editora'] = $vlr;
+                        } else {
+                            $dt['editora'][0] = $vlr;
+                        }
+                        break;
+                    case 'isbn':
+                        $isbn = $vlr;
+                        if (strlen($isbn) == 10) {
+                            $dt['isbn13'] = $ISBN->isbn10to13($isbn);
+                            $dt['isbn10'] = $isbn;
+                        } elseif (strlen($isbn) == 13) {
+                            $dt['isbn13'] = $isbn;
+                            $dt['isbn10'] = $ISBN->isbn13to10($isbn);
+                        } else {
+                            echo "ISBN Inválido";
+                        }
+                        break;
+                    case 'title':
+                        if (strpos($vlr, '(') > 0) {
+                            $vlr = substr($vlr, 0, strpos($vlr, '('));
+                        }
+                        $dt['title'] = nbr_title(trim($vlr));
+                        break;
+                    case 'pages':
+                        $dt['pages'] = $vlr . ' p.';
+                        break;
+                    case 'title_long':
+                        $dt['title_long'] = $vlr;
+                        break;
+                    case 'title_long':
+                        $dt['title_long'] = $vlr;
+                        break;
+                    case 'subjects':
+                        foreach ($vlr as $id => $subj) {
+                            if (!isset($dt['subjects'])) {
+                                $dt['subjects'] = [];
+                            }
+                            array_push($dt['subjects'], nbr_title($subj));
+                        }
+
+                        break;
+                    case 'synopsis':
+                        $dt['abstract'] = $vlr;
+                        break;
+                    case 'dimensions':
+                        $dm = $this->dimensoes($vlr);
+                        $dt['dimensoes'] = $dm;
+                        break;
+                    case 'dimensions_structured':
+                        /***** Dimensoes */
+                        $dm = $this->dimensoesEX((array)$vlr);
+                        $dt['dimensoes_2'] = $dm;
+                        break;
+                    default:
+                        echo 'Não localizado a propriedade ';
+                        echo $prop;
+                        echo '==>';
+                        if (is_array($vlr)) {
+                            pre($vlr, false);
+                        } else {
+                            pre($vlr, false);
+                            //echo '<b>'.$vlr.'</b>';
+                        }
+                        echo '<br>';
+                        exit;
+                        break;
+                }
+            }
         }
-
-
+        return $dt;
+    }
 }
