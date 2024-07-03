29cb7b8e68583e8725f3103eafaea2469439a23b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-03 15:02:40 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -82,7 +82,7 @@ def check_utf8():
 
             for i in range(0, len(dados2), segment_length):
                 xdados = dados2[i:i + segment_length]
-                print(xdados.hex(' '),xdados)
+                print("==<",xdados.hex(' '),xdados)
             print("============================================================")
             sys.exit()
             dados = str(dados)
