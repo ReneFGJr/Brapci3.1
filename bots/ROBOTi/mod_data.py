f9f57d8f81dbf0696089c5c6ab130ce5d926d48a
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-03 08:35:00 -0300

    Update mod_data.py

--- bots/ROBOTi/mod_data.py
+++ bots/ROBOTi/mod_data.py
@@ -36,6 +36,7 @@ def literal_double(prop = 0):
             n = 0
             for item2 in row2:
                 IDd = item2[3]
+                print(item2)
                 pha[n] = item2[1]
                 n += 1
                 if n > 2:
