60dd2fa91f38f2934603e4cf147779034c683b13
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-04 13:06:13 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -79,6 +79,7 @@ def correct_utf8_encoding(data, IDn):
                     data = data.replace(b'\xc3\xa2',b'[!]')
                     data = data.replace(b'\xc3\xa3',b'[a3]')
                     data = data.replace(b'\xc3\xac',b'[a~]')
+                    data = data.replace(b'\xc3\xa2\xc2\x80\xc2\x99','')
 
                     data = data.replace(b'\xc2\x80',b'')
                     data = data.replace(b'\xc2\x81',b'')
