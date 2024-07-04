4afa9024333319270366aa16551f82359c8b8023
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-04 11:31:28 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -52,9 +52,11 @@ def correct_utf8_encoding(data):
             return data
         if (encoding == 'utf-8'):
 
+            print(data)
+            data = data.encode('utf-8', errors='ignore').decode('utf-8')
+
             if (b'\xc3' in data) or (b'\xc2' in data):
-                print(data)
-                data = data.encode('utf-8', errors='ignore').decode('utf-8')
+
                 print(data)
                 data = data.encode('utf-8', errors='ignore')
 
