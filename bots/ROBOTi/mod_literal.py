b8a31a3211553d2783c4fe9fa1773e4bf9f31e30
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-04 08:22:43 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -43,6 +43,7 @@ def correct_utf8_encoding(data):
         return data
 
 def check_utf8():
+        print("154 - Check UTF8")
         qr = f"SELECT id_n, n_name FROM brapci_rdf.brapci_literal"
         rows = database.query(qr)
         for row in rows:
