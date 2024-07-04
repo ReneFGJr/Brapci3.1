9f48624a16b8b96f2210efc491bb230798c75a69
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-04 15:06:06 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -215,7 +215,6 @@ def check_utf8():
         qr += " or n_name LIKE '%ã"+chr(130)+"%' "
         qr += " or n_name LIKE '%ã"+chr(157)+"%' "
         qr += " or n_name LIKE '%"+chr(194)+"%' "
-        qr += " or n_name LIKE '%"+chr(195)+"%' "
         qr += " )"
         qr += " limit 1000"
         rows = database.query(qr)
