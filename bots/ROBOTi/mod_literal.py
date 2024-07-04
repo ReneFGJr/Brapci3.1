7fa2cf75444d72f1ff32104942e1ce6cb249ec2c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-04 08:21:42 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -53,6 +53,8 @@ def check_utf8():
                 print(f"Corrigindo: {original_data} -> {corrected_data}")
                 #sql_update = f"UPDATE {table} SET {column} = %s WHERE id = %s"
                 #cursor.execute(sql_update, (corrected_data, row['id']))
+            else:
+                print(f"==: {original_data} -> {corrected_data}")
 
 def check_utf8_old():
     qr = "select id_n, n_name "
