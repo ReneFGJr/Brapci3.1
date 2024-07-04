eebfa40bf51ad772b265ab643a14a9fd62bf01e1
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-04 12:20:59 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -75,9 +75,9 @@ def correct_utf8_encoding(data, IDn):
                 except Exception as e:
                     print("Origin::",data)
 
-                    data = data.replace(b'\xc2\x9c',b'#')
-                    data = data.replace(b'\xc2\x98',b'#')
-                    data = data.replace(b'\xc2\xa2',b'#')
+                    #data = data.replace(b'\xc2\x9c',b'#')
+                    #data = data.replace(b'\xc2\x98',b'#')
+                    #data = data.replace(b'\xc2\xa2',b'#')
                     print("Replaced",data)
                     string_data = data.decode('utf-8', errors='ignore')
                     try:
