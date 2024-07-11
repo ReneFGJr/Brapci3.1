f38391f80102ad3c13361b2aa7bff9c8a017bf54
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-11 16:47:43 -0300

    Update RDF.php

--- app/Models/RDF2/RDF.php
+++ app/Models/RDF2/RDF.php
@@ -133,7 +133,10 @@ class RDF extends Model
                     $sx .= anchor(PATH . '/rdf/Class/' . $d2 . '/reimport', 'Reimporta', ['class' => 'ms-2 btn btn-outline-warning']);
                     $sx .= anchor(PATH . '/api/rdf/in/all', 'Importa', ['class' => 'ms-2 btn btn-outline-danger']);
 
-                    $sx .= '========';
+                    foreach($dt['Form'] as $id=>$lined)
+                        {
+                            pre($lined);
+                        }
                 }
                 return bs(bsc($sx));
                 break;
