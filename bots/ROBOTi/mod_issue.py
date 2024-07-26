5ea50be4c8f7bbe34c4599d0855da24c1c0944ea
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-07-26 08:32:39 -0300

    Update mod_issue.py

--- bots/ROBOTi/mod_issue.py
+++ bots/ROBOTi/mod_issue.py
@@ -59,6 +59,7 @@ def identify(rg):
             if (row == []):
                 row = create_issue(JNL,year,vol,nr)
             else:
+                print(row)
                 if row[3] == 0:
                     print("Atualizando ISSUE")
                     ISSUE = create_issue_rdf(JNL,year,vol,nr)
