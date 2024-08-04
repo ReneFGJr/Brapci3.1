bbfd0d8a8564bd438d385bf47e5bf19492021dc2
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-08-04 07:47:21 -0300

    Create orcid.service.ts

---
+++ angular/BRP/src/app/000_core/010_services/orcid.service.ts
@@ -0,0 +1,31 @@
+// auth.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private clientId = 'APP-5SXE6F14K56K8RQN';
+  private redirectUri = 'http://localhost:4200/callback';
+  private tokenUrl = 'https://orcid.org/oauth/token';
+  private authorizeUrl = 'https://orcid.org/oauth/authorize';
+
+  constructor(private http: HttpClient) {}
+
+  login() {
+    window.location.href = `${this.authorizeUrl}?client_id=${this.clientId}&response_type=code&scope=/authenticate&redirect_uri=${this.redirectUri}`;
+  }
+
+  getAccessToken(code: string) {
+    const body = new HttpParams()
+      .set('client_id', this.clientId)
+      .set('client_secret', '93a45b75-b66c-41bc-a13a-54aaf3746df4')
+      .set('grant_type', 'authorization_code')
+      .set('code', code)
+      .set('redirect_uri', this.redirectUri);
+
+    return this.http.post(this.tokenUrl, body);
+  }
+}
