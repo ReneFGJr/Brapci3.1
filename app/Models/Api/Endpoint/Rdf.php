b2ea092610702c94b3c9855a54ea1995705bc4ab
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-08-13 15:58:26 -0300

    Update Rdf.php

--- app/Models/Api/Endpoint/Rdf.php
+++ app/Models/Api/Endpoint/Rdf.php
@@ -57,10 +57,8 @@ class Rdf extends Model
         header('Access-Control-Allow-Origin: *');
 
 
-        if (get("test") == '')
-        {
-            if (($d2 != 'import') and ($d2 != 'in') and ($d2 != 'searchSelect'))
-            {
+        if (get("test") == '') {
+            if (($d2 != 'import') and ($d2 != 'in') and ($d2 != 'searchSelect')) {
                 header("Access-Control-Allow-Headers: Content-Type");
                 header("Content-Type: application/json");
             }
@@ -76,147 +74,152 @@ class Rdf extends Model
         $RDFclassDomain = new \App\Models\RDF2\RDFclassDomain();
         $RDFproperty = new \App\Models\RDF2\RDFproperty();
         $Language = new \App\Models\AI\NLP\Language();
+        $Socials = new \App\Models\Socials();
 
         $RSP = [];
         //header("Content-Type: application/json");
-        switch($d2)
-            {
-                case 'a':
-                    $RSP = $RDFform->editRDFapi($d3);
-                    $RSP['status'] = '200';
-                    echo json_encode($RSP);
-                    exit;
-                    break;
-                case 'createLiteral':
-                    $dd = [];
-                    $dd['post'] = $_POST;
-                    $q = get("q");
-                    $ID = get("ID");
-                    $prop = get('prop');
+        switch ($d2) {
+            case 'a':
+                $RSP = $RDFform->editRDFapi($d3);
+                $RSP['status'] = '200';
+                echo json_encode($RSP);
+                exit;
+                break;
+            case 'createLiteral':
+                $dd = [];
+                $dd['post'] = $_POST;
+                $q = get("q");
+                $ID = get("ID");
+                $prop = get('prop');
 
-                    /* Register IDn */
-                    $lang = $Language->getTextLanguage($q);
-                    $idn = $RDFliteral->register($q,$lang);
+                /* Register IDn */
+                $lang = $Language->getTextLanguage($q);
+                $idn = $RDFliteral->register($q, $lang);
 
-                    /* Register Data */
-                    $RDFdata->register($ID,$prop,0,$idn);
-                    $dd['status'] = '200';
-                    $dd['idn'] = $idn;
-                    echo json_encode($dd);
-                    exit;
-                    break;
-                case 'delData':
+                /* Register Data */
+                $RDFdata->register($ID, $prop, 0, $idn);
+                $dd['status'] = '200';
+                $dd['idn'] = $idn;
+                echo json_encode($dd);
+                exit;
+                break;
+            case 'delData':
+                $key = get("key");
+                $_POST['token'] = $key;
+                $RSP = $Socials->validToken();
+
+                pre($RSP);
+
+                if ($RSP['status'] = '200') {
                     $RDFdata = new \App\Models\RDF2\RDFdata();
                     $dt = $RDFdata->where('id_d', $d3)->first();
-                    if ($dt != [])
-                        {   pre($_POST);
-                            $RDFdata->where('id_d', $d3)->delete();
-                            $RSP['status'] = '200';
-                            $RSP['message'] = 'Removed';
-                        } else {
-                            $RSP['status'] = '400';
-                            $RSP['message'] = 'Register not found';
-                            $RSP['ID'] = $d3;
-                        }
-                    echo json_encode($RSP);
-                    exit;
-                case 'deleteConcept':
-                    $RDF = new \App\Models\RDF2\RDF();
-                    $dd = $RDF->remove($d3);
-                    echo json_encode($dd);
-                    exit;
-                    break;
-                case 'createConcept':
-                    $RDFconcept = new \App\Models\RDF2\RDFconcept();
-                    $Language = new \App\Models\AI\NLP\Language();
-                    $dd = [];
-                    $dd['Class'] = $d3;
-                    $dd['Name'] = get("name");
-                    $dd['Lang'] = $Language->getTextLanguage($dd['Name']);
-                    $dd['id'] = $RDFconcept->createConcept($dd);
-                    echo json_encode($dd);
-                    exit;
-                    break;
-                case 'getResource':
-                    $RSP = [];
-                    $RSP['ID'] = get("ID");
-                    $RSP['prop'] = get("prop");
-                    $dt = $RDF->le($RSP['ID']);
-                    $ClassID = $dt['concept']['id_c'];
-                    $RSP['ClassID'] = $ClassID;
-                    $propID = $RDFproperty->getProperty($RSP['prop']);
-                    $RSP['resource'] = $RDFclassDomain->getResources($ClassID,$propID);
-                    break;
-                case 'dataAddLiteral':
-                    /************************ REGISTRA */
-                    $RDFClass = new \App\Models\RDF2\RDFclass();
-                    $RDFdata = new \App\Models\RDF2\RDFdata();
-                    $RDFliteral = new \App\Models\RDF2\RDFliteral();
-                    $Language = new \App\Models\AI\NLP\Language();
-                    $Literal = get("txt");
-                    $lang = $Language->getTextLanguage($Literal);
+                    if ($dt != []) {
+                        //$RDFdata->where('id_d', $d3)->delete();
+                        $RSP['status'] = '200';
+                        $RSP['message'] = 'Removed';
+                    } else {
+                        $RSP['status'] = '400';
+                        $RSP['message'] = 'Register not found';
+                        $RSP['ID'] = $d3;
+                    }
+                }
+                echo json_encode($RSP);
+                exit;
+            case 'deleteConcept':
+                $RDF = new \App\Models\RDF2\RDF();
+                $dd = $RDF->remove($d3);
+                echo json_encode($dd);
+                exit;
+                break;
+            case 'createConcept':
+                $RDFconcept = new \App\Models\RDF2\RDFconcept();
+                $Language = new \App\Models\AI\NLP\Language();
+                $dd = [];
+                $dd['Class'] = $d3;
+                $dd['Name'] = get("name");
+                $dd['Lang'] = $Language->getTextLanguage($dd['Name']);
+                $dd['id'] = $RDFconcept->createConcept($dd);
+                echo json_encode($dd);
+                exit;
+                break;
+            case 'getResource':
+                $RSP = [];
+                $RSP['ID'] = get("ID");
+                $RSP['prop'] = get("prop");
+                $dt = $RDF->le($RSP['ID']);
+                $ClassID = $dt['concept']['id_c'];
+                $RSP['ClassID'] = $ClassID;
+                $propID = $RDFproperty->getProperty($RSP['prop']);
+                $RSP['resource'] = $RDFclassDomain->getResources($ClassID, $propID);
+                break;
+            case 'dataAddLiteral':
+                /************************ REGISTRA */
+                $RDFClass = new \App\Models\RDF2\RDFclass();
+                $RDFdata = new \App\Models\RDF2\RDFdata();
+                $RDFliteral = new \App\Models\RDF2\RDFliteral();
+                $Language = new \App\Models\AI\NLP\Language();
+                $Literal = get("txt");
+                $lang = $Language->getTextLanguage($Literal);
 
-                    $id_prop = $RDFClass->getClass(get("prop"));
-                    $ID = get("source");
-                    $ID2 = 0;
-                    $lit = $RDFliteral->register($Literal,$lang);
+                $id_prop = $RDFClass->getClass(get("prop"));
+                $ID = get("source");
+                $ID2 = 0;
+                $lit = $RDFliteral->register($Literal, $lang);
 
-                    $RDFdata->register($ID, $id_prop, $ID2, $lit);
-                    break;
-                case 'dataAdd':
-                    /************************ REGISTRA */
-                    $RDFClass = new \App\Models\RDF2\RDFclass();
-                    $RDFdata = new \App\Models\RDF2\RDFdata();
-                    $ID = get("source");
-                    $id_prop = $RDFClass->getClass(get("prop"));
-                    $ID2 = get("resource");
-                    $lit = 0;
-                    $RDFdata->register($ID, $id_prop, $ID2, $lit);
-                    $RSP = $RDFform->searchSelect('', '');
-                    break;
-                case 'searchSelect':
-                    $RSP = $RDFform->searchSelect($d3,$d4);
-                    break;
-                case 'search':
-                    $RSP = $RDFform->search($d3,$d4);
-                    break;
-                case 'remissive':
-                    $RSP['data'] = $RDFrules->RDFremissive();
-                    break;
-                case 'getdata':
-                    $RSP['data'] = $RDFconcept->getData($d3);
-                    break;
-                case 'get':
-                    $RDFClass = new \App\Models\RDF2\RDFclass();
-                    $RSP = $RDFClass->get($d3);
-                    break;
-                case 'v':
-                    $RSP = $RDF->v($d3);
-                    break;
-                case 'import':
-                    $RDF = new \App\Models\RDF2\RDF();
-                    return $RSP = $RDF->import();
-                    break;
-                case 'resume':
-                    $RDF = new \App\Models\RDF2\RDF();
-                    $RSP = $RDF->resume();
-                case 'in':
-                    $RDFtools = new \App\Models\RDF2\RDFtoolsImport();
-                    if ($d3 == 'all')
-                        {
-                            $RSP = $RDFtools->importRDFAll();
-                        } elseif ($d3 == 'old')
-                        {
-                            $RSP = $RDFtools->importRDFOld();
-                        } else {
-                            $RSP = $RDFtools->importRDF($d3,true);
-                        }
-                    break;
-                default:
-                    $RSP['Class'] = $RDFclass->getClasses();
-                    $RSP['Property'] = $RDFproperty->getProperties();
-                    break;
-            }
+                $RDFdata->register($ID, $id_prop, $ID2, $lit);
+                break;
+            case 'dataAdd':
+                /************************ REGISTRA */
+                $RDFClass = new \App\Models\RDF2\RDFclass();
+                $RDFdata = new \App\Models\RDF2\RDFdata();
+                $ID = get("source");
+                $id_prop = $RDFClass->getClass(get("prop"));
+                $ID2 = get("resource");
+                $lit = 0;
+                $RDFdata->register($ID, $id_prop, $ID2, $lit);
+                $RSP = $RDFform->searchSelect('', '');
+                break;
+            case 'searchSelect':
+                $RSP = $RDFform->searchSelect($d3, $d4);
+                break;
+            case 'search':
+                $RSP = $RDFform->search($d3, $d4);
+                break;
+            case 'remissive':
+                $RSP['data'] = $RDFrules->RDFremissive();
+                break;
+            case 'getdata':
+                $RSP['data'] = $RDFconcept->getData($d3);
+                break;
+            case 'get':
+                $RDFClass = new \App\Models\RDF2\RDFclass();
+                $RSP = $RDFClass->get($d3);
+                break;
+            case 'v':
+                $RSP = $RDF->v($d3);
+                break;
+            case 'import':
+                $RDF = new \App\Models\RDF2\RDF();
+                return $RSP = $RDF->import();
+                break;
+            case 'resume':
+                $RDF = new \App\Models\RDF2\RDF();
+                $RSP = $RDF->resume();
+            case 'in':
+                $RDFtools = new \App\Models\RDF2\RDFtoolsImport();
+                if ($d3 == 'all') {
+                    $RSP = $RDFtools->importRDFAll();
+                } elseif ($d3 == 'old') {
+                    $RSP = $RDFtools->importRDFOld();
+                } else {
+                    $RSP = $RDFtools->importRDF($d3, true);
+                }
+                break;
+            default:
+                $RSP['Class'] = $RDFclass->getClasses();
+                $RSP['Property'] = $RDFproperty->getProperties();
+                break;
+        }
         echo json_encode($RSP);
         exit;
     }
