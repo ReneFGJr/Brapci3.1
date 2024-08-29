507b743a4c9ea97254f27cdc6b9833b7fe02924c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-08-29 16:35:08 -0300

    Update mod_cited.py

--- bots/ROBOTi/mod_cited.py
+++ bots/ROBOTi/mod_cited.py
@@ -15,6 +15,7 @@ def categorizeCitedByElastic():
 
     for line in row:
         ID = line[0]
+        print(line)
         url = "https://cip.brapci.inf.br/api/brapci/get/v1/"+ID
         print(url)
 
