3d0533ccb0b717dac50289c39aea8f8b45b843e6
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-10 12:04:39 -0300

    Update mod_elasticsearch.py

--- bots/ROBOTi/mod_elasticsearch.py
+++ bots/ROBOTi/mod_elasticsearch.py
@@ -124,6 +124,7 @@ def export_elasticsearch_v2_2(line, offset, dtt, limit):
                         idaa[idx] = 1
 
         # Palavras-chave
+        print(DT)
         if 'Keywords' in DT:
             for ks in DT['Keywords'].values():
                 if isinstance(ks, dict):
@@ -213,6 +214,7 @@ def reindex():
 
 def dataset_news():
     qr = "select * from brapci_elastic.dataset where new = 1 and  `use` = 0 order by id_ds desc"
+    qr = "select * from brapci_elastic.dataset where ID = 231497"
     row = database.query(qr)
 
     for ln in row:
