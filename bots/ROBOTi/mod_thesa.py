15ac22e4850446432d2698639c0d5bb22e90a0f7
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-10 18:29:13 -0300

    Update mod_thesa.py

--- bots/ROBOTi/mod_thesa.py
+++ bots/ROBOTi/mod_thesa.py
@@ -38,7 +38,7 @@ def IA_thesa(term,lang):
 
 def checkExistConcept(ID,lang):
     qr = "select * thesa_concept "
-    qr += " join brapci_thesa.thesa_literal ON c_term = id_l and l_lang = '{lang}'"
+    qr += f" join brapci_thesa.thesa_literal ON c_term = id_l and l_lang = '{lang}'"
     qr += f" where c_group = {ID}"
     print(qr)
     xxxx
