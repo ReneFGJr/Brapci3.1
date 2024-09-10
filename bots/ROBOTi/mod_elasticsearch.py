7d8318b7a13804cee9eb3fe506c49e5d29045619
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-10 11:36:22 -0300

    Update mod_elasticsearch.py

--- bots/ROBOTi/mod_elasticsearch.py
+++ bots/ROBOTi/mod_elasticsearch.py
@@ -189,6 +189,7 @@ def export_elasticsearch_v2_2(line, offset, dtt, limit):
             sx = f'{id} => {result["result"]} v.{result["_version"]} ({dt["collection"]})'
             print(sx)
         except Exception as e:
+            print(e)
             return {'error': 'Exception', 'message': str(e)}
         # Simulação de função exported (não implementada)
         # self.exported(id, 0)
