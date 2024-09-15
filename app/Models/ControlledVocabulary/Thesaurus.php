afa3e1c870c95286aec1912fd102af36e67eaa96
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-09-15 17:35:54 -0300

    Update Thesaurus.php

--- app/Models/ControlledVocabulary/Thesaurus.php
+++ app/Models/ControlledVocabulary/Thesaurus.php
@@ -99,7 +99,7 @@ class Thesaurus extends Model
             {
                 $sx = form_open();
                 $sx .= form_input('q','form-control full', 'form-control full');
-                $sx .= form_submit('filter');
+                $sx .= form_submit('btn btn-secondary','filter');
                 $sx .= form_close();
                 return $sx;
             }
