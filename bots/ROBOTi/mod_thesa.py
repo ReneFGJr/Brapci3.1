e2e40888b23e00e7f2a732f7cf390043883fda4e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-09-15 22:01:10 -0300

    Update mod_thesa.py

--- bots/ROBOTi/mod_thesa.py
+++ bots/ROBOTi/mod_thesa.py
@@ -79,7 +79,6 @@ def checkExistConcept(ID,lang):
     qr += f" inner join brapci_thesa.thesa_literal ON c_term = id_l and l_lang = '{lang}'"
     qr += f" where c_group = {ID}"
 
-    print(qr)
     row = database.query(qr)
     if row == []:
         return False
