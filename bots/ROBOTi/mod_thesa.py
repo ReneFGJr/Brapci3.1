7a7b925c035a965ec8432af84fc33356f2f6869e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-09-15 21:56:29 -0300

    Update mod_thesa.py

--- bots/ROBOTi/mod_thesa.py
+++ bots/ROBOTi/mod_thesa.py
@@ -44,7 +44,7 @@ def translate(term,lang):
     ES = checkExistConcept(IDc,'es')
     PT = checkExistConcept(IDc,'pt')
 
-    print("TRADUCAO",EN,ES,PT,"ID:",ID,TERM)
+    print("TRADUCAO",EN,ES,PT,"ID:",ID,TERM,IDc)
 
     sys.exit()
 
