2a33927d8b0b24674e4b7e722c787df64e214b32
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-09-17 22:26:39 -0300

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
 
 $routes->get('/api', 'Api::index');
 $routes->get('/api/(:any)', 'Api::index/$1');
 $routes->post('/api/(:any)', 'Api::index/$1');
 $routes->put('/api/(:any)', 'Api::index/$1');
+
+/********** DEFAULT */
+$routes->get('/', 'MainPages::index');
+$routes->post('/', 'MainPages::index');
