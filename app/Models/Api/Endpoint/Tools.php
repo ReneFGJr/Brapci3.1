1c551c5bb4641deeee9d40b47dc6076e4b25d892
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-09-23 22:37:22 -0300

    Update Tools.php

--- app/Models/Api/Endpoint/Tools.php
+++ app/Models/Api/Endpoint/Tools.php
@@ -76,7 +76,7 @@ class Tools extends Model
                     $text = get("text");
                     $RIS = new \App\Models\Metadata\RIS();
                     $RSP = [];
-                    //$RSP['response'] = $RIS->risToMarc21($text);
+                    $RSP['response'] = $RIS->risToMarc21($text);
                     $RSP['RIS'] = $text;
                     $RSP['post'] = $_POST;
                     echo json_encode($RSP);
