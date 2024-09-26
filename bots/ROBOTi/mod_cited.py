605f807b1cab22293aca4a199f6e32e3cff2dc93
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-26 16:52:21 -0300

    Update mod_cited.py

--- bots/ROBOTi/mod_cited.py
+++ bots/ROBOTi/mod_cited.py
@@ -170,6 +170,7 @@ def categorizeYear():
         ref = line[1]
         year = year_identify(ref)
         qu = f"update brapci_cited.cited_article set ca_year = '{year}' where id_ca = {ID}"
+        print(f"Update Year {year} in {ID}")
         database.update(qu)
     sys.exit()
 
