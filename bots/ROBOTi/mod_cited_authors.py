02c9af1bbb458d665d19c89e50b2e60b425203fd
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-09-29 10:35:29 -0300

    Update mod_cited_authors.py

--- bots/ROBOTi/mod_cited_authors.py
+++ bots/ROBOTi/mod_cited_authors.py
@@ -15,15 +15,12 @@ def extract_authors_full(reference):
     # Regex para capturar os autores antes do título
     # Inclui palavras com letras maiúsculas e minúsculas, além de espaços para nomes compostos
     # Padrão para capturar autores no formato "SOBRENOME, Nome"
-    padrao_autor = r'([A-ZÁÉÍÓÚÑ]+(?: [A-ZÁÉÍÓÚÑ]+)*),\s*([A-Za-zÁÉÍÓÚáéíóúñÑ]+(?:\s+[A-Za-zÁÉÍÓÚáéíóúñÑ]+)*)'
-
-    # Encontrar todos os autores que seguem o padrão acima
-    match = re.findall(padrao_autor, reference)
+    pattern = r'([A-ZÁÉÍÓÚÑ]+(?: [A-ZÁÉÍÓÚÑ]+)*),\s*([A-Za-zÁÉÍÓÚáéíóúñÑ]+(?:\s+[A-Za-zÁÉÍÓÚáéíóúñÑ]+)*)'
+    match = re.match(pattern, reference)
 
     if match:
         authors_str = match.group(1)
         # Separar autores por ";" e remover espaços extras
         authors = [author.strip() for author in authors_str.split(";")]
         return authors
-
     return []
