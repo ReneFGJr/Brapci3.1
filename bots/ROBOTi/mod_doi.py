580ac37faf153485b9e44ef06707a9e522e56b49
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-09-30 18:54:44 -0300

    Update mod_doi.py

--- bots/ROBOTi/mod_doi.py
+++ bots/ROBOTi/mod_doi.py
@@ -5,6 +5,7 @@ from urllib.parse import unquote
 def encontrar_doi(texto):
     # Expressão regular para identificar o padrão de DOI
     texto = unquote(texto)
+    texto = texto.replace('/ ','/')
     padrao_doi = r'\b(10\.\d{4,9}/[-._;()/:A-Z0-9]+)\b'
 
     # Procurar todos os DOI's no texto
