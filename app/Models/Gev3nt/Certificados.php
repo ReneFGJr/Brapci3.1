2cca7ae273adc3e676cab11363d4847c84978244
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-10-08 10:32:08 -0300

    Update Certificados.php

--- app/Models/Gev3nt/Certificados.php
+++ app/Models/Gev3nt/Certificados.php
@@ -98,7 +98,7 @@ class Certificados extends Model
         $pdf->AddPage();
 
         // Definir a imagem de fundo
-        $pdf->Image($imagemFundo, 0, 0, 320, $pdf->getPageHeight()+100, '', '', '', false, 300, '', false, false, 0);
+        $pdf->Image($imagemFundo, 0, 0, 320, 240, '', '', '', false, 300, '', false, false, 0);
 
         // Definir a posição e o estilo do texto
         $pdf->SetFont('helvetica', 'B', 24);
