abff5fe33d52841a7bf05a89df536813ea9ea42e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-12 07:59:54 -0300

    Update mod_gender.py

--- bots/ROBOTi/mod_gender.py
+++ bots/ROBOTi/mod_gender.py
@@ -12,7 +12,7 @@ def check():
     qr = "select id_cc, cc_use, n_name  "
     qr += " from brapci_rdf.rdf_concept "
     qr += " inner join brapci_rdf.rdf_literal ON id_n = cc_pref_term"
-    qr += f" left join brapci_rdf.rdf_data ON (d_r1 = id_cc) and (id_p == {IDprop})"
+    qr += f" left join brapci_rdf.rdf_data ON (d_r1 = id_cc) and (d_p = {IDprop})"
     qr += f" where cc_class = {IDClass}"
     qr += " and id_cc = cc_use "
     qr += " order by n_name, id_cc"
