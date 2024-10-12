08d66595c36c928b61391bae9560f2319c99d766
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-12 10:04:44 -0300

    Update mod_abstract.py

--- bots/ROBOTi/mod_abstract.py
+++ bots/ROBOTi/mod_abstract.py
@@ -6,5 +6,16 @@ def check_double():
     prop = 'hasAbstract'
     IDprop = mod_class.getClass(prop)
 
-    print(IDprop)
-    qr = ""
+    qr = "select * from ( "
+    qr += "SELECT count(*) as total, d_r1, n_lang "
+    qr += "FROM `rdf_data`  "
+    qr += "inner join rdf_literal ON d_literal = id_n "
+    qr += f"where d_p = {prop} "
+    qr += "group by d_r1, n_lang "
+    qr += ") as tabela "
+    qr += "WHERE total > 1;"
+
+    row = database.query(qr)
+    for line in row:
+        print(line)
+        print("=======================")
