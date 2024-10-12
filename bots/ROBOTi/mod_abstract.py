fb7eab768c40546e3a4f46f07e4b63816dd49bd3
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-12 10:13:44 -0300

    Update mod_abstract.py

--- bots/ROBOTi/mod_abstract.py
+++ bots/ROBOTi/mod_abstract.py
@@ -2,6 +2,10 @@ import database
 import mod_class
 import sys
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
+
 
 def check_double():
     prop = 'hasAbstract'
@@ -31,6 +35,20 @@ def removeDouble(ID,lang,IDprop):
     qr += f" where d_r1 = {ID} and d_p = {IDprop} and n_lang = '{lang}' "
     qr += " order by id_n "
     row = database.query(qr)
-    for line in row:
-        print(line)
+
+    ln1 = row[0]
+    ln2 = row[1]
+    print(grau_de_equivalencia(ln1,ln2))
     sys.exit()
+
+def grau_de_equivalencia(texto1, texto2):
+    # Vetorização dos textos usando TF-IDF (Term Frequency - Inverse Document Frequency)
+    vectorizer = TfidfVectorizer().fit_transform([texto1, texto2])
+
+    # Calcula a similaridade de cosseno entre os dois textos
+    cosine_sim = cosine_similarity(vectorizer[0:1], vectorizer[1:2]).flatten()[0]
+
+    # Converte para uma escala de 0 a 100
+    grau_equivalencia = round(cosine_sim * 100, 2)
+
+    return grau_equivalencia
