ba0c1aa34003812439ea122f6018bed9219acc92
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-28 07:15:41 -0300

    Update ai_cited.py

--- bots/TOOLS/ai_cited.py
+++ bots/TOOLS/ai_cited.py
@@ -56,7 +56,7 @@ def preparar_referencias(texto):
     texto = texto.replace(' \n',' ')
 
     # Letras
-    ltrs = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','(',',',';',':']
+    ltrs = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','(',',',';',':','0','1','2','3','4','5','6','7','8','9']
     for l in ltrs:
         texto = texto.replace('\n'+l,l)
 
