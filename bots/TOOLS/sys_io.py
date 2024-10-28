a25c7990278d50c28cbd0dd3fd8a07131f31f6b5
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-27 21:52:28 -0300

    Update sys_io.py

--- bots/TOOLS/sys_io.py
+++ bots/TOOLS/sys_io.py
@@ -1,3 +1,5 @@
+import os
+
 def readfile(nome_arquivo):
     try:
         # Abre o arquivo no modo de leitura
@@ -9,6 +11,19 @@ def readfile(nome_arquivo):
         print(f"O arquivo '{nome_arquivo}' não foi encontrado.")
         return None
 
+
+
+def ler_arquivos_diretorio(diretorio):
+    try:
+        # Lista todos os arquivos no diretório
+        arquivos = os.listdir(diretorio)
+        print(arquivos)
+    except FileNotFoundError:
+        print(f"O diretório '{diretorio}' não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {e}")
+    return ""
+
 def getNameFile(id):
     # Converte o id para string e preenche com zeros à esquerda até ter 8 caracteres
     id_str = str(id).zfill(8)
@@ -19,4 +34,6 @@ def getNameFile(id):
     # Monta o caminho com a estrutura ../../public/_repository/xx/xx/xx/xx/
     caminho = "../../public/_repository/" + "/".join(partes) + "/"
 
-    return caminho
+    file = ler_arquivos_diretorio(caminho)
+
+    return file
