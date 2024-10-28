df3600b575129bec4fec9c0f18cf8ec636662ca5
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-28 07:14:54 -0300

    Update ai_cited.py

--- bots/TOOLS/ai_cited.py
+++ bots/TOOLS/ai_cited.py
@@ -50,15 +50,13 @@ def preparar_referencias(texto):
     texto = texto.replace(': ',':')
     texto = texto.replace('; ',';')
 
-    texto = texto.replace(',\n',',')
     texto = texto.replace('\n,',',')
     texto = texto.replace('\n.','.')
-    texto = texto.replace(';\n',';')
-    texto = texto.replace(':\n',':')
+
     texto = texto.replace(' \n',' ')
 
     # Letras
-    ltrs = ['a','b','c','d','e','f','g','h']
+    ltrs = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','(',',',';',':']
     for l in ltrs:
         texto = texto.replace('\n'+l,l)
 
