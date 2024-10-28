64b5649ffd3650537a31f00388a729601d4931f5
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-28 09:09:01 -0300

    Update ai_cited.py

--- bots/TOOLS/ai_cited.py
+++ bots/TOOLS/ai_cited.py
@@ -103,9 +103,13 @@ def preparar_referencias(texto):
     for i, ln in enumerate(lns):
         if not sys_io.soNumero(ln):
             print(i,'=x=',ln)
-            lns[i] = lns[i+1]
-            lns[i+1] = ''
-            print(i,'=z=',lns[i+1])
+            try:
+                lns[i] = lns[i+1]
+                lns[i+1] = ''
+                print(i,'=z=',lns[i+1])
+            except:
+                print(i,"OPS",ln)
+
 
     sys.exit()
 
