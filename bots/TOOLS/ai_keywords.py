19f6c46451d03749e2515a456e7d7b130d4aeba6
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-28 19:29:36 -0300

    Update ai_keywords.py

--- bots/TOOLS/ai_keywords.py
+++ bots/TOOLS/ai_keywords.py
@@ -3,7 +3,8 @@ import re
 def extract_keywords(text,id):
     text = text.replace(chr(10),' ')
     # Use regex to find "Palavras-chave:" followed by any text until the end of the line
-    match = re.search(r"Palavras-chave:\s*(.*)", text)
+    #match = re.search(r"Palavras-chave:\s*(.*)", text)
+    match = re.search(r"Palavras-chave:\s*(.*?)(?=Abstract:)", text, re.DOTALL)
     if match:
         keywords = match.group(1).split(";")  # Split keywords separated by semicolons
         return [keyword.strip() for keyword in keywords]  # Strip whitespace from each keyword
