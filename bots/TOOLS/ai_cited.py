bb6c8d9fc323f32b88d9c95e59bbbee2625bb95f
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-28 08:40:14 -0300

    Update ai_cited.py

--- bots/TOOLS/ai_cited.py
+++ bots/TOOLS/ai_cited.py
@@ -110,7 +110,7 @@ def preparar_referencias(texto):
             linhas[i] = '##@@$$'
         else:
             if (strFIM1 == strFIM2):
-                linhas[idIn] += ' !! ' strFIM2+ ' || '+ linhas[i+1]
+                linhas[idIn] += ' !! ' + strFIM2+ ' || '+ linhas[i+1]
             else:
                 idIn = i
 
