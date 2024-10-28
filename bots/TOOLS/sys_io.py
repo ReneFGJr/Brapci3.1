8b7183d8c24a6ceb136b8208f3eb848747b1d39e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-10-28 06:12:56 -0300

    Update sys_io.py

--- bots/TOOLS/sys_io.py
+++ bots/TOOLS/sys_io.py
@@ -62,5 +62,5 @@ def getNameFile(id):
 
 ################################# Separa linhas
 def separar_por_linhas(texto):
-    texto += '\n'+'#FIM'+'\n'
+    texto += '\n\r'
     return texto.splitlines()
