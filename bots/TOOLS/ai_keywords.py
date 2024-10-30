c989186f915c9847f4186e5ca34cfbc8015cdde4
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-10-30 08:36:33 -0300

    Update ai_keywords.py

--- bots/TOOLS/ai_keywords.py
+++ bots/TOOLS/ai_keywords.py
@@ -3,34 +3,33 @@ import sys
 import mod_api
 
 def extract_keywords(text,id):
-    text = text.replace(chr(10),' ')
-    text = text.replace('.',';')
-    # Use regex to find "Palavras-chave:" followed by any text until the end of the line
-    #match = re.search(r"Palavras-chave:\s*(.*)", text)
+    text = text.replace(chr(10), ' ')
+    text = text.replace('.', ';')
     match = re.search(r"Palavras-chave:\s*(.*?)(?=Abstract:)", text, re.DOTALL)
 
     if match:
-        keywords = match.group(1).split(";")  # Split keywords separated by semicolons
-        keys =  [keyword.strip().capitalize() for keyword in keywords if keyword.strip()]
+        keywords = match.group(1).split(";")
+        keys = [keyword.strip().capitalize() for keyword in keywords if keyword.strip()]
 
         url = 'https://cip.brapci.inf.br/api/rdf/createConcept/Subject?name='
         for k in keys:
             if k != '':
-                data = {'name':k}
-                rst = mod_api.api_post(url+k,data)
-                print(rst)
+                data = {'name': k}
+                rst = mod_api.api_post(url + k, data)
 
                 try:
-                    if rst['id']:
+                    # Verificar se 'rst' é um dicionário e possui a chave 'id'
+                    if isinstance(rst, dict) and 'id' in rst:
                         idr = rst['id']
-                        print("==>",rst)
-                        url = 'https://cip.brapci.inf.br/api/rdf/dataAdd/?source=' + id + '&prop=hasSubject&resource=' + idr
+                        print("==>", rst)
+                        url = f'https://cip.brapci.inf.br/api/rdf/dataAdd/?source={id}&prop=hasSubject&resource={idr}'
                         print(url)
+                    else:
+                        print(f"Resposta inesperada da API para '{k}': {rst}")
                 except Exception as e:
                     print("=================ERRO===========")
                     print(f"Erro ao processar a resposta da API para a palavra-chave '{k}': {e}")
                     print(f"URL: {url}")
-                    sys.exit()
         return keys
     else:
         return []  # Return an empty list if "Palavras-chave:" not found
