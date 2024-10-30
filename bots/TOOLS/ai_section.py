074f604c4455ec24c193f43ead22bf114d4d77d5
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-10-30 10:04:41 -0300

    Update ai_section.py

--- bots/TOOLS/ai_section.py
+++ bots/TOOLS/ai_section.py
@@ -1,17 +1,23 @@
 import re
 import sys_io
 import sys
+import mod_api
 
 def locate_extrair_sessao(texto):
     Section = ''
     if 'GT- 10 – Informação e Memória' in texto:
-        Section = 'GT-10'
+        Section = 'GT-10 (ENANCIB)'
     if 'GT- 10 – Informação e Memória' in texto:
-        Section = 'GT-10'
+        Section = 'GT-10 (ENANCIB)'
 
     return Section
 
 def extrair_sessao(texto,id):
     # GT
     gt = locate_extrair_sessao(texto)
-    print(gt)
+
+    if gt != '':
+        urlKey = 'https://cip.brapci.inf.br/api/rdf/createConcept/Section?lang=pt&name='
+        data = {'apikey': gt}
+        rst = mod_api.api_post(urlKey + gt, data)
+        #hasSectionOf
