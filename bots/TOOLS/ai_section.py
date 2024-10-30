4562fe549fd8f43d03fc4ff84ec699622c1d0597
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-10-30 10:47:07 -0300

    Update ai_section.py

--- bots/TOOLS/ai_section.py
+++ bots/TOOLS/ai_section.py
@@ -3,86 +3,63 @@ import sys_io
 import sys
 import mod_api
 
+# Dicionário para mapeamento das sessões
+GT_SECTIONS = {
+    'GT- ESPECIAL': 'GT-ESPECIAL',
+    'GT1 Estudos Históricos e Epistemológicos da Ciência da Informação': 'GT-01 (ENANCIB)',
+    'GT2 Organização e Representação do Conhecimento': 'GT-02 (ENANCIB)',
+    'GT3 Informação, Educação e Trabalho': 'GT-03 (ENANCIB)',
+    'GT4 Gestão da Informação e do Conhecimento': 'GT-04 (ENANCIB)',
+#    'GT4 GESTÃO DA INFORMAÇÃO E DO CONHECIMENTO': 'GT-04 (ENANCIB)',
+    'GT5 Informação e Economia da Informação': 'GT-05 (ENANCIB)',
+    'GT6 Informação, Educação e Trabalho': 'GT-06 (ENANCIB)',
+    'GT7 Produção e Comunicação': 'GT-07 (ENANCIB)',
+    'GT8 Informação e Tecnologia': 'GT-08 (ENANCIB)',
+    'GT9 Museu, Patrimônio e Informação': 'GT-09 (ENANCIB)',
+    'GT10 Informação e Memória': 'GT-10 (ENANCIB)',
+    'GT11 Informação e Saúde': 'GT-11 (ENANCIB)',
+    'GT12 Informação, Estudos Étnico-Raciais, Gênero e Diversidade': 'GT-12 (ENANCIB)',
+}
+
+# Função para localizar e extrair a sessão
 def locate_extrair_sessao(texto):
-    Section = ''
-    texto = texto.replace('GT-','GT')
-    texto = texto.replace(' –','')
-    texto = texto.replace('GT ','GT')
-
+    texto = texto.replace('GT-', 'GT').replace(' –', '').replace('GT ', 'GT')
     print(texto[:100])
-    if 'GT- ESPECIAL' in texto:
-        Section = 'GT-ESPECIAL'
+    for key, section in GT_SECTIONS.items():
+        if key in texto:
+            return section
+        if key.upper() in texto:
+            return section
+    return ''
 
-    if 'GT1 Estudos Históricos e Epistemológicos da Ciência da Informação' in texto:
-        Section = 'GT-01 (ENANCIB)'
-    if 'GT2 Organização e Representação do Conhecimento' in texto:
-        Section = 'GT-02 (ENANCIB)'
-    if 'GT3 Informação, Educação e Trabalho' in texto:
-        Section = 'GT-03 (ENANCIB)'
-    if 'GT4 Gestão da Informação e do Conhecimento' in texto:
-        Section = 'GT-04 (ENANCIB)'
-    if 'GT5 Informação e Economia da Informação' in texto:
-        Section = 'GT-05 (ENANCIB)'
-    if 'GT6 Informação, Educação e Trabalho' in texto:
-        Section = 'GT-06 (ENANCIB)'
-
-    if 'GT7 Produção e Comunicação' in texto:
-        Section = 'GT-07 (ENANCIB)'
-    if 'GT8 Informação e Tecnologia' in texto:
-        Section = 'GT-08 (ENANCIB)'
-    if 'GT9 Museu, Patrimônio e Informação' in texto:
-        Section = 'GT-09 (ENANCIB)'
-    if 'GT10 Informação e Memória' in texto:
-        Section = 'GT-10 (ENANCIB)'
-    if 'GT11 Informação e Saúde' in texto:
-        Section = 'GT-11 (ENANCIB)'
-    if 'GT12 Informação, Estudos Étnico-Raciais, Gênero e Diversidade' in texto:
-        Section = 'GT-12 (ENANCIB)'
-
-    return Section
-
+# Função para localizar e extrair a modalidade
 def locate_extrair_modalidade(texto):
-    Modalidade = ''
     if 'Modalidade: Resumo Expandido' in texto:
-        Modalidade = 'Resumo Expandido'
-    if 'Modalidade: Trabalho Completo' in texto:
-        Modalidade = 'Trabalho Completo'
-    return Modalidade
+        return 'Resumo Expandido'
+    elif 'Modalidade: Trabalho Completo' in texto:
+        return 'Trabalho Completo'
+    return ''
 
-    return Section
+# Função para fazer a chamada de API com o nome da sessão/modalidade
+def post_to_api(name, prop, id):
+    urlKey = f'https://cip.brapci.inf.br/api/rdf/createConcept/Section?lang=pt&name={name}'
+    data = {'apikey': name}
+    response = mod_api.api_post(urlKey, data)
+    IDs = response['id']
+    url = f'https://cip.brapci.inf.br/api/rdf/dataAdd/?source={id}&prop={prop}&resource={IDs}'
+    mod_api.api_post(url, data)
 
-
-
-def extrair_sessao(texto,id):
-    # GT
+# Função principal para extrair sessão e modalidade
+def extrair_sessao(texto, id):
+    # Extrair e enviar sessão
     gt = locate_extrair_sessao(texto)
+    if gt:
+        post_to_api(gt, 'hasSectionOf', id)
+        print("Sessão:", gt)
 
-    if gt != '':
-        urlKey = 'https://cip.brapci.inf.br/api/rdf/createConcept/Section?lang=pt&name='
-        data = {'apikey': gt}
-        rst = mod_api.api_post(urlKey + gt, data)
-        IDs = rst['id']
-
-        url = f'https://cip.brapci.inf.br/api/rdf/dataAdd/?source={id}&prop=hasSectionOf&resource={IDs}'
-        rst = mod_api.api_post(url, data)
-
-        print("Sessão",gt)
-        #hasSectionOf
-
-    # GT
+    # Extrair e enviar modalidade
     mod = locate_extrair_modalidade(texto)
-
-    if mod != '':
-        urlKey = 'https://cip.brapci.inf.br/api/rdf/createConcept/Section?lang=pt&name='
-        data = {'apikey': mod}
-        rst = mod_api.api_post(urlKey + mod, data)
-        IDs = rst['id']
-
-        url = f'https://cip.brapci.inf.br/api/rdf/dataAdd/?source={id}&prop=hasSectionOf&resource={IDs}'
-        rst = mod_api.api_post(url, data)
-
-        url = f'https://cip.brapci.inf.br/api/rdf/dataAdd/?source={id}&prop=hasModalidadSection&resource={IDs}'
-        rst = mod_api.api_post(url, data)
-
-        print("Modalidade",mod)
-        #hasSectionOf
+    if mod:
+        post_to_api(mod, 'hasSectionOf', id)
+        post_to_api(mod, 'hasModalidadSection', id)
+        print("Modalidade:", mod)
