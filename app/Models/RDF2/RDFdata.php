2cff7c304cc3744847006db1430e0d533e6fdda0
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-10-30 08:29:23 -0300

    Update RDFdata.php

--- app/Models/RDF2/RDFdata.php
+++ app/Models/RDF2/RDFdata.php
@@ -55,6 +55,8 @@ class RDFdata extends Model
         $d['d_p'] = $id_prop;
         $d['d_literal'] = $lit;
 
+        pre($d);
+
         if ((($ID2 == 0) and ($lit == 0)) or ($id_prop == 0)) {
             $RSP['message'] = "<br>OPS2 - registro inválido - PROP ($id_prop) - ID2 ($ID2)  - LIT ($lit)<br>";
             $RSP['status'] = '500';
