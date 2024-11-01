e0906f4975582366b9c986f7654dfc852fa21c2c
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2024-11-01 11:57:00 -0300

    Update _bash.py

--- bots/TOOLS/_bash.py
+++ bots/TOOLS/_bash.py
@@ -13,14 +13,17 @@ def exec(id):
 import numpy as np
 
 # Lista de IDs fornecida
-ids = [
-258122
-]
+# Caminho do arquivo
+caminho_arquivo = 'dados.txt'
+
+# Leitura do arquivo e armazenamento dos dados em uma lista
+with open(caminho_arquivo, 'r') as file:
+    dados = [int(linha.strip()) for linha in file]
 
 # Converter a lista para um array numpy
-ids_array = np.array(ids)
-ids_array
+ids_array = np.array(dados)
 
 for it in ids_array:
     print("=====",it)
-    exec(it)
+    if (it != ''):
+        exec(it)
