429edf8538b499743a6bbd1a6b3f19edfb76ca9a
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-01 01:54:06 -0300

    Update mod_convert_repository.py

--- bots/TOOLS/mod_convert_repository.py
+++ bots/TOOLS/mod_convert_repository.py
@@ -1,16 +1,20 @@
 import database
 import io
 
+
+def directory(id):
+    os.makedirs(caminho, exist_ok=True)
 def convert():
-    qr = "SELECT * FROM brapci_rdf.rdf_literal "
-    qr += "JOIN brapci_rdf.rdf_data ON d_literal = id_n "
+    qr = "SELECT D2.d_r1, id_n, n_name FROM brapci_rdf.rdf_literal  "
+    qr += "JOIN brapci_rdf.rdf_data as D1 ON D1.d_literal = id_n "
+    qr += "JOIN brapci_rdf.rdf_data as D2 ON D2.d_r2 = D1.d_r1 "
     qr += "WHERE `n_name` like '_repository/1%' "
     qr += "limit 1 "
 
     row = database.query(qr)
-    print("Convert")
-    print(row)
 
-    print(qr)
+    print("Convert")
+    for line in row:
+        print(line)
 
 convert()
