a80cf1c51e916418d7dc9421098ebf144c188018
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-01 03:01:28 -0300

    Update mod_convert_repository.py

--- bots/TOOLS/mod_convert_repository.py
+++ bots/TOOLS/mod_convert_repository.py
@@ -50,7 +50,7 @@ def convert():
     qr = "SELECT D2.d_r1, id_n, n_name FROM brapci_rdf.rdf_literal  "
     qr += "JOIN brapci_rdf.rdf_data as D1 ON D1.d_literal = id_n "
     qr += "JOIN brapci_rdf.rdf_data as D2 ON D2.d_r2 = D1.d_r1 "
-    qr += "WHERE `n_name` like '_repository/0/%' "
+    qr += "WHERE `n_name` like '_repository/2%' "
 
     row = database.query(qr)
 
