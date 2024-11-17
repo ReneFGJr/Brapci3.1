75dc26687bece4fc1e195a7db17806f640dbb143
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-17 08:01:16 -0300

    Update mod_rdf.py

--- bots/TOOLS/mod_rdf.py
+++ bots/TOOLS/mod_rdf.py
@@ -1,7 +1,7 @@
 import database
 
 def recover(id):
-    qr = "select cc_class, d_r1, d_r2 from brapci_rdf.rdf_data "
+    qr = "select c_class, d_r1, d_r2 from brapci_rdf.rdf_data "
     qr += "inner join brapci_rdf.rdf_class on d_p = id_c "
     qr += f" where (d_r1 = {id}) or (d_r2 = {id}) "
     row = database.query(qr)
