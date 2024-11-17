8100584032ec12ba819448be997a24ec1282234d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-17 08:03:54 -0300

    Update mod_rdf.py

--- bots/TOOLS/mod_rdf.py
+++ bots/TOOLS/mod_rdf.py
@@ -3,7 +3,7 @@ import database
 def recover(id,classe):
     qr = "select c_class, d_r1, d_r2, n_name, n_lang from brapci_rdf.rdf_data "
     qr += "inner join brapci_rdf.rdf_class on d_p = id_c "
-    qr += "left join brapci_rdf.rdf_literal on d_literal = id_n "
+    qr += "right join brapci_rdf.rdf_literal on d_literal = id_n "
     qr += f" where (d_r1 = {id}) or (d_r2 = {id}) "
     row = database.query(qr)
 
