cf9e58f1a6bd4eca0b6224ad8d6f973cae066998
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-17 07:58:48 -0300

    Update mod_rdf.py

--- bots/TOOLS/mod_rdf.py
+++ bots/TOOLS/mod_rdf.py
@@ -2,6 +2,6 @@ import database
 
 def recover(id):
     qr = "select * from brapci_rdf.rdf_data "
-    qr = f" where (d_r1 = {id}) or (d_r2 = {id}) "
+    qr += f" where (d_r1 = {id}) or (d_r2 = {id}) "
     row = database.query(qr)
     print(row)
