e341bef660c8a35126efc695ce26d68c6b9af62e
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-17 08:36:02 -0300

    Update mod_rdf.py

--- bots/TOOLS/mod_rdf.py
+++ bots/TOOLS/mod_rdf.py
@@ -23,4 +23,14 @@ def le(id):
     qr += f" where (d_r1 = {id}) or (d_r2 = {id}) "
     row = database.query(qr)
 
+    qr = "select * from brapci_rdf.rdf_concept "
+    qr += "left join brapci_rdf.rdf_class on cc_class = id_c "
+    qr += "left join brapci_rdf.rdf_literal on cc_pref_term = id_n "
+    qr += f" where (d_r1 = {id}) or (d_r2 = {id}) "
+    row2 = database.query(qr)
+
+    dt = []
+    dt['concept'] = row2
+    dt['data'] = row
+
     return row
