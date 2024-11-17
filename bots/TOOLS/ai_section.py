b399c2a108370c36b680ffcdbb218bfb55ecfbf9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-17 09:59:30 -0300

    Update ai_section.py

--- bots/TOOLS/ai_section.py
+++ bots/TOOLS/ai_section.py
@@ -21,6 +21,15 @@ GT_SECTIONS = {
     'GT12 Informação': 'GT-12 (ENANCIB)'
 }
 
+GT_MODALIDADE = {
+    'Modalidade: Resumo Expandido':'Resumo Expandido',
+    'Modalidade: Trabalho Completo':'Trabalho Completo',
+    'Modalidade: Texto completo':'Trabalho Completo',
+    'Modalidade: Trabalho completo':'Trabalho Completo',
+    'Artigo completo':'Trabalho Completo',
+    'Modalidade da Apresentação: Pôster':'Pôster',
+}
+
 # Função para localizar e extrair a sessão
 def locate_extrair_sessao(texto):
     texto = texto[:1200]
@@ -41,19 +50,21 @@ def locate_extrair_sessao(texto):
 
 # Função para localizar e extrair a modalidade
 def locate_extrair_modalidade(texto):
-    if 'Modalidade: Resumo Expandido' in texto:
-        return 'Resumo Expandido'
-    elif 'Modalidade: Trabalho Completo' in texto:
-        return 'Trabalho Completo'
-    elif 'Modalidade: Texto completo' in texto:
-        return 'Trabalho Completo'
-    elif 'Modalidade: Trabalho completo' in texto:
-        return 'Trabalho Completo'
-    elif 'Artigo completo':
-        return 'Trabalho Completo'
-    elif 'Modalidade da Apresentação: Pôster':
-        return 'Pôster'
-    return ''
+    texto = texto[:1200]
+    texto = texto.replace('GT-', 'GT').replace('- ',' ').replace(' –', ' ').replace('–', '').replace('  ',' ').replace('GT ', 'GT').replace('GT0', 'GT').replace('&', 'e')
+    sectionX = ''
+    maxPos = 99999999
+    for key, section in GT_MODALIDADE.items():
+        if key in texto:
+            posicao = texto.find(key)
+            if posicao < maxPos:
+                maxPos = posicao
+                sectionX = key
+        if key.upper() in texto:
+            if posicao < maxPos:
+                maxPos = posicao
+                sectionX = key
+    return sectionX
 
 # Função para fazer a chamada de API com o nome da sessão/modalidade
 def post_to_api(name, prop, id):
