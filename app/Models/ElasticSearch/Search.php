5f63ba39a3185ec06d50e4cd83aa284fba437239
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-11-24 15:40:26 -0300

    Update Search.php

--- app/Models/ElasticSearch/Search.php
+++ app/Models/ElasticSearch/Search.php
@@ -58,7 +58,7 @@ class Search extends Model
             $data['_source'] = array("article_id", "id_jnl", "type", "title", "abstract", "subject", "year", "legend", "full");
 
             /* Strategy */
-            $strategy['qurey']['match']['full'] = $term;
+            $strategy['query']['match']['full'] = $term;
 
             /******************** Limites */
             $data['size'] = $offset;
