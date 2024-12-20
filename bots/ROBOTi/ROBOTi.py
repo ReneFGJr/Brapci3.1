c2dc9f4fa4bbf02cde8fb1126cd1c6216a7416af
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-12-20 07:47:24 -0300

    Update ROBOTi.py

--- bots/ROBOTi/ROBOTi.py
+++ bots/ROBOTi/ROBOTi.py
@@ -174,6 +174,7 @@ def run(parm):
         mod_elasticsearch.remove_editorial()
         mod_dataset.check_type()
         mod_ontology.checkData()
+        mod_concept.removeElastic()
 
         mod_author.check_dupla_remissiva()
         mod_author.check_remissiva()
@@ -183,6 +184,7 @@ def run(parm):
         mod_dataset.check_pbci()
         mod_subject.check_duplicate()
         mod_subject.removeOrfa()
+
 
     if (act == 'keyword'):
         print("=================KEYWORDS")
