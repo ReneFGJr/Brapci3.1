123e9655ccfee24489ebafb6223783474ad14720
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-12-20 06:50:52 -0300

    Update mod_concept.py

--- bots/ROBOTi/mod_concept.py
+++ bots/ROBOTi/mod_concept.py
@@ -59,7 +59,7 @@ def register_literal_class(classe,name,lang):
 
 def removeElastic():
     total = 0
-    qu = f"select ID from brapci_elastic.dataset where status = 9"
+    qu = f"select ID from brapci_elastic.dataset where status = 9 and JOURNAL = 75 and YEAR = 2023"
     row = database.query(qu)
     for item in row:
         total = total + 1
