a58250904b8dfe8a5b146f78c134f198b1d1b932
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-12-22 17:51:49 -0300

    Update mod_literal.py

--- bots/ROBOTi/mod_literal.py
+++ bots/ROBOTi/mod_literal.py
@@ -391,6 +391,7 @@ def check_trim():
         name = name.replace('  ',' ')
         id = ln[0]
         qru = f"update brapci_rdf.rdf_literal set n_name = '{name}' where id_n = {id}"
+        print(qru)
         database.update(qru)
         dd = dd + 1
         print('=1=>',name)
