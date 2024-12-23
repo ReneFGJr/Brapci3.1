593def76eb4fa313fae26cd8a2c98f9563287fca
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2024-12-22 21:52:41 -0300

    Update Download.php

--- app/Models/Base/Download.php
+++ app/Models/Base/Download.php
@@ -77,7 +77,7 @@ class Download extends Model
             $RSP['message'] = 'Success';
             $RSP['file'] = $file;
             $RSP['fileTXT'] = $fileTXT;
-            $RSP['full'] = file_get_contents($dir.$file);
+            $RSP['full'] = file_get_contents($file);
             //$RSP['line'] = $this->explode_line($RSP['full']);
             return $RSP;
 
