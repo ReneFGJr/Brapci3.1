d121462e0a2c648b87a91093e0c21e16d3b9889b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-02 14:16:24 -0300

    Update mod_statistics.py

--- bots/ROBOTi/mod_statistics.py
+++ bots/ROBOTi/mod_statistics.py
@@ -3,11 +3,11 @@ import sys
 
 def get_statistics():
     db = database.get_db()
-    stats = database.query('SELECT count(*) as total, CLASS FROM brapci_elastic.dataset group CLASS ')
-    print(stats)
+    row = database.query('SELECT count(*) as total, CLASS FROM brapci_elastic.dataset group CLASS ')
+    print(row)
 
     qd = "delete from brapci_elastic.dataset where ind_name like 'ITEM_%'"
-    database.delete(qd)
+    database.update(qd)
 
     for ln in row:
         print(ln)
