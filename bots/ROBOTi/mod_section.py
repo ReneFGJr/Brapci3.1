85af3015aa09171fbe140af766b11205ed83429a
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-02 11:21:57 -0300

    Update mod_section.py

--- bots/ROBOTi/mod_section.py
+++ bots/ROBOTi/mod_section.py
@@ -6,6 +6,21 @@ import database
 
 table = "brapci.section"
 
+def classificationSection():
+    qr = f"select * from brapci_oaipmh.sectoai_setspec where s_section = 1969"
+    row = database.query(qr)
+
+    if row == []:
+        print(f"Nenhum section para classificar")
+    else:
+        for r in row:
+            print(f"Classificando {r[0]}")
+            classSection(row)
+    return 0
+
+def classSection(row):
+    print(row)
+
 def getSection(Name):
     qr = f"select sc_rdf from brapci.sections where sc_name = '{Name}'"
     row = database.query(qr)
