c4d9adda25a8adcfcb9a47606e9e779e5c318076
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-02 14:35:37 -0300

    Update mod_statistics.py

--- bots/ROBOTi/mod_statistics.py
+++ bots/ROBOTi/mod_statistics.py
@@ -1,5 +1,6 @@
 import database
 import sys
+from datetime import datetime
 
 def get_statistics():
     row = database.query('SELECT count(*) as total, CLASS FROM brapci_elastic.dataset group by CLASS ')
@@ -7,6 +8,9 @@ def get_statistics():
 
     qd = "delete from brapci.statistics where ind_name like 'ITEM_%'"
     database.update(qd)
+
+    data_atual = datetime.now()
+    row.append([data_atual.strftime("%d/%m/%Y"), 'UPDATE'])
 
     for ln in row:
         qi = "insert into brapci.statistics (ind_name, ind_total) values ('ITEM_" + ln[1] + "', " + str(ln[0]) + ")"
