77020d3aed23243c6c45d9c07026d35b076dcc15
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-02 14:44:58 -0300

    Update mod_statistics.py

--- bots/ROBOTi/mod_statistics.py
+++ bots/ROBOTi/mod_statistics.py
@@ -26,7 +26,7 @@ def get_statistics():
         ind_name = f"ITEM_{ln[1]}"
         ind_total = ln[0]
 
-        qi = "INSERT INTO brapci.statistics (ind_name, ind_total) VALUES ('{ind_name}', '{ind_total}')"
+        qi = f"INSERT INTO brapci.statistics (ind_name, ind_total) VALUES ('{ind_name}', '{ind_total}')"
         print(qi)
         database.insert(qi)
         print(f"Registro inserido: ind_name={ind_name}, ind_total={ind_total}")
