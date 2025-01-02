2ff7fa8af6c01e8df8a711a0bc2ad032ccc03c11
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-02 14:56:13 -0300

    Update mod_statistics.py

--- bots/ROBOTi/mod_statistics.py
+++ bots/ROBOTi/mod_statistics.py
@@ -31,12 +31,12 @@ def get_statistics():
         database.insert(qi)
         print(f"Registro inserido: ind_name={ind_name}, ind_total={ind_total}")
 
-    qr = "SELECT cc_class, c_class, count(*) as total "
+    qr = "SELECT count(*) as total, cc_class "
     qr += "FROM brapci_rdf.rdf_concept "
     qr += "inner join brapci_rdf.rdf_class ON cc_class = id_c "
     qr += "where cc_use = id_cc "
     qr += " and ((id_c = 50) or (id_c = 13) or (id_c = 50) or (id_c = 9)) "
-    qr += "group by cc_class, c_class  "
+    qr += "group by cc_class"
     qr += "ORDER BY `rdf_class`.`c_class` ASC"
     row = database.query(qr)
     print(row)
