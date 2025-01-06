0c3aa6eb6edc2d8172dc1b7d349b8bc3225c4757
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-05 21:41:45 -0300

    Update Download.php

--- app/Models/Base/Download.php
+++ app/Models/Base/Download.php
@@ -198,8 +198,13 @@ class Download extends Model
 
 
         if (substr($name, 0, 4) == 'http') {
-            pre($dt);
-            $name = troca($name, 'http://', 'https://');
+            if (strpos($name, 'rev-ib.unam.mx'))
+                {
+
+                } else {
+                    $name = troca($name, 'http://', 'https://');
+                }
+
             $url = $name;
             echo h('<a href="' . $url . '">' . $url . '</a>', 5);
 
