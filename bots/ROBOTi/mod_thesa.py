53bdef3ecc40644ef05bd694bcfe96c8d7e08c99
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-06 00:11:12 -0300

    Update mod_thesa.py

--- bots/ROBOTi/mod_thesa.py
+++ bots/ROBOTi/mod_thesa.py
@@ -30,11 +30,12 @@ def IA_thesa():
 
 def IA_thesa2(ID):
     # Termo da Brapci
-    qr = "SELECT n_name, n_lang FROM brapci_rdf.rdf_concept "
+    qr = "SELECT n_name, n_lang, c_class FROM brapci_rdf.rdf_concept "
+    qr = " inner join brapci_rdf.rdf_class ON cc_class = id_c"
     qr += " inner join brapci_rdf.rdf_literal ON id_n = cc_pref_term"
     qr += f" where id_cc = {ID}"
     row = database.query(qr)
-    print(row)
+    print("Dados do Subject",row)
     IA_group_index(ID)
 
 def IA_create_group(ID):
