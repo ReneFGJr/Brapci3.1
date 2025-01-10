5e1f1ca5e4e5ac4955ee37bfc819aafecba3d058
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-10 08:50:34 -0300

    Update mod_pdf.py

--- bots/ROBOTi/mod_pdf.py
+++ bots/ROBOTi/mod_pdf.py
@@ -15,13 +15,19 @@ def harvestingPDF():
     qr += " order by ID DESC "
     qr += " limit 1"
     row = database.query(qr)
-    print(row)
+
+    for line in row:
+        ID = line[0]
+        getPDF(ID)
+    print("Fim da coleta de PDF")
+    return ""
 
 def getPDF(ID):
     row = mod_concept.le(ID)
     prop = mod_class.getClass("hasFileStorage")
     qr = "select n_name, n_lang from brapci_rdf.rdf_data "
     qr += "inner join brapci_rdf.rdf_concept ON d_r2 = id_cc "
+    qr += "inner join brapci_rdf.rdf_class ON cc_class = id_c "
     qr += "inner join brapci_rdf.rdf_literal ON cc_pref_term = id_n "
     qr += f" where d_r1 = {ID} and d_p = {prop}"
     row = database.query(qr)
