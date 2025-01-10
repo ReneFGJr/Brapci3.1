3e008a5a765ff113725edba08d6707cf46c43a15
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-01-10 19:41:44 -0300

    Update mod_pdf.py

--- bots/ROBOTi/mod_pdf.py
+++ bots/ROBOTi/mod_pdf.py
@@ -130,7 +130,7 @@ def download_methods(row):
         updatePDFdataset(ID,1)
         return False
     else:
-        #updatePDFdataset(ID,-1)
+        updatePDFdataset(ID,-1)
         return True
 
 def fileName(ID):
