94c96dbb85ce78a270d11e6e82913b3abb893580
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 08:37:10 -0300

    Update mod_especial_rules.py

--- bots/ROBOTi/mod_especial_rules.py
+++ bots/ROBOTi/mod_especial_rules.py
@@ -4,16 +4,14 @@ import mod_elasticsearch
 import sys
 
 def rule01():
+    limit = 10
     # Remove os dados da Revista Pesquisa Brasileira em Ciência da Informação e Biblioteconomia (12) que não tem PDF
-    qr = "select ID,id_ds from brapci_elastic.dataset where JOURNAL = 12 and (`PDF` < 0)"
+    qr = "select ID,id_ds from brapci_elastic.dataset where JOURNAL = 12 and (`PDF` < 0) limit {limit}"
     row = database.query(qr)
 
     for line in row:
-        print(line)
         ID = line[0]
         IDe = line[1]
         print("Removido trabalho ",ID," da Revista RPBCIB")
         mod_concept.remove(ID)
-
         mod_elasticsearch.remove(IDe)
-        sys.exit()
