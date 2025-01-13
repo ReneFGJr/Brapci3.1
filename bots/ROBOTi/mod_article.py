6d9a42dfdcc00c485b30ea7ba15302f34f6847d2
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 15:14:53 -0300

    Update mod_article.py

--- bots/ROBOTi/mod_article.py
+++ bots/ROBOTi/mod_article.py
@@ -141,7 +141,7 @@ def check_method02(data,jnl,id):
             return ID
         else:
             for line in row:
-                print(line[1])
+                print("=====>",line[1])
 
 
     sys.exit()
