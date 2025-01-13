5cd0fa4d9ed87ee285f067bf2e43b9e317c5f8fb
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 10:35:17 -0300

    Update mod_dataset.py

--- bots/ROBOTi/mod_dataset.py
+++ bots/ROBOTi/mod_dataset.py
@@ -71,28 +71,6 @@ def check_double_issue():
     qr += "limit 20"
     row = database.query(qr)
 
-    last = ''
-    lastID = ''
-    tot = 0
-
-    for item in row:
-        name = str(item[0]) + ' | ' + item[1]+' | ' + item[2]+' | ' + str(item[4])
-        ID = item[3]
-
-        if (name == last):
-            print(ID,lastID,name)
-            tot = tot + 1
-            qu = "update brapci_elastic.dataset "
-            qu += f" set `use` = {lastID} "
-            qu += f" where ID = {ID} "
-            print("===>",qu)
-            database.update(qu)
-
-        last = name
-        lastID = ID
-    print("Total",tot)
-    remove_duplicate()
-
 def check_duplicate():
     qr = "select JOURNAL, TITLE, AUTHORS, ID, YEAR from brapci_elastic.dataset "
     qr += " where `use` = 0 "
