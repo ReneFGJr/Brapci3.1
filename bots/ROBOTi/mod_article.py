b95c0eb660afef84a9a5e0d81fef8176802d5957
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 18:59:52 -0300

    Update mod_article.py

--- bots/ROBOTi/mod_article.py
+++ bots/ROBOTi/mod_article.py
@@ -154,7 +154,7 @@ def check_method02(data,OAIID,IDjnl):
                     # Comparação com OAIID
                     if OAIID in identifier:
                         print("====FOUND====>", OAIID)
-                        print(line[1])
+                        print(line[0])
                         sys.exit()
                     else:
                         print(f"NOT FOUND JSON Identifier: {identifier}")
