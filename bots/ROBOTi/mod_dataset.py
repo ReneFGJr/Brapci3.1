90ce5fad886e754124d5b5d2eb50e4071ab9fcf1
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 18:28:21 -0300

    Update mod_dataset.py

--- bots/ROBOTi/mod_dataset.py
+++ bots/ROBOTi/mod_dataset.py
@@ -78,7 +78,6 @@ def check_double_issue():
     row = database.query(qr)
     for line in row:
         ID = line[0]
-        ID = 22572
         print("Processando ",ID,"========================")
         qr = "select * from brapci_oaipmh.oai_listidentify where oai_rdf = " + str(ID)
         row2 = database.query(qr)
