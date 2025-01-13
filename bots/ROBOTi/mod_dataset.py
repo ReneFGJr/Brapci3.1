ed924ce521feec4d41da7bc7c8e0e8db3051ccaa
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 10:23:30 -0300

    Update mod_dataset.py

--- bots/ROBOTi/mod_dataset.py
+++ bots/ROBOTi/mod_dataset.py
@@ -16,7 +16,7 @@ def check_pbci():
         abs = json.loads(abs)
         try:
             txt = abs['Abstract']['pt'][0]
-            if 'Acesso &agrave; revista ' in txt or 'Acesso ao texto completo' in txt or 'http' in txt or 'acesso ao resumo' in txt or 'acesso ao texto completo' in txt:
+            if 'Acesso &agrave; revista' in txt or 'Acesso ao texto completo' in txt or 'http' in txt or 'acesso ao resumo' in txt or 'acesso ao texto completo' in txt:
                 qu = f"update brapci_elastic.dataset set `use` = -99 where ID = {ID}"
                 print(qu)
                 database.update(qu)
