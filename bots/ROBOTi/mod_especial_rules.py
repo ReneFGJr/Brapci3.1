4abe6ef939509a659404e76577035be9ce797403
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 08:26:23 -0300

    Update mod_especial_rules.py

--- bots/ROBOTi/mod_especial_rules.py
+++ bots/ROBOTi/mod_especial_rules.py
@@ -2,7 +2,7 @@ import database
 
 def rule01():
     # Remove os dados da Revista Pesquisa Brasileira em Ciência da Informação e Biblioteconomia (12) que não tem PDF
-    qr = "select ID from brapci_elastic.dataset where JOURNAL = 12 and `use` < 0"
+    qr = "select ID from brapci_elastic.dataset where JOURNAL = 12 and `PDF` < 0"
     row = database.query(qr)
 
     print(qr)
