c8167e165bf04bc59d199811525ac6e57ef56319
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-13 18:16:10 -0300

    Update oai_listidentify.py

--- bots/ROBOTi/oai_listidentify.py
+++ bots/ROBOTi/oai_listidentify.py
@@ -8,13 +8,9 @@ def register(ID,JNL):
     print("HELLO")
 
 def chageStatusID(ID,status):
-    qu = "update " + table + " set status = " + str(status) + " where id_oai = " + str(ID)
+    qu = "update " + table + " set oai_status = " + str(status) + " where id_oai = " + str(ID)
     database.update(qu)
 
 def zeraRDF(ID):
     qu = "update " + table + " set oai_rdf = 0 where id_oai = " + str(ID)
-    database.update(qu)
-
-def chageStatus(ID,status):
-    qu = "update " + table + " set status = " + str(status) + ", oai_rdf = 0 where oai_rdf = " + str(ID)
     database.update(qu)
