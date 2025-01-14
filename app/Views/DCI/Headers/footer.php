d4a7f05beef612045ab8666c105062f2c41a9460
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-14 11:48:51 -0300

    A

--- app/Views/DCI/Headers/footer.php
+++ app/Views/DCI/Headers/footer.php
@@ -1,124 +1,140 @@
-</body>
-<?php
-$Socials = new \App\Models\Socials();
-$ADM = $Socials->getAccess("#ADM");
-if (!isset($bg)) {
-    $bg = 'bg-primary';
-}
-?>
-<footer class="<?= $bg; ?> text-white text-center text-lg-start d-print-none" style="margin-top: 120px;">
-    <!-- Grid container -->
-    <div class="container-fluid p-4">
-        <!--Grid row-->
-        <div class="row">
-            <!--Grid column-->
-            <div class="col-lg-5 col-md-12 mb-5 mb-md-0">
-                <h5 class="text-uppercase">Coleções Brapci</h5>
-                <p>
-                    A Brapci é um agregador que reune fontes de informação da área de Ciência da Informação,
-                    Biblioteconomia, Arquivologia e Museologia. É um base de dados mantida com a colaboração de diversas
-                    instituições de ensino e pesquisa. Tem como mantenedora a Universidade Federal do Rio Grando do Sul
-                    e o Programa de Pós-Graduação em Ciência da Informação (PPGCIN).
-                </p>
-            </div>
-            <!--Grid column-->
+					<footer class="page-footer font-small blue-grey lighten-5" style="margin-top: 50px;">
 
-            <!--Grid column-->
-            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase">Bases de Dados</h5>
+					    <div style="background-color: #9009A8;">
+					        <div class="container">
 
-                <ul class="list-unstyled mb-0">
-                    <li>
-                        <a href="<?= PATH; ?>/" class="text-white">Brapci Revistas</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/benancib" class="text-white">Benancib</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/proceedings" class="text-white">Brapci Eventos</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/pq" class="text-white">Pesquisadores PQ</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/books" class="text-white">Brapci Livros</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/autoridade" class="text-white">Autoridades</a>
-                    </li>
-                    <?php
-                    if ($ADM == true) {
-                        echo '<li>';
-                        echo '<a href="' . PATH . '/admin" class="text-white">Administração</a>';
-                        echo '</li>';
-                    }
-                    ?>
-                </ul>
-            </div>
-            <!--Grid column-->
+					            <!-- Grid row-->
+					            <div class="row py-4 d-flex align-items-center">
 
-            <!--Grid column-->
-            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-                <h5 class="text-uppercase mb-0">Links</h5>
+					                <!-- Grid column -->
+					                <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+					                    <h6 class="mb-0">Get connected with us on social networks!</h6>
+					                </div>
+					                <!-- Grid column -->
 
-                <ul class="list-unstyled">
-                    <li>
-                        <a href="<?= PATH; ?>/about" class="text-white">Sobre a Brapci</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/contact" class="text-white">Contato</a>
-                    </li>
-                    <li>
-                        <a href="<?= PATH; ?>/cited" class="text-white">Como citar</a>
-                    </li>
-                </ul>
-            </div>
+					                <!-- Grid column -->
+					                <div class="col-md-6 col-lg-7 text-center text-md-right">
 
-            <!--Grid column-->
-            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-                <ul class="list-unstyled">
-                    <li>
-                        <?php
-                        /***************** LOGOS */
-                        $logo1 = array();
-                        $logo2 = array();
-                        $logo1['/img/logo/logo_ppgcin_ufrgs_pb.png'] = 'https://ufrgs.br/ppgcin';
-                        $logo2['/img/logo/ufrgs_PB_100.png'] = 'https://ufrgs.br/';
+					                    <!-- Facebook -->
+					                    <a class="fb-ic">
+					                        <i class="fab fa-facebook-f white-text mr-4"> </i>
+					                    </a>
+					                    <!-- Twitter -->
+					                    <a class="tw-ic">
+					                        <i class="fab fa-twitter white-text mr-4"> </i>
+					                    </a>
+					                    <!-- Google +-->
+					                    <a class="gplus-ic">
+					                        <i class="fab fa-google-plus-g white-text mr-4"> </i>
+					                    </a>
+					                    <!--Linkedin -->
+					                    <a class="li-ic">
+					                        <i class="fab fa-linkedin-in white-text mr-4"> </i>
+					                    </a>
+					                    <!--Instagram-->
+					                    <a class="ins-ic">
+					                        <i class="fab fa-instagram white-text"> </i>
+					                    </a>
 
-                        if ($bg == 'bg-benancib') {
-                            $logo1['/img/logo/logo_ppgci_uff_pb.png'] = 'https://ppgci-uff.com.br/';
-                            $logo2['/img/logo/logo_uff_pb.png'] = 'https://uff.br/';
-                        }
+					                </div>
+					                <!-- Grid column -->
 
-                        if ($bg == 'bg-livros') {
-                        }
+					            </div>
+					            <!-- Grid row-->
 
-                        foreach ($logo1 as $img => $url) {
-                            echo '<a href="' . $url . '" target="_new">';
-                            echo '<img src="' . $img . '" style="height: 50px;" class="m-2">';
-                            echo '</a>';
-                        }
+					        </div>
+					    </div>
+					    <!-- Footer Links -->
+					    <div class="container text-center text-md-left mt-5">
 
-                        foreach ($logo2 as $img => $url) {
-                            echo '<a href="' . $url . '" target="_new">';
-                            echo '<img src="' . $img . '" style="height: 50px;" class="m-2">';
-                            echo '</a>';
-                        }
-                        ?>
-                    </li>
+					        <!-- Grid row -->
+					        <div class="row mt-3 dark-grey-text">
 
-                </ul>
-            </div>
-            <!--Grid column-->
-        </div>
-        <!--Grid row-->
-    </div>
-    <!-- Grid container -->
+					            <!-- Grid column -->
+					            <div class="col-md-3 col-lg-4 col-xl-3 mb-4">
 
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-        © <?= date("Y"); ?> Copyright:
-        <a class="text-white" href="https://brapci.inf.br/">Brapci</a>
-    </div>
-    <!-- Copyright -->
-</footer>
+					                <!-- Content -->
+					                <h6 class="text-uppercase font-weight-bold">social.COMPANY NAME</h6>
+					                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					                <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+					                    consectetur
+					                    adipisicing elit.</p>
+
+					            </div>
+					            <!-- Grid column -->
+
+					            <!-- Grid column -->
+					            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+					                <!-- Links -->
+					                <h6 class="text-uppercase font-weight-bold">social.Products</h6>
+					                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					                <p>
+					                    <a class="dark-grey-text" href="#!">MDBootstrap</a>
+					                </p>
+					                <p>
+					                    <a class="dark-grey-text" href="#!">MDWordPress</a>
+					                </p>
+					                <p>
+					                    <a class="dark-grey-text" href="#!">BrandFlow</a>
+					                </p>
+					                <p>
+					                    <a class="dark-grey-text" href="#!">Bootstrap Angular</a>
+					                </p>
+
+					            </div>
+					            <!-- Grid column -->
+
+					            <!-- Grid column -->
+					            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+					                <!-- Links -->
+					                <h6 class="text-uppercase font-weight-bold">social.Useful_links</h6>
+					                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					                <p>
+					                    <a class="dark-grey-text" href="#!">social.profile</a>
+					                </p>
+					                <p>
+					                    <a class="dark-grey-text" href="#!">social.help</a>
+					                </p>
+
+					            </div>
+					            <!-- Grid column -->
+
+					            <!-- Grid column -->
+					            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+					                <!-- Links -->
+					                <h6 class="text-uppercase font-weight-bold">social.Contact</h6>
+					                <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+					                <p>
+					                    <i class="fas fa-home mr-3"></i> New York, NY 10012, US
+					                </p>
+					                <p>
+					                    <i class="fas fa-envelope mr-3"></i> info@example.com
+					                </p>
+					                <p>
+					                    <i class="fas fa-phone mr-3"></i> + 01 234 567 88
+					                </p>
+					                <p>
+					                    <i class="fas fa-print mr-3"></i> + 01 234 567 89
+					                </p>
+
+					            </div>
+					            <!-- Grid column -->
+
+					        </div>
+					        <!-- Grid row -->
+
+					    </div>
+					    <!-- Footer Links -->
+
+					    <!-- Copyright -->
+					    <div class="footer-copyright text-center text-black-50 py-3">© 2019-2024 Copyright:
+					        <a class="dark-grey-text" href="https://github.com/ReneFGJr/Cedap" target="_github">GitHub / ReneFGJr /
+					            Cedap </a>
+					    </div>
+					    <!-- Copyright -->
+
+					</footer>
+					<!-- Footer -->
