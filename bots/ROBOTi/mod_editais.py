3eb48caeafee40df6c8e510b914e62e86754d977
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-15 08:13:21 -0300

    Update mod_editais.py

--- bots/ROBOTi/mod_editais.py
+++ bots/ROBOTi/mod_editais.py
@@ -5,6 +5,9 @@ def register(agency,title,status):
     qr += f"where e_agencia = '"+str(agency)+"' and e_title = '"+title+"' "
     row = database.query(qr)
 
+    if status == 'Aberto':
+        status = 1
+
     if len(row) == 0:
         qr = "insert into brapci_editais.editais "
         qr += "(e_agencia,e_title,e_status) values "
