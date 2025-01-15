284fd61c0a79c7c468658821285be4c422719fb6
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-15 08:12:09 -0300

    Update mod_editais.py

--- bots/ROBOTi/mod_editais.py
+++ bots/ROBOTi/mod_editais.py
@@ -2,13 +2,13 @@ import database
 
 def register(agency,title,status):
     qr = "select * from brapci_editais.editais "
-    qr += f"where e_agencia = '"+agency+"' and e_title = '"+title+"' "
+    qr += f"where e_agencia = '"+str(agency)+"' and e_title = '"+title+"' "
     row = database.query(qr)
 
     if len(row) == 0:
         qr = "insert into brapci_editais.editais "
         qr += "(e_agencia,e_title,e_status) values "
-        qr += "('"+agency+"','"+title+"','"+status+"')"
+        qr += "('"+str(agency)+"','"+title+"','"+status+"')"
         database.query(qr)
         return True
     else:
