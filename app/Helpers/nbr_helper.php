52bc46e204f15123e1a39f815507dcc6e94e0c11
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-22 17:21:57 -0300

    Update nbr_helper.php

--- app/Helpers/nbr_helper.php
+++ app/Helpers/nbr_helper.php
@@ -124,7 +124,9 @@ function nbr_author($xa, $xp)
                 echo "====";
                 echo $n;
                 echo "====";
+
                 pre($n,false);
+                exit;
                 $Nf[$r] = substr($n, 0, $pos + 1) . mb_strtoupper($n[$pos + 1]) . mb_strtolower(substr($n, $pos + 2));
             }
             $pos = strpos($n, ' ');
