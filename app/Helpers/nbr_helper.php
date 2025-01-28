f49f2c32eded54794b3e71436fdaacc9090a1fda
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-01-28 16:45:22 -0300

    Update nbr_helper.php

--- app/Helpers/nbr_helper.php
+++ app/Helpers/nbr_helper.php
@@ -78,6 +78,8 @@ function nbr_author($xa, $xp)
     }
     $xa = trim($xa);
 
+    pre($xa,false);
+
     /******************************* NOME SOBRENOME */
     if (strpos($xa, ',') > 0) {
         $xa = substr($xa, strpos($xa, ',') + 1) . ' ' . substr($xa, 0, strpos($xa, ','));
