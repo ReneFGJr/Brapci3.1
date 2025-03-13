2102c033e4c835e63897afe105f6291cd8516e81
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-03-13 14:53:17 -0300

    A

---
+++ Ollama/fooocus/a.bat
@@ -0,0 +1,4 @@
+pip install pygit2==1.15.1
+git clone https://github.com/lllyasviel/Fooocus.git
+cd Fooocus
+C:\Users\renef\AppData\Local\Programs\Python\Python313\python.exe entry_with_update.py --share --always-high-vram
