ba3e312d5f398c997471fcfbca75d772468a0320
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-03-14 17:45:34 -0300

    Update Bibtex.php

--- app/Models/Metadata/Bibtex.php
+++ app/Models/Metadata/Bibtex.php
@@ -56,6 +56,91 @@ class Bibtex extends Model
 				}
 		}
 
+	function short($ln)
+	{
+		$bibtex = "@article{";
+
+		// Define a citation key (e.g., authorYear)
+		$citationKey = isset($ln['Authors'][0]) ? strtolower(explode(' ', $ln['Authors'][0])[0]) : 'unknown';
+		$citationKey .= isset($ln['YEAR']) ? $ln['YEAR'] : '';
+		$bibtex .= $citationKey . ",\n";
+
+		// Authors
+		if (isset($ln['Authors'])) {
+			$authors = array_map(function ($author) {
+				return nbr_author($author, 2);
+			}, $ln['Authors']);
+			$bibtex .= "  author = {" . implode(' and ', $authors) . "},\n";
+		}
+
+		// Title
+		$Title = (array)$ln['Title'];
+		$title = $Title['pt'][0] ?? $Title['es'][0] ?? $Title['en'][0] ?? $Title['fr'][0] ?? '';
+		$bibtex .= "  title = {" . $title . "},\n";
+
+		// Year
+		if (isset($ln['YEAR'])) {
+			$bibtex .= "  year = {" . $ln['YEAR'] . "},\n";
+		}
+
+		// Journal Information
+		if (isset($ln['Issue'])) {
+			$Issue = (array)$ln['Issue'];
+
+			if (isset($Issue['journal'])) {
+				$bibtex .= "  journal = {" . $Issue['journal'] . "},\n";
+			}
+			if (!empty($Issue['vol'])) {
+				$bibtex .= "  volume = {" . sonumero($Issue['vol']) . "},\n";
+			}
+			if (!empty($Issue['nr'])) {
+				$bibtex .= "  number = {" . sonumero($Issue['nr']) . "},\n";
+			}
+		}
+
+		// DOI (if exists)
+		if (isset($ln['DOI'])) {
+			$bibtex .= "  doi = {" . $ln['DOI'] . "},\n";
+		}
+
+		// URL
+		if (isset($ln['ID'])) {
+			$bibtex .= "  url = {https://hdl.handle.net/20.500.11959/brapci/" . $ln['ID'] . "},\n";
+		}
+
+		// Abstract
+		if (isset($ln['Abstract'])) {
+			$Abstract = (array)$ln['Abstract'];
+			$abstract = $Abstract['pt'][0] ?? $Abstract['es'][0] ?? $Abstract['en'][0] ?? $Abstract['fr'][0] ?? '';
+			$abstract = str_replace([chr(13), chr(10)], ' ', $abstract);
+			$bibtex .= "  abstract = {" . $abstract . "},\n";
+		}
+
+		// Keywords
+		if (isset($ln['Subject'])) {
+			$keywords = [];
+			foreach ($ln['Subject'] as $line) {
+				if (is_array($line)) {
+					foreach ($line as $word) {
+						$keywords[] = $word;
+					}
+				} else {
+					$keywords[] = $line;
+				}
+			}
+			$bibtex .= "  keywords = {" . implode(', ', $keywords) . "},\n";
+		}
+
+		// Database and Language
+		$bibtex .= "  note = {Database: BRAPCI, Language: Portuguese},\n";
+
+		// End of entry
+		$bibtex .= "}\n";
+
+		return ascii($bibtex);
+	}
+
+
 
 	function BibtexArticle($d)
 		{
@@ -77,10 +162,10 @@ class Bibtex extends Model
 					if (isset($d[$field]))
 						{
 							$sx .= $field.' = {'.$d[$fld[$r]].'},'.cr();
-						}					
+						}
 				}
 			$sx .= 'source = {Brapci},'.cr();
-			
+
 			$sx .= 'post = {'.date("Y-m-d").'}'.cr();
 			$sx .= '}'.cr();
 			return $sx;
