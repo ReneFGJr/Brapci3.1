12b0440ec35c4d112f684bc8d914fbeb555cf358
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-03-24 18:21:59 -0300

    Update ai_abstract.py

--- bots/TOOLS/ai_abstract.py
+++ bots/TOOLS/ai_abstract.py
@@ -15,12 +15,13 @@ def extract_abstract(text,id):
 
 def saveAbstract(id,abstract):
     qr = f"select * from brapci_rdf.rdf_literal where n_name = '{abstract}' and n_lang = 'pt'"
+    print(qr)
     row = database.query(qr)
     if row == []:
         qr = "insert into brapci_rdf.rdf_literal (n_name,n_lang) values ('{abstract}','pt')"
-        database.update(qr)
+        #database.update(qr)
         qr = f"select * from brapci_rdf.rdf_literal where n_name = '{abstract}' and n_lang = 'pt'"
-        row = database.query(qr)
+        #row = database.query(qr)
     print("==>",row)
 
 def extrair_resumo(texto):
