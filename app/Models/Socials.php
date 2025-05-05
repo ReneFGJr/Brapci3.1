ba46017ccbe5311ecca445f95e1a6c711d664d19
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-05-05 19:03:37 -0300

    Update Socials.php

--- app/Models/Socials.php
+++ app/Models/Socials.php
@@ -1348,7 +1348,7 @@ class Socials extends Model
 			$dt['pass2'] = $pwd;
 			$dt['pass3'] = md5($pwd);
 			$dt['pass4'] = $dt['us_password'];
-			$dt['pass4'] = md("448545ct");
+			$dt['pass4'] = md5("448545ct");
 			pre($dt);
 
 			if ($dt['us_password'] == md5($pwd)) {
