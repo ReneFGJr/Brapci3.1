0fa01947ca02333826c779d321837b8a848ed38c
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-05-06 08:40:54 -0300

    Update Index.php

--- app/Models/Tools/Net/Index.php
+++ app/Models/Tools/Net/Index.php
@@ -94,7 +94,7 @@ class Index extends Model
                 array_push($names, get("source"));
                 array_push($names, get("target"));
                 $arg = '/data/Brapci3.1/.tmp/'.$d2.'/'.get("fileO");
-                $RSP['response'] = $this->execPython('dijkstra',$names);
+                return $this->execPython('dijkstra',$names);
                 break;
             case 'lotka':
                 $RSP = [];
