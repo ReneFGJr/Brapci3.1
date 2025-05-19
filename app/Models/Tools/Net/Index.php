cd0b7926edcb2cb39c441b668a228a4b4a4932d6
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-05-19 16:23:53 -0300

    Update Index.php

--- app/Models/Tools/Net/Index.php
+++ app/Models/Tools/Net/Index.php
@@ -52,9 +52,6 @@ class Index extends Model
         // Caminho para o script Python
         $script_python = '/data/Brapci3.1/bots/TOOLS/'.$endpoint.'.py';
 
-        echo $script_python;
-        exit;
-
         // Monta o comando para executar o Python com o script e o argumento
         if (is_array($argumento)) {
             $arg = '';
