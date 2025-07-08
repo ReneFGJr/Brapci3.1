dbd6ac337a4460335c5d06c47de878613fa1c632
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-07-08 11:52:09 -0300

    Update mod_rdf.py

--- bots/TOOLS/mod_rdf.py
+++ bots/TOOLS/mod_rdf.py
@@ -17,7 +17,7 @@ def rdf_insert_concept(Classe,pref_term):
     qr = f"select * from brapci_rdf.rdf_concept where cc_class = {Classe} and cc_pref_term = {idT}"
     row = database.query(qr)
     if row == []:
-        qr = f"insert into brapci_rdf.rdf_concept (cc_class,cc_pref_term, cc_use, c_equivalent, cc_origin, cc_status, cc_versions) values ({Classe},{idT},0,0,'',1','1')"
+        qr = f"insert into brapci_rdf.rdf_concept (cc_class,cc_pref_term, cc_use, c_equivalent, cc_origin, cc_status, cc_version) values ({Classe},{idT},0,0,'',1','1')"
         database.update(qr)
 
         qr = f"select * from brapci_rdf.rdf_concept where cc_class = {Classe} and cc_pref_term = {idT}"
