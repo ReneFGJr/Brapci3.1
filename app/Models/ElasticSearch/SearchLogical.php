c99bf2401b433cd71beed672ecedc5aa8007c160
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-08-18 15:22:36 -0300

    Update SearchLogical.php

--- app/Models/ElasticSearch/SearchLogical.php
+++ app/Models/ElasticSearch/SearchLogical.php
@@ -233,6 +233,8 @@ class SearchLogical extends Model
             array_push($query['query']['bool']['must'], $filter);
         }
 
+        pre($query);
+
         /******* Range */
         $di = ((int)trim(get("year_start")) - 1);
         $df = ((int)trim(get("year_end")) + 1);
