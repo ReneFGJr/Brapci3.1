5c3ccd484dbffd0c6c0f0e6d5de77fc460a7c7f8
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-08-18 21:39:32 -0300

    Update mod_elasticsearch.py

--- bots/ROBOTi/mod_elasticsearch.py
+++ bots/ROBOTi/mod_elasticsearch.py
@@ -101,6 +101,9 @@ def export_elasticsearch_v2_2(row, offset, dtt, limit):
 
 
             all_keywords = data.get("Subject", {}).get('pt')
+            keywords = ''
+            for kw in all_keywords:
+                keywords += ascii(kw) + ' '
 
             all_abstract = data.get("Abstract", {}).get('pt')
 
@@ -111,7 +114,7 @@ def export_elasticsearch_v2_2(row, offset, dtt, limit):
             for author in author_namesX:
                 if "name" in author:
                     author_names.append(author["name"])
-            full = ascii(line[10]) + ' ' + ascii(all_abstract) + ' '.join(author_names) + ' ' + ' '.join(all_keywords)
+            full = ascii(line[10]) + ' ' + ascii(all_abstract) + ' '.join(author_names)  + ' ' + keywords
             dt = {}
             dt['id'] = line[0]
             dt['class'] = line[4]
