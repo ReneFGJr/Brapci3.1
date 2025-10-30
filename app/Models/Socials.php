1e76ad20c117c530a6010cf4fa14f3015880ccb7
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-10-30 11:57:41 -0300

    Update Socials.php

--- app/Models/Socials.php
+++ app/Models/Socials.php
@@ -1385,6 +1385,7 @@ class Socials extends Model
 
 			if ($dt['us_password'] == md5($pwd)) {
 				if (($dt['us_apikey'] == '') or ($dt['us_apikey'] == null)) {
+					pre($dt);
 					$apikey = md5($dt['us_password'] . $dt[0]['us_login']);
 					$dq = [];
 					$dq['us_apikey'] = $apikey;
