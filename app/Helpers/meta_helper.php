7e10b7eb144f64409e0a999067c48d7b93bc26f7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-01-05 18:15:46 -0300

    Update meta_helper.php

--- app/Helpers/meta_helper.php
+++ app/Helpers/meta_helper.php
@@ -1,12 +1,34 @@
 <?php
 
 if (!function_exists('meta')) {
+
     function meta(array $meta = []): string
     {
         $html = '';
 
         foreach ($meta as $name => $content) {
-            $html .= '<meta name="' . esc($name) . '" content="' . esc($content) . '">' . PHP_EOL;
+
+            /* Caso 1: keywords como array */
+            if (is_array($content) && $name === 'keywords') {
+                $content = implode(', ', $content);
+            }
+
+            /* Caso 2: meta complexa (og:, twitter:) */
+            if (is_array($content)) {
+
+                $property = $content['property'] ?? $name;
+                $value    = $content['content']  ?? '';
+
+                if ($value !== '') {
+                    $html .= '<meta property="' . esc($property) .
+                        '" content="' . esc($value) . '">' . PHP_EOL;
+                }
+            } else {
+
+                /* Caso 3: meta simples */
+                $html .= '<meta name="' . esc($name) .
+                    '" content="' . esc($content) . '">' . PHP_EOL;
+            }
         }
 
         return $html;
