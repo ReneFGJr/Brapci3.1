e4f2c4af1cc9c3db32c72e74ea0b0906b1198fa2
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-01-05 18:21:36 -0300

    A

--- app/Views/Events/form.php
+++ app/Views/Events/form.php
@@ -1,36 +1,43 @@
-<form method="post" enctype="multipart/form-data"
-    action="<?= isset($event) ? site_url('events/update/' . $event['id_ev']) : site_url('events/store') ?>">
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-12">
 
-    <label>Nome</label>
-    <input type="text" name="ev_name" class="form-control"
-        value="<?= $event['ev_name'] ?? '' ?>">
+            <form method="post" enctype="multipart/form-data"
+                action="<?= isset($event) ? site_url('events/update/' . $event['id_ev']) : site_url('events/store') ?>">
 
-    <label>Local</label>
-    <input type="text" name="ev_place" class="form-control"
-        value="<?= $event['ev_place'] ?? '' ?>">
+                <label>Nome</label>
+                <input type="text" name="ev_name" class="form-control"
+                    value="<?= $event['ev_name'] ?? '' ?>">
 
-    <label>Data início</label>
-    <input type="date" name="ev_data_start" class="form-control"
-        value="<?= $event['ev_data_start'] ?? '' ?>">
+                <label>Local</label>
+                <input type="text" name="ev_place" class="form-control"
+                    value="<?= $event['ev_place'] ?? '' ?>">
 
-    <label>Data fim</label>
-    <input type="date" name="ev_data_end" class="form-control"
-        value="<?= $event['ev_data_end'] ?? '' ?>">
+                <label>Data início</label>
+                <input type="date" name="ev_data_start" class="form-control"
+                    value="<?= $event['ev_data_start'] ?? '' ?>">
 
-    <label>URL</label>
-    <input type="url" name="ev_url" class="form-control"
-        value="<?= $event['ev_url'] ?? '' ?>">
+                <label>Data fim</label>
+                <input type="date" name="ev_data_end" class="form-control"
+                    value="<?= $event['ev_data_end'] ?? '' ?>">
 
-    <label>Descrição</label>
-    <textarea name="ev_description" class="form-control"><?= $event['ev_description'] ?? '' ?></textarea>
+                <label>URL</label>
+                <input type="url" name="ev_url" class="form-control"
+                    value="<?= $event['ev_url'] ?? '' ?>">
 
-    <label>Imagem</label>
-    <input type="file" name="ev_image" class="form-control">
+                <label>Descrição</label>
+                <textarea name="ev_description" class="form-control"><?= $event['ev_description'] ?? '' ?></textarea>
 
-    <?php if (!empty($event['ev_image'])): ?>
-        <img src="<?= base_url('uploads/events/' . $event['ev_image']) ?>" width="120">
-    <?php endif; ?>
+                <label>Imagem</label>
+                <input type="file" name="ev_image" class="form-control">
 
-    <br>
-    <button class="btn btn-success">Salvar</button>
-</form>
+                <?php if (!empty($event['ev_image'])): ?>
+                    <img src="<?= base_url('uploads/events/' . $event['ev_image']) ?>" width="120">
+                <?php endif; ?>
+
+                <br>
+                <button class="btn btn-success">Salvar</button>
+            </form>
+        </div>
+    </div>
+</div>
--- app/Views/Events/index.php
+++ app/Views/Events/index.php
@@ -1,29 +1,36 @@
-<a href="<?= site_url('events/create') ?>" class="btn btn-primary">Novo Evento</a>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-12">
+            <h1>Lista de Eventos</h1>
+            <a href="<?= site_url('events/create') ?>" class="btn btn-primary">Novo Evento</a>
 
-<table class="table table-bordered mt-3">
-    <tr>
-        <th>Imagem</th>
-        <th>Nome</th>
-        <th>Local</th>
-        <th>Início</th>
-        <th>Ações</th>
-    </tr>
+            <table class="table table-bordered mt-3">
+                <tr>
+                    <th>Imagem</th>
+                    <th>Nome</th>
+                    <th>Local</th>
+                    <th>Início</th>
+                    <th>Ações</th>
+                </tr>
 
-    <?php foreach ($events as $e): ?>
-        <tr>
-            <td>
-                <?php if ($e['ev_image']): ?>
-                    <img src="<?= base_url('uploads/events/' . $e['ev_image']) ?>" width="80">
-                <?php endif; ?>
-            </td>
-            <td><?= esc($e['ev_name']) ?></td>
-            <td><?= esc($e['ev_place']) ?></td>
-            <td><?= $e['ev_data_start'] ?></td>
-            <td>
-                <a href="<?= site_url('events/edit/' . $e['id_ev']) ?>">Editar</a> |
-                <a href="<?= site_url('events/delete/' . $e['id_ev']) ?>"
-                    onclick="return confirm('Confirma exclusão?')">Excluir</a>
-            </td>
-        </tr>
-    <?php endforeach ?>
-</table>
+                <?php foreach ($events as $e): ?>
+                    <tr>
+                        <td>
+                            <?php if ($e['ev_image']): ?>
+                                <img src="<?= base_url('uploads/events/' . $e['ev_image']) ?>" width="80">
+                            <?php endif; ?>
+                        </td>
+                        <td><?= esc($e['ev_name']) ?></td>
+                        <td><?= esc($e['ev_place']) ?></td>
+                        <td><?= $e['ev_data_start'] ?></td>
+                        <td>
+                            <a href="<?= site_url('events/edit/' . $e['id_ev']) ?>">Editar</a> |
+                            <a href="<?= site_url('events/delete/' . $e['id_ev']) ?>"
+                                onclick="return confirm('Confirma exclusão?')">Excluir</a>
+                        </td>
+                    </tr>
+                <?php endforeach ?>
+            </table>
+        </div>
+    </div>
+</div>
