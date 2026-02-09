3e73ea259fc6ac6d8f977d363d020c614a3ffd3f
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-02-09 16:46:16 -0300

    Update AuthFilter.php

--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -6,6 +6,7 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
+helper('sisdoc');
 class AuthFilter implements FilterInterface
 {
     public function before(RequestInterface $request, $arguments = null)
