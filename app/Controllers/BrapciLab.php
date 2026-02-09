3df872d3185248fa63c7512eb23e694aa1e576a8
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-02-09 17:14:34 -0300

    Update BrapciLab.php

--- app/Controllers/BrapciLab.php
+++ app/Controllers/BrapciLab.php
@@ -329,7 +329,6 @@ class BrapciLab extends BaseController
         $useID = $this->session->get('user_id'); // ajuste conforme seu auth
         $Socials = new \App\Models\Socials();
         $data = $Socials->where('id_us', $useID)->findAll();
-        pre($data);
 
         echo view('BrapciLabs/layout/header', $data);
         echo view('BrapciLabs/layout/sidebar');
