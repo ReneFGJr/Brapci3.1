edd677c243a64550e4aebf9e733c9da0e69925ea
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-02-09 16:47:57 -0300

    Update AuthFilter.php

--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -12,6 +12,7 @@ class AuthFilter implements FilterInterface
     public function before(RequestInterface $request, $arguments = null)
     {
         echo '<pre>';
+        print_($_SESSION);
         print_r(session());
         exit;
         if (! session()->get('logged_in')) {
