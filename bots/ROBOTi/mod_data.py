bbe02084295b543f38090ff08fa0ad2032f84b5d
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-03-06 15:45:38 -0300

    Update mod_data.py

--- bots/ROBOTi/mod_data.py
+++ bots/ROBOTi/mod_data.py
@@ -38,6 +38,8 @@ def removeDouble():
                 oai_deleted = item2[3]
                 if (IDidO == oai_id):
                     print(ID,oai_id,oai_id_jnl,oai_rdf,oai_deleted)
+                    qd = "delete from brapci_oaipmh.oai_listidentify where id_oai = "+str(ID)
+                    database.update(qd)
                 IDdiO = oai_id
             print(row2)
             print(item)
