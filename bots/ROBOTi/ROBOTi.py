f0e4ea17d9814fcfdb7449b6df8407dc2c545fd7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-03-05 23:59:05 -0300

    Update ROBOTi.py

--- bots/ROBOTi/ROBOTi.py
+++ bots/ROBOTi/ROBOTi.py
@@ -130,6 +130,7 @@ def run(parm):
 
     #********************* Dados Duplicados
     if (act == '600'):
+        roboti_task.task_start('GETRECORD')
         mod_data.removeDouble()
 
     if (act == '100'):
