7a84519d5c5cd805adf7c3254bc3fc0a7104b4ed
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-03-05 23:14:30 -0300

    Update mod_article.py

--- bots/ROBOTi/mod_article.py
+++ bots/ROBOTi/mod_article.py
@@ -27,9 +27,9 @@ def process(rg):
     TYPE = rg[11]
     IDX = rg[5]
 
-    print(Fore.YELLOW+f"... Processando ISSUE {TYPE} ({ID}): "+Fore.GREEN+rg[1]+Fore.WHITE)
-    print("ID",ID)
+    print(Fore.YELLOW+f"... Processando ISSUE {TYPE} ({ID}.{JNL}): "+Fore.GREEN+rg[1]+Fore.WHITE)
     path = mod_listidentify.directory(ID)+'.getRecord.json'
+    sys.exit()
 
     try:
         ##print(path)
