ede1ffe3d6c8abdecea5c835e8224ece08e7f24d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-03-16 19:14:56 -0300

    Update BrapciWorksModel.php

--- app/Models/BrapciLabs/BrapciWorksModel.php
+++ app/Models/BrapciLabs/BrapciWorksModel.php
@@ -130,8 +130,8 @@ class BrapciWorksModel extends Model
         echo '<h4>SmartRetriavel</h4>';
         echo '<p><b>Query:</b> ' . $query . ' - Project: ' . $project . '</p>';
         echo '<p><b>Vocabulário:</b><a href="https://www.ufrgs.br/thesa/web/thesa/' . $thesaVC . '" target="_blank"> Thesaurus ' . $thesaVC . '</a>
-                | <a href="' . base_url('BrapciLabs/BrapciWorks/updateVC/' . $thesaVC) . '" class="btn btn-sm btn-secondary" title="Atualizar vocabulário"><li class="bi bi-arrow-repeat"></a>
-                | <a href="' . base_url('BrapciLabs/BrapciWorks/viewVC/' . $thesaVC) . '?query=' . urlencode($query) . '&project=' . urlencode($project) . '" class="btn btn-sm btn-primary" title="Reexecutar busca">Reexecutar</a>
+                | <a href="' . base_url('BrapciLabs/updateVC/' . $thesaVC) . '" class="btn btn-sm btn-secondary" title="Atualizar vocabulário"><li class="bi bi-arrow-repeat"></a>
+                | <a href="' . base_url('BrapciLabs/viewVC/' . $thesaVC) . '?query=' . urlencode($query) . '&project=' . urlencode($project) . '" class="btn btn-sm btn-primary" title="Reexecutar busca"><li class="bi bi-eye"></a>
         </p>';
 
         if (!file_exists($PRG)) {
