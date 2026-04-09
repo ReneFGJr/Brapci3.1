5518267fe238af04dcd83bfc0614628bf817e33c
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-04-09 16:03:01 -0300

    Update mod_data.py

--- bots/ROBOTi/mod_data.py
+++ bots/ROBOTi/mod_data.py
@@ -69,7 +69,7 @@ def removeDouble():
         GROUP BY oai_rdf
         ) as tabela where total > 1
         ORDER BY oai_rdf, total desc
-        limit 5
+        limit 50
     """
 
     print("600 - Verificando dados duplicados")
