0ddae1ea4b63efc44d935a5a1925fcac42d6a32b
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-05-27 10:00:41 -0300

    Update ai_cited.py

--- bots/TOOLS/ai_cited.py
+++ bots/TOOLS/ai_cited.py
@@ -195,8 +195,7 @@ def locale_referencias_type(text):
 
     # Percorre cada linha
     for linha in linhas:
-        linha_limpa = re.sub(r'^\s*(?:[1-9]\d{0,2})\.\s+', '', linha).strip()
-        linha_limpa = re.sub(r'^\s*(?:#+\s*)?(?:[1-9]\d{0,2})\.\s+', '',linha_limpa).strip()
+        linha_limpa = linha.strip()
         linha_limpa = linha_limpa.replace('1.  ','').replace('2. ','').replace('3. ','').replace('4. ','').replace('5. ','').replace('6. ','').replace('7. ','').replace('8. ','').replace('9. ','').strip()
 
         print(linha_limpa)
