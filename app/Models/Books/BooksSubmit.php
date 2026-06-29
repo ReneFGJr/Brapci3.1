59a6e361fac2f19699382ba42ef17c073337e278
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-06-29 15:41:33 -0300

    Update BooksSubmit.php

--- app/Models/Books/BooksSubmit.php
+++ app/Models/Books/BooksSubmit.php
@@ -119,6 +119,9 @@ class BooksSubmit extends Model
         $sx .= '<button type="submit" class="btn btn-primary">' . lang('brapci.import_json') . '</button>';
         $sx .= '</form>';
         $sx .= '<div class="mt-3 small text-muted">Envie um arquivo JSON para processar esta submissão.</div>';
+
+        $txt = file_get_contents('../../_Documments/_PROMPTS-IA/catalogacao-brapci-livros.txt');
+        $sx .= '<textarea class="form-control mt-3" rows="10" readonly>' . $txt . '</textarea>';
         return $sx;
     }
 
