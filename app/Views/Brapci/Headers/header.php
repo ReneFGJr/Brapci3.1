5590219ff78fe432815876d92add682ccfb3dac1
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-07-07 09:58:37 -0300

    Update header.php

--- app/Views/Brapci/Headers/header.php
+++ app/Views/Brapci/Headers/header.php
@@ -4,6 +4,7 @@ if (!isset($page_title)) {
     $page_title = 'Brapci - Homepage';
 }
 $data['GOOGLEID'] = 'G-HSS9RYF8ZS';
+$baseUrl = rtrim(base_url(), '/');
 ?>
 
 <!-- Google tag (gtag.js) -->
@@ -25,9 +26,9 @@ $data['GOOGLEID'] = 'G-HSS9RYF8ZS';
 
     <meta name="google-site-verification" content="VZpzNVBfl5kOEtr9Upjmed96smfsO9p4N79DZT38toA" />
 
-    <link rel="icon" href="<?= URL; ?>/favicon.png" type="image/x-icon" />
-    <link rel="shortcut icon" href="<?= URL; ?>/favicon.png" type="image/x-icon" />
-    <link rel="shortcut icon" href="<?= URL; ?>/favicon.png" />
+    <link rel="icon" href="<?= $baseUrl; ?>/favicon.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?= $baseUrl; ?>/favicon.png" type="image/x-icon" />
+    <link rel="shortcut icon" href="<?= $baseUrl; ?>/favicon.png" />
 
     <?php
     if (isset($metadata)) {
@@ -36,25 +37,25 @@ $data['GOOGLEID'] = 'G-HSS9RYF8ZS';
     ?>
 
     <!---- JS -->
-    <script src="<?= URL; ?>/js/popper.min.js" crossorigin="anonymous">
+    <script src="<?= $baseUrl; ?>/js/popper.min.js" crossorigin="anonymous">
     </script>
-    <script src="<?= URL; ?>/js/bootstrap.js" crossorigin="anonymous">
+    <script src="<?= $baseUrl; ?>/js/bootstrap.js" crossorigin="anonymous">
     </script>
     <!--    <script src="<?= URL; ?>/js/bootstrap.bundle.min.js" crossorigin="anonymous">
     </script> -->
-    <script src="<?= URL; ?>/js/jquery.min.js?v3.6" crossorigin="anonymous">
+    <script src="<?= $baseUrl; ?>/js/jquery.min.js?v3.6" crossorigin="anonymous">
     </script>
-    <script src="<?= URL; ?>/js/bootstrap-datepicker.min.js?v1.9.0" crossorigin="anonymous">
+    <script src="<?= $baseUrl; ?>/js/bootstrap-datepicker.min.js?v1.9.0" crossorigin="anonymous">
     </script>
-    <script src="<?= URL; ?>/js/brapci.js?v0.1" crossorigin="anonymous"></script>
-    <script src="<?= URL; ?>/js/sisdoc_form.js?v0.1" crossorigin="anonymous"></script>
+    <script src="<?= $baseUrl; ?>/js/brapci.js?v0.1" crossorigin="anonymous"></script>
+    <script src="<?= $baseUrl; ?>/js/sisdoc_form.js?v0.1" crossorigin="anonymous"></script>
 
 
     <!---- CSS -->
-    <link rel="stylesheet" href="<?= URL; ?>/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?= URL; ?>/css/bootstrap-datepicker">
+    <link rel="stylesheet" href="<?= $baseUrl; ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?= $baseUrl; ?>/css/bootstrap-datepicker">
 
-    <link rel="stylesheet" href="<?= URL; ?>/css/brapci.css?v0.0.4">
+    <link rel="stylesheet" href="<?= $baseUrl; ?>/css/brapci.css?v0.0.4">
 
     <?= view('Google/Analytics', $data); ?>
 </head>
