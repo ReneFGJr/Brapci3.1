901d3964e24ea6ab703cb1b4e8849fda24db8625
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-07-15 14:56:42 -0300

    Update mod_issue.py

--- bots/ROBOTi/mod_issue.py
+++ bots/ROBOTi/mod_issue.py
@@ -474,11 +474,11 @@ def decode(n,lg,vl):
     ############################################## Recupera ANO
     ################### method 01 - (YEAR)
     try:
-        yearR = re.findall('\(\d{4}\)',n)
+        yearR = re.findall(r'\(\d{4}\)', n)
         if yearR == []:
-            yearR = re.findall('\ \d{4}\;',n)
+            yearR = re.findall(r'\s\d{4};', n)
         if yearR == []:
-            yearR = re.findall('\ \d{4}',n)
+            yearR = re.findall(r'\s\d{4}', n)
         for y in yearR:
             year = y.replace('(','')
             year = year.replace(')','')
@@ -492,8 +492,9 @@ def decode(n,lg,vl):
         vols = ['vol. ','vol.','v. ','v.']
         vol = ''
         for cg in vols:
-            volR = re.findall(cg+'\d',n)
-            volR = re.findall(" "+cg+"[a-zA-Z0-9\/\.\-_\+]+",n)
+            prefix = re.escape(cg)
+            volR = re.findall(prefix + r'\d', n)
+            volR = re.findall(r'\s' + prefix + r'[a-zA-Z0-9/._\-\+]+', n)
             if (vol == '') and (volR != []):
                 vol = volR[0]
                 vol = vol.replace(cg,'').strip()
@@ -505,7 +506,8 @@ def decode(n,lg,vl):
         vols = ['nr. ','num.','n. ','n.','núm.', ' Esp', ' esp']
         nr = ''
         for cg in vols:
-            volR = re.findall(" "+cg+"[a-zA-Z0-9\/\.\-_\+]+",n)
+            prefix = re.escape(cg)
+            volR = re.findall(r'\s' + prefix + r'[a-zA-Z0-9/._\-\+]+', n)
             #print("volRY",volR)
             if (nr == '') and (volR != []):
                 nr = volR[0]
