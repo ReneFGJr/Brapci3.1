c93b95b838c0ef59ceccd9b6d4b0eddf2291903b
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-07-20 11:33:47 -0300

    Update Abnt.php

--- app/Models/Metadata/Abnt.php
+++ app/Models/Metadata/Abnt.php
@@ -49,6 +49,8 @@ class Abnt extends Model
 		$Class = $dt['Class'];
 		$link = '<a href="/v/'.$dt['ID'].'" target="_blank" class="link">';
 		$linka = '</a>';
+		echo '===>'.$Class;
+		pre($dt);
 
 		switch($Class)
 			{
