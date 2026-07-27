dc81c39fd2e586aeb3633bf3e3033cc34c49152b
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-07-27 11:58:29 -0300

    Update Socials.php

--- app/Models/Socials.php
+++ app/Models/Socials.php
@@ -337,14 +337,14 @@ class Socials extends Model
 			case 'admin':
 				$sx = $this->admin($id, $dt, $cab);
 				break;
-
+			/*
 			case 'forgout':
 				$sx = $this->forgout_form($id, $dt, $cab);
 				break;
 			case 'forgot':
 				$sx = $this->forgout_form($id, $dt, $cab);
 				break;
-
+			*/
 			case 'login':
 				$sx = $cab;
 				$sx .= $this->login();
@@ -1524,13 +1524,11 @@ class Socials extends Model
 	function forgout_form($d1 = '', $d2 = '')
 	{
 		$sx = '';
-		/*
 		if (!isset($_SESSION['forgout'])) {
 			$sx = '<h2>' . lang('social.link_expired') . '</h2>';
 			$sx .= bsmessage('Abra o link no mesmo navegador da solicitação do e-mail', 3);
 			return $sx;
 		}
-		*/
 
 		$dt = $this->find($d1);
 		$keys = $_SESSION['forgout'];
