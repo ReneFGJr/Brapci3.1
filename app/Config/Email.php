9c606f568762b742acfa293f827ef6deb060fc58
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-07-27 12:13:24 -0300

    Update Email.php

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -136,5 +136,7 @@ class Email extends BaseConfig
         $this->SMTPTimeout = (int) env('EMAIL_TIMEOUT', 30);
 
         $this->SMTPCrypto = strtolower(env('EMAIL_ENCRYPTION', 'tls'));
+
+        pre($this);
     }
 }
