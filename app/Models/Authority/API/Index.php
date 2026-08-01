c686bce7c43e07b7c8180e4cd5ed65b83e27ec51
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-08-01 12:40:46 -0300

    Update Index.php

--- app/Models/Authority/API/Index.php
+++ app/Models/Authority/API/Index.php
@@ -168,8 +168,9 @@ class Index extends Model
         {
             $IDn = str_pad($ID, 8, "0", STR_PAD_LEFT);
             $file = '/_repository/' . substr($IDn,0,2) . '/' . substr($IDn,2,2) . '/' . substr($IDn,4,2) . '/' . substr($IDn,6,2) . '/photo_' . $IDn . '.jpg';
-
-            if (file_exists($_SERVER['DOCUMENT_ROOT'].$file))
+            $fileF = $_SERVER['DOCUMENT_ROOT'] . $file;
+            pre($fileF,false);
+            if (file_exists($fileF))
                 {
                     return 'https://cip.brapci.inf.br/' . $file;
                 } else {
