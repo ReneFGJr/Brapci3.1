0fe1294795d2a447a660c7a9a07e3611fc36614b
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-08-21 16:41:23 -0300

    Update RDFmetadata.php

--- app/Models/RDF2/RDFmetadata.php
+++ app/Models/RDF2/RDFmetadata.php
@@ -187,7 +187,7 @@ class RDFmetadata extends Model
 
                 $dts = $PublicationRanking
                     ->select($cp)
-                    ->join('journals_publication', 'id_publication_ranking = ranking_sources', 'left')
+                    ->join('ranking_sources', 'id_publication_ranking = ranking_sources', 'left')
                     ->where('id_publication', $dt['jnl_frbr'])->findAll();
 
                 pre($dts);
