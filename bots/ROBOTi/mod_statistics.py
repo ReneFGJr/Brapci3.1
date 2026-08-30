027619fbbb400cb96b965db302e5fbe34d939a22
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2026-08-30 16:02:28 -0300

    Update mod_statistics.py

--- bots/ROBOTi/mod_statistics.py
+++ bots/ROBOTi/mod_statistics.py
@@ -30,13 +30,13 @@ def get_statistics():
 
     #******************************* Sourcers
     qr = "SELECT count(*) as total, concat('ITEM_collections','_',jnl_collection) as c_class, jnl_collection as id_c "
-    qr += "FROM `brapci.source_source` WHERE 1 "
+    qr += "FROM brapci.source_source WHERE 1 "
     qr += "group by `jnl_collection`; "
     row3 = database.query(qr)
 
     #******************************* Sourcers
     qr = "SELECT count(*) as total, concat('ITEM_collections','_',jnl_collection) as c_class, jnl_collection as id_c "
-    qr += "FROM `brapci.source_source` WHERE jnl_historic = 1 "
+    qr += "FROM brapci.source_source WHERE jnl_historic = 1 "
     qr += "group by `jnl_collection`; "
     row4 = database.query(qr)
 
