a49e113803f1557f69ffd17826525c34543d7f7c
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-09-14 18:22:29 -0300

    Update Brapci.php

--- app/Models/Api/Endpoint/Brapci.php
+++ app/Models/Api/Endpoint/Brapci.php
@@ -816,6 +816,7 @@ class Brapci extends Model
                         return $Elastic->searchFull4();
                         break;
                     case 'v5':
+                        pre("OK");
                         return $Elastic->searchFull5();
                         break;
                     default:
