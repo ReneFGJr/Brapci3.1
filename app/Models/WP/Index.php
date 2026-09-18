20c1c61bdcf6121887c775e08a15ff944dde2c18
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-09-18 08:35:10 -0300

    Update Index.php

--- app/Models/WP/Index.php
+++ app/Models/WP/Index.php
@@ -73,7 +73,7 @@ class Index extends Model
                     if ($page === 'team' && $line['a_lang'] === 'pt') {
                         $text = str_replace('Idealizado do projeto', 'Idealizadora do projeto', $text);
                     }
-                    $lnl = ['row'=>$text,'lang'=>$line['a_lang']];
+                    $lnl = ['row'=>$text,'lang'=>$line['a_lang'],'ordem'=>(int)$line['a_order']];
                     array_push($ln,$lnl);
                 }
             return $ln;
